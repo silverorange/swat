7b5a52834b4ac009a69402398b0a7ebf396bd717
Nathan Fredrickson <nathan@silverorange.com>
2004-12-09 02:38:35 +0000

    new class
    svn commit r32

---
+++ Swat/SwatErrorMessage.php
@@ -0,0 +1,20 @@
+<?
+/**
+ * @package Swat
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @copyright silverorange 2004
+ */
+require_once('Swat/SwatObject.php');
+
+/**
+ * Data class for a user error message.
+ */
+class SwatErrorMessage extends SwatObject {
+
+	public $message;
+
+	function __construct($msg) {
+		$this->message = $msg;
+	}
+
+}
