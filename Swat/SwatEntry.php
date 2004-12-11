963af7d60f45860bf6080c3ad126b203cfdd5bba
Steven Garrity <steven@silverorange.com>
2004-12-11 02:33:03 +0000

    Added some missing doc blocks and renamed $text to $value in SwatEntry and SwatTextarea.
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@55 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/SwatEntry.php
+++ Swat/SwatEntry.php
@@ -13,25 +13,41 @@ require_once('Swat/SwatException.php');
  */
 class SwatEntry extends SwatControl {
 
-	public $text = '';
+	/*
+	 * @var string Text content of the widget.
+	 */
+	public $value = '';
+
+	/**
+	 * @var int Size in characters of the HTML text form input.
+	 */
 	public $size = 50;
 	
+	/**
+	 * @var int Maximum number of allowable characters in HTML text form input, or null.
+	 */
+	public $maxlength = null;
+
+
 	function display() {
 		$inputtag = new SwatHtmlTag('input');
 		$inputtag->type = 'text';
 		$inputtag->name = $this->name;
 		$inputtag->id = $this->name;
-		$inputtag->value = $this->text;
+		$inputtag->value = $this->value;
 		$inputtag->onfocus = "this.select();";
 		$inputtag->size = $this->size;
+
+		if ($this->maxlength != null)
+			$inputtag->maxlength = $this->maxlength;
 
 		$inputtag->display();
 	}	
 
 	function process() {
-		$this->text = $_POST[$this->name];
+		$this->value = $_POST[$this->name];
 
-		if ($this->required && !strlen($this->text))
+		if ($this->required && !strlen($this->value))
 			$this->addErrorMessage(_S("The %s field is required."));
 	}
 }
@@ -43,8 +59,8 @@ class SwatEntryInteger extends SwatEntry {
 	function process() {
 		parent::process();
 
-		if (is_numeric($this->text))
-			$this->text = intval($this->text);
+		if (is_numeric($this->value))
+			$this->value = intval($this->value);
 		else
 			$this->addErrorMessage(_S("The %s field must be an integer."));
 	}
@@ -57,8 +73,8 @@ class SwatEntryFloat extends SwatEntry {
 	function process() {
 		parent::process();
 
-		if (is_numeric($this->text))
-			$this->text = floatval($this->text);
+		if (is_numeric($this->value))
+			$this->value = floatval($this->value);
 		else
 			$this->addErrorMessage(_S("The %s field must be a number."));
 	}
