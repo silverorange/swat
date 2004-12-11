0fe42ac387fcbe7e8c601525890d7ca4c5e765e0
Nathan Fredrickson <nathan@silverorange.com>
2004-12-11 04:25:27 +0000

    Split SwatEntry and it's subclasses into separate files.
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@60 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Swat/SwatEntryFloat.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package Swat
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @copyright silverorange 2004
+ */
+require_once('Swat/SwatEntry.php');
+
+/**
+ * A float entry widget.
+ */
+class SwatEntryFloat extends SwatEntry {
+
+	public function init() {
+		$this->size = 10;
+	}
+
+	function process() {
+		parent::process();
+
+		if (is_numeric($this->value))
+			$this->value = floatval($this->value);
+		else
+			$this->addErrorMessage(_S("The %s field must be a number."));
+	}
+}
+?>
---
+++ Swat/SwatEntryInteger.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package Swat
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @copyright silverorange 2004
+ */
+require_once('Swat/SwatEntry.php');
+
+class SwatEntryInteger extends SwatEntry {
+
+	public function init() {
+		$this->size = 5;
+	}
+
+	function process() {
+		parent::process();
+
+		if (is_numeric($this->value))
+			$this->value = intval($this->value);
+		else
+			$this->addErrorMessage(_S("The %s field must be an integer."));
+	}
+}
+
