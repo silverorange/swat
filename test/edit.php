1052703aa1a73549eaa28024c4a716473524d2e8
Nathan Fredrickson <nathan@silverorange.com>
2004-12-12 19:51:35 +0000

    Some restructuring of swat data files (css, images, javascript) and test files.  Test code (and site code) should reference swat images as "swat/images/file.png".  Also javascript can be put in js files in www/javascript and be referenced as "swat/javascript/file.js" in client-side includes.
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@81 94490950-a86c-4493-9ccb-530f72933e47

--- test/edit.php
+++ test/edit.php
@@ -4,11 +4,15 @@ require('header.php');
 
 require_once('Swat/SwatLayout.php');
 
-$layout = new SwatLayout('date.xml');
+$layout = new SwatLayout('edit.xml');
 
 // TODO: not sure about this notation:
+$replystatus = $layout->getWidget('replystatus');
 $form1 = $layout->getWidget('form1');
 $frame1 = $layout->getWidget('frame1');
+
+$replystatus->options = array('0' => 'Normal', '1' => 'Hidden');
+$replystatis->selected_value = '0';
 
 if ($form1->process()) {
 	echo '<pre>';
