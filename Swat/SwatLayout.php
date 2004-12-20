91093f640f0c9a3bbf3756385cbc7d5cbd40f94f
Nathan Fredrickson <nathan@silverorange.com>
2004-12-20 19:40:39 +0000

    Add support for non-widgets: SwatTableViewColumn and SwatCellRenderer.  It adds complexity here, but sure makes using table views easy.
    svn commit r109

--- Swat/SwatLayout.php
+++ Swat/SwatLayout.php
@@ -52,20 +52,39 @@ class SwatLayout extends SwatObject {
 	private function build($node, $parent_widget) {
 		foreach ($node->children() as $childname => $childnode) {
 
-			$widget = $this->buildWidget($childname, $childnode);
+			$widget = $this->buildWidget($childname, $childnode, $parent_widget);
 
-			$this->widgets[$widget->name] = $widget;
+			if ($widget instanceof SwatWidget)
+				$this->widgets[$widget->name] = $widget;
 
-			if ($parent_widget == null)
+			if ($parent_widget == null) {
 				$parent_widget = $widget;
-			else
+
+			} elseif ($parent_widget instanceof SwatTableView) {
+				if ($widget instanceof SwatTableViewColumn)
+					$parent_widget->appendColumn($widget);
+				else
+					throw new SwatException('SwatLayout: Only '.
+						'SwatTableViewColumns can be nested within '.
+						'SwatTableViews ('.$xmlfile.')');
+
+			} elseif ($parent_widget instanceof SwatTableViewColumn) {
+				if ($widget instanceof SwatCellRenderer)
+					$parent_widget->renderer = $widget;
+				else
+					throw new SwatException('SwatLayout: Only '.
+						'SwatCellRenders can be nested within '.
+						'SwatTableViewsColumns ('.$xmlfile.')');
+
+			} else {
 				$parent_widget->add($widget);
+			}
 
 			$this->build($childnode, $widget);
 		}
 	}
 
-	private function buildWidget($name, $node) {
+	private function buildWidget($name, $node, $parent_widget) {
 
 		require_once("Swat/$name.php");
 		$w = eval(sprintf("return new %s();", $name));
@@ -92,7 +111,11 @@ class SwatLayout extends SwatObject {
 				elseif (strncmp($attrvalue, 'string:', 7) == 0)
 					$w->$attrname = substr($attrvalue, 7);
 
-				else {
+				elseif (strncmp($attrvalue, 'data:', 5) == 0) {
+					$field = substr($attrvalue, 5);
+					$parent_widget->linkField($field, $attrname);
+
+				} else {
 					if ($attrvalue == 'false' || $attrvalue == 'true' )
 						trigger_error(__CLASS__.": Possible missing 'bool:' ".
 							"on attribute $attrname", E_USER_NOTICE);
