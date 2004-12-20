226964252c728cb708b2c215e84c38d4a6f38600
Nick Burka <nick@silverorange.com>
2004-12-20 15:08:36 +0000

    Added much more functionality to the SwatDate and SwatTime objects. * Error checking for dates outside of the valid range * Fix for displaying dates within a range of less than 1 month * Much better javascript to do defaulting
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@106 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Swat/SwatTime.php
@@ -0,0 +1,220 @@
+<?php
+/**
+ * @package Swat
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @copyright silverorange 2004
+ */
+require_once('Swat/SwatControl.php');
+require_once('Swat/SwatFlydown.php');
+require_once('Date.php');
+
+/**
+ * A time entry widget.
+ */
+class SwatTime extends SwatControl {
+	
+	/**
+	 * Time of the widget. The year, month, and day fields of the Date are
+	 * unused and should be considered undefined.
+	 * @var Date
+	 */
+	public $value = null;
+	
+	const  HOUR   = 1;
+	const  MINUTE = 2;
+	const  SECOND = 4;
+	
+	/**
+	 * Time parts that are required. Bitwise combination of SwatTime::HOUR,
+	 * SwatTime::MINUTE, and SwatTime::SECOND.
+	 * @var int
+	 */
+	public $required;
+	
+	/**
+	 * Time parts that are displayed. Bitwise combination of SwatTime::HOUR,
+	 * SwatTime::MINUTE, and SwatTime::SECOND.
+	 * @var int
+	 */
+	public $display;
+	
+	/**
+	 * Start time of the valid range (inclusive). Default 00:00:00.
+	 * The year, month, and day fields of the Date are ignored and should
+	 * be considered undefined.
+	 * @var Date
+	 */
+	public $valid_range_start;
+	
+	/**
+	 * End time of the valid range (inclusive). Default 23:59:59.
+	 * The year, month, and day fields of the Date are ignored and should
+	 * be considered undefined.
+	 * @var Date
+	 */
+	public $valid_range_end;
+	
+	private $hourfly;
+	private $minutefly;
+	private $secondfly;
+	private $ampmfly;
+	
+	private $created = false;
+	
+	public function init() {
+		$this->required = self::HOUR | self::MINUTE;
+		$this->display  = self::HOUR | self::MINUTE;
+
+		$date = new Date();
+		$date->setYear(0);
+		$date->setMonth(1);
+		$date->setDay(1);
+		$date->setHour(0);
+		$date->setMinute(0);
+		$date->setSecond(0);
+		$this->valid_range_start = clone $date;
+
+		$date->setHour(23);
+		$date->setMinute(59);
+		$date->setSecond(59);
+		$this->valid_range_end = clone $date;
+	}
+	
+	public function display() {
+		$this->createFlydowns();
+		$this->displayJavascript();
+		
+		if ($this->display & self::HOUR)
+			$this->hourfly->display();
+		
+		if ($this->display & self::MINUTE)
+			$this->minutefly->display();
+		
+		if ($this->display & self::SECOND)
+			$this->secondfly->display();
+
+		if ($this->display & self::HOUR)
+			$this->ampmfly->display();
+	}
+	
+	public function process() {
+		$this->createFlydowns();
+
+		if ($this->display & self::HOUR) {
+			$this->hourfly->process();
+			$this->ampmfly->process();
+			$hour   = intval($this->hourfly->value);
+			$ampm   = $this->ampmfly->value;
+			
+			if ($hour == -1)
+				$this->addErrorMessage(_S("Hour is Required."));
+				
+			if ($ampm == -1)
+				$this->addErrorMessage(_S("AM/PM is Required."));
+			
+			if ($ampm == 'pm') {
+				$hour += 12;
+				if ($hour == 24)
+					$hour = 0;
+			}
+		} else {
+			$hour = 0;
+		}
+
+		if ($this->display & self::MINUTE) {
+			$this->minutefly->process();
+			$minute = intval($this->minutefly->value);
+			
+			if ($minute == -1)
+				$this->addErrorMessage(_S("Minute is Required."));
+		} else {
+			$minute = 0;
+		}
+
+		if ($this->display & self::SECOND) {
+			$this->secondfly->process();
+			$second = intval($this->secondfly->value);
+			
+			if ($second == -1)
+				$this->addErrorMessage(_S("Second is Required."));
+		} else {
+			$second = 0;
+		}
+
+		$this->value = new Date();
+		$this->value->setYear(0);
+		$this->value->setMonth(1);
+		$this->value->setDay(1);
+		$this->value->setHour($hour);
+		$this->value->setMinute($minute);
+		$this->value->setSecond($second);
+	}
+	
+	private function createFlydowns() { 
+		if ($this->created) return;
+		
+		$this->created = true;
+
+		if ($this->display & self::HOUR)
+			$this->createHourFlydown();
+
+		if ($this->display & self::MINUTE)
+			$this->createMinuteFlydown();
+
+		if ($this->display & self::SECOND)
+			$this->createSecondFlydown();
+			
+		if ($this->display & self::HOUR)
+			$this->createAmPmFlydown();
+	}
+	
+	private function createHourFlydown() {
+		$this->hourfly = new SwatFlydown($this->name.'_hour');
+		$this->hourfly->options = array(-1 => '');
+		$this->hourfly->onchange = sprintf("timeSet('%s', this);",
+			$this->name);
+				
+		for ($i = 1; $i <= 12; $i++)
+			$this->hourfly->options[$i] = $i;
+	}
+	
+	private function createMinuteFlydown() {
+		$this->minutefly = new SwatFlydown($this->name.'_minute');
+		$this->minutefly->options = array(-1 => '');
+		$this->minutefly->onchange = sprintf("timeSet('%s', this);",
+			$this->name);
+		
+		for ($i = 0; $i <= 59; $i++)
+			$this->minutefly->options[$i] = str_pad($i,2,'0',STR_PAD_LEFT);
+	}
+	
+	private function createSecondFlydown() {	
+		$this->secondfly = new SwatFlydown($this->name.'_second');
+		$this->secondfly->options = array(-1 => '');
+		$this->secondfly->onchange = sprintf("timeSet('%s', this);",
+			$this->name);
+		
+		for ($i = 0; $i <= 59; $i++)
+			$this->secondfly->options[$i] = str_pad($i,2,'0',STR_PAD_LEFT);
+	}
+	
+	private function createAmPmFlydown() {
+		$this->ampmfly = new SwatFlydown($this->name.'_ampm');
+		$this->ampmfly->options = array(-1 => '', 'am' => 'AM', 'pm' => 'PM');
+		$this->ampmfly->onchange = sprintf("timeSet('%s', this);",
+			$this->name);
+	}
+
+	private function displayJavascript() {
+		static $created = false;
+		
+		if (!$created) {
+			$created = true;
+			
+			echo '<script type="text/javascript">';
+			include('Swat/javascript/swat-time.js');
+			echo '</script>';
+		}
+	}
+}
+?>
---
+++ Swat/javascript/swat-time.js
@@ -0,0 +1,86 @@
+<?php
+require_once('Swat/javascript/swat-find-index.js');
+?>
+
+function timeSet(id, activeFlydown) {
+	var e = timeInit(id);
+
+	//hour is required for this, so stop if it doesn't exist
+	if (!hour) return;
+	
+	if (activeFlydown.value == -1) {
+		timeReset(id, true);
+	} else {
+		var vDate = new Date();	
+		var this_hour = vDate.getHours();
+		
+		if (this_hour > 11)
+			this_hour = (this_hour - 12);
+		
+		if (e.hour.value == this_hour)
+			timeSetNow(id, true);
+		else
+			timeSetDefault(id, true);
+	}
+}
+
+function timeInit(id) {
+	this.hour   = document.getElementById(id + '_hour');
+	this.minute = document.getElementById(id + '_minute');
+	this.second = document.getElementById(id + '_second');
+	this.ampm   = document.getElementById(id + '_ampm');
+	this.date   = document.getElementById(id + '_month');
+	return this;
+}
+
+function timeReset(id, chkdate) {
+	var e = timeInit(id);
+	
+	if (e.hour)   e.hour.selectedIndex = 0;
+	if (e.minute) e.minute.selectedIndex = 0;
+	if (e.second) e.second.selectedIndex = 0;
+	if (e.ampm)   e.ampm.selectedIndex = 0;
+	if (e.date && chkdate)
+		dateReset(id, false);
+}
+
+function timeSetNow(id, chkdate) {
+	var e = timeInit(id);
+	var vDate = new Date();	
+	
+	if (vDate.getHours() < 12) {
+		hour_out = vDate.getHours();
+		ampm_out = 1;
+	} else {
+		hour_out = (vDate.getHours() - 12);
+		ampm_out = 2;
+	}
+	
+	if (e.hour && e.hour.selectedIndex == 0)
+		e.hour.selectedIndex = find_index(e.hour, hour_out);
+	if (e.minute && e.minute.selectedIndex == 0)
+		e.minute.selectedIndex = find_index(e.minute, vDate.getMinutes());
+	if (e.second && e.second.selectedIndex == 0)
+		e.second.selectedIndex = find_index(e.second,vDate.getSeconds());
+	if (e.ampm && e.ampm.selectedIndex == 0) {
+		e.ampm.selectedIndex = ampm_out;
+	}
+	if (e.date && chkdate)
+		dateSetNow(id, false);
+}
+
+function timeSetDefault(id, chkdate) {
+	var e = timeInit(id);
+	
+	if (e.hour && e.hour.selectedIndex == 0)
+		e.hour.selectedIndex = 1;
+	if (e.minute && e.minute.selectedIndex == 0) 
+		e.minute.selectedIndex = 1;
+	if (e.second && e.second.selectedIndex == 0)
+		second.selectedIndex = 1;
+	if (e.ampm && e.ampm.selectedIndex == 0) {
+		e.ampm.selectedIndex = 1;
+	}
+	if (e.date && chkdate)
+		dateSetDefault(id, false);
+}
