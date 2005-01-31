c3602a9a6693d56c87001536526a6a3b25be97d8
Nick Burka <nick@silverorange.com>
2005-01-31 16:59:52 +0000

    Updated actions to allow for defaulted second widget.
    svn commit r280

--- Swat/javascript/swat-actions.js
+++ Swat/javascript/swat-actions.js
@@ -1,10 +1,11 @@
-function swatActionsDisplay(id, value) {
-	if (this.lastItem)
-		this.lastItem.className = 'swat-hidden';
-		
-	element = document.getElementById(id + '_' + value);
-	if (element) {
-		element.className = '';
-		this.lastItem = element;
+function swatActionsDisplay(action, name) {
+	for (i = 0; i < action.options.length; i++) {
+		element = document.getElementById(name + '_' + action.options[i].value);
+		if (element) {
+			if (action.value == action.options[i].value)
+				element.className ='';
+			else
+				element.className = 'swat-hidden';
+		}
 	}
 }
