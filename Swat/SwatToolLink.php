334fbd53a2bc4da7e0174ddf83ba698c3159e15a
Nathan Fredrickson <nathan@silverorange.com>
2005-02-01 23:42:11 +0000

    New class SwatToolLink to be used for "Create a New..." links as well as other tools in the non-existent SwatToolBox
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@289 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Swat/SwatToolLink.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package Swat
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @copyright silverorange 2004
+ */
+require_once('Swat/SwatHtmlTag.php');
+
+/**
+ * A block of content in the widget tree.
+ */
+class SwatToolLink extends SwatControl {
+
+	/**
+	 * The title of the link.
+	 * @var string
+	 */
+	public $title = '';
+
+	/**
+	 * The href of the link.
+	 * @var string
+	 */
+	public $href = '';
+
+	// TODO: add an optional image, and possibly stock images
+
+	public function display() {
+		$anchor = new SwatHtmlTag('a');
+		$anchor->href = $this->href;
+		$anchor->content = $this->title;
+		$anchor->class = 'swat-tool-link';
+
+		$anchor->display();
+	}	
+
+}
+
+?>
