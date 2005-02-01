7385c8067402fff96a05e90e3117cfd38c8fcfe0
Nathan Fredrickson <nathan@silverorange.com>
2005-02-01 16:30:46 +0000

    Add SwatContentBlock, a simple control to put arbitrary content into the widget tree.
    svn commit r285

---
+++ Swat/SwatContentBlock.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package Swat
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @copyright silverorange 2004
+ */
+require_once('Swat/SwatControl.php');
+
+/**
+ * A block of content in the widget tree.
+ */
+class SwatContentBlock extends SwatControl {
+
+	/*
+	 * Text content of the widget.
+	 * @var string
+	 */
+	public $content = '';
+
+	public function display() {
+		echo $this->content;
+	}	
+
+}
+
+?>
