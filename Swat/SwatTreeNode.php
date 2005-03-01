6d4c4e2811997e0d177052fade5e06602c6dd022
Nick Burka <nick@silverorange.com>
2005-03-01 15:20:40 +0000

    A generic tree building object with some convenience methods
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@323 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Swat/SwatTreeNode.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * A simple class for building a tree structure
+ *
+ * @package Swat
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @copyright silverorange 2004
+ */
+class SwatTreeNode {
+	/**
+	 * Array of data used for display
+	 * @var array
+	 */
+	var $data;
+	
+	/**
+	 * An array of children nodes
+	 * @var string
+	 */
+	var $children = array();
+
+	//TODO: add parent and a getPath() method would return the path of this
+	//node
+
+	public function __construct($data = array()) {
+		$this->data = $data;
+	}
+
+	/**
+	 * Return this branch as an array
+	 *
+	 * A utility method to return all child elements in a flat array with 
+	 * keys in the form of: id1/id2/id3
+	 */
+	public function toArray() {
+		$options = array();
+		
+		$this->expandNode($options, $this);
+
+		return $options;
+	}
+
+	private function expandNode(&$options, $node, $path = array()) {
+		if (count($path)) $options[implode('/',$path)] = $node->data;
+
+		foreach ($node->children as $id => $child_node)
+			$this->expandNode($options, $child_node, $this->appendPath($path, $id));
+	}
+	
+	private function appendPath($path, $id) {
+		$path[] = $id;
+		return $path;
+	}
+}
+?>
