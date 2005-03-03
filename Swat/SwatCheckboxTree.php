eb656e37f0e05eba085406d9139e0589277e4965
Nick Burka <nick@silverorange.com>
2005-03-03 11:29:02 +0000

    Added two new tree controls for flydowns and checkboxes and addded a checkall to the checkbox list
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@340 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Swat/SwatCheckboxTree.php
@@ -0,0 +1,72 @@
+<?php
+require_once('Swat/SwatCheckboxArray.php');
+
+/**
+ * A checkbox array widget formatted into a tree
+ *
+ * @package Swat
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @copyright silverorange 2004
+ */
+class SwatCheckboxTree extends SwatControl {
+
+	/**
+	 * Checkbox tree structure
+	 *
+	 * An tree collection of {@link SwatTreeNode}s.
+	 * @var SwatTreeNode
+	 */
+	public $tree = null;
+	
+	/**
+	 * Tree Path (read-only)
+	 *
+	 * An array containing the branch of the selected node.
+	 * Set at process.
+	 * @var array
+	 */
+	public $path;
+	
+	public function display() {
+		if ($this->tree != null)
+			$this->options = $this->tree->toArray();
+
+		$div_tag = new SwatHtmlTag('div');
+		$label_tag = new SwatHtmlTag('label');
+		$label_tag->class = 'swat-control';
+		
+		$input_tag = new SwatHtmlTag('input');
+		$input_tag->type = 'checkbox';
+		$input_tag->name = $this->name.'[]';
+		
+		foreach ($this->options as $key => $data) {
+			$key_array = explode('/',$key);
+		
+			$div_tag->style='margin-left: '.((count($key_array) - 1)).'em;';
+		
+			$input_tag->id = $key;
+			$input_tag->value = $key;
+			
+			if (in_array($this->values, $this->value))
+				$input_tag->checked = "checked";
+
+			$label_tag->for = $key;
+			$label_tag->open();
+
+			$div_tag->open();
+				$input_tag->display();
+			
+				$label_tag->open();
+					echo $data['title'];
+				$label_tag->close();
+			$div_tag->close();
+		}
+	}
+
+	public function process() {
+		$this->path = explode('/',$this->value);
+		$this->value = $this->path[count($this->path)-1];
+	}
+}
+
+?>
---
+++ Swat/SwatFlydownTree.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * A flydown (aka combo-box) selection widget formatted into a tree
+ *
+ * @package Swat
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @copyright silverorange 2004
+ */
+class SwatFlydownTree extends SwatFlydown {
+
+	/**
+	 * Flydown options
+	 *
+	 * An tree collection of {@link SwatTreeNode}s for the flydown.
+	 * @var SwatTreeNode
+	 */
+	public $tree = null;
+	
+	/**
+	 * Tree Path
+	 *
+	 * An array containing the branch of the selected node.
+	 * @var array
+	 */
+	public $path;
+	
+	public function display() {
+		if ($this->tree != null)
+			$this->options = $this->tree->toArray();
+
+		foreach ($this->options as $key => $data) {
+			$key_array = explode('/',$key);
+			$pad = str_repeat('&nbsp;&nbsp;', (count($key_array) - 1));
+			$this->options[$key] = $pad.$data['title'];		
+		}
+		
+		parent::display();
+	}
+
+	public function process() {
+		parent::process();
+		$this->path = explode('/',$this->value);
+		$this->value = $this->path[count($this->path)-1];
+	}
+}
+
+?>
