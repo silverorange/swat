c3c28a17f3e325759f7258797105e1737efa1feb
Nick Burka <nick@silverorange.com>
2005-03-16 21:44:29 +0000

    Revamped SwatMessageBox to use an array of SwatMessages
    svn commit r493

---
+++ Swat/SwatMessage.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * A data class to store a message  
+ *
+ * @package Swat
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @copyright silverorange 2004
+ */
+class SwatMessage {
+
+	const INFO = 1;
+	const WARNING = 2;
+	const ERROR = 3;
+
+	/**
+	 * Type of message
+	 * @var int
+	 */
+	public $type;
+
+	/**
+	 * Message text
+	 * @var string
+	 */
+	public $content = null;
+
+	/**
+	 * @param string $content Message text 
+	 * @param integer $type Type of message. Set from class constants.
+	 */
+	public function __construct($content, $type = self::INFO) {
+		$this->content = $content;
+		if ($type !== null)
+			$this->type = $type;
+	}
+
+}
+
+?>
