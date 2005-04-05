511cc19098f33e19aa30220d60f9f1d3c5a62cb1
Nathan Fredrickson <nathan@silverorange.com>
2005-04-05 17:51:55 +0000

    New generic DB wrapper class
    svn commit r558

---
+++ SwatDB/SwatDBWrapper.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * MDB2 Wrapper
+ *
+ * @package SwatDB
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @copyright silverorange 2005
+ */
+class SwatDBWrapper {
+
+	private $_found = false;
+
+	function __construct($rs) {
+		if (MDB2::isError($rs))
+			throw new Exception($rs->getMessage());
+
+		if ($rs->numrows()) {
+			$this->_found = true;
+
+			$row = $rs->fetchRow(MDB2_FETCHMODE_OBJECT);
+
+			foreach (get_object_vars($this) as $var => $val)
+				$this->$var = $row->$var;
+		}
+	}
+
+	function isFound() {
+		return $this->_found;
+	}
+}
+
+?>
