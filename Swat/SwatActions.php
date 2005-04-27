a77feea4f5f7dd689f9fe1c806772ac8c71305bf
Michael Gauthier <mike@silverorange.com>
2005-04-27 19:15:33 +0000

    Add a label tag to the index actions flydown.
    This may need some styleing added to it.
    
    
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@585 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/SwatActions.php
+++ Swat/SwatActions.php
@@ -67,7 +67,9 @@ class SwatActions extends SwatControl implements SwatUIParent {
 			$this->selected = null;
 
 		echo '<div class="swat-actions">';
+		echo '<label for="'.$this->name.'_actionfly">';
 		echo sprintf('%s: ', _S('Action'));
+		echo '</label>';
 		$this->actionfly->display();
 		echo ' ';
 		$this->apply_button->display();
