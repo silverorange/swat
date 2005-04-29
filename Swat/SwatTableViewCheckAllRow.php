acc242b13045d1ac0755967ca4a926089d8385e2
Michael Gauthier <mike@silverorange.com>
2005-04-29 19:26:15 +0000

    Smarter table output code.
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@589 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/SwatTableViewCheckAllRow.php
+++ Swat/SwatTableViewCheckAllRow.php
@@ -24,27 +24,40 @@ class SwatTableViewCheckAllRow extends SwatTableViewRow {
 
 		echo '<tr>';
 
+		// find column
+		$count = 0;
 		foreach ($columns as $column) {
-			$count = 0;
-
 			if ($column->name == $this->column_name) {
-				$td_tag = new SwatHtmlTag('td');
-				$td_tag->colspan = count($columns) - $count;
 
 				$check_all = new SwatCheckAll();
 				$check_all->series_name = $column->view->name.'_items';
-
-				$td_tag->open();
-				$check_all->display();
-				$td_tag->close();
 
 				break;
 
 			} else {
 				$count++;
-				echo '<td>&nbsp;</td>';
 			}
 		}
+
+		if ($count) {
+			
+			$td_before_tag = new SwatHtmlTag('td');
+			if ($count > 1)
+				$td_before_tag->colspan = $count;
+			
+			$td_before_tag->open();
+			echo '&nbsp;';
+			$td_before_tag->close();
+
+		}
+		
+		$td_tag = new SwatHtmlTag('td');
+		if (count($columns) - $count > 1)
+			$td_tag->colspan = count($columns) - $count;
+
+		$td_tag->open();
+		$check_all->display();
+		$td_tag->close();
 
 		echo '</tr>';
 	}
