e1a471cc9e585d9ed30fa33b693b6621c1ab90d7
Michael Gauthier <mike@silverorange.com>
2005-05-10 19:38:30 +0000

    make test work
    svn commit r594

--- test/edit.php
+++ test/edit.php
@@ -4,12 +4,13 @@ require('header.php');
 
 require_once('Swat/SwatUI.php');
 
-$layout = new SwatLayout('edit.xml');
+$interface = new SwatUI();
+$interface->loadFromXML('edit.xml');
 
 // TODO: not sure about this notation:
-$replystatus = $layout->getWidget('replystatus');
-$form1 = $layout->getWidget('form1');
-$frame1 = $layout->getWidget('frame1');
+$replystatus = $interface->getWidget('replystatus');
+$form1 = $interface->getWidget('form1');
+$frame1 = $interface->getWidget('frame1');
 
 $replystatus->options = array('0' => 'Normal', '1' => 'Hidden');
 $replystatis->selected_value = '0';
