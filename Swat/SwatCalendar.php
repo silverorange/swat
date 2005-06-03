7b18847e0a8377e10d12b9a4749de3f3c627828e
Michael Gauthier <mike@silverorange.com>
2005-06-03 15:29:11 +0000

    Tidy this file
    - move javascript include into a displayJavascript method
    - tidy comments
    - wrap lines at 80 chars
    - use SwatHtmlTag instead of echo statements
    
    
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@701 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/SwatCalendar.php
+++ Swat/SwatCalendar.php
@@ -13,29 +13,27 @@ class SwatCalendar extends SwatControl {
 
 	/**
 	 * Start date of the valid range (inclusive).
+	 *
 	 * @var Date
 	 */
 	public $valid_range_start;
 	
 	/**
 	 * End date of the valid range (exclusive).
+	 *
 	 * @var Date
 	 */
 	public $valid_range_end;
 	
-	
 	public function display() {
-		echo '<script type="text/javascript">';
-		include_once('Swat/javascript/swat-find-index.js');
-		include_once('Swat/javascript/swat-calendar.js');
-		echo '</script>';
+		$this->displayJavascript();
 		
 		$date = new Date();
 		$date->setDay(1);
 		$date->setMonth(1);
 		$date->setYear(1995);
 		
-		//set the names of weeks (locale-specific)
+		// Set the names of weeks (locale-specific)
 		$weeks = array();
 		for ($i = 1; $i < 8; $i++) {
 			$weeks[] = $date->format('%a');
@@ -43,7 +41,7 @@ class SwatCalendar extends SwatControl {
 		}
 		$weeks = "['".implode("','", $weeks)."']";
 		
-		//set the names of months (locale-specific)
+		// Set the names of months (locale-specific)
 		$months = array();
 		for ($i = 1; $i < 13; $i++) {
 			$months[] = $date->format('%b');
@@ -61,29 +59,46 @@ class SwatCalendar extends SwatControl {
 			$start_date = '';
 			
 		if (isset($this->valid_range_end)) {
-			//javascript calendar is inclusive, subtract one second from range
+			// Javascript calendar is inclusive, subtract one second from range
 			$tmp = clone $this->valid_range_end;
 	        $tmp->subtractSeconds(1);
 			$end_date = $tmp->format("%m/%d/%Y");
-		} else 
+		} else { 
 			$end_date = '';
-
+		}
 		 
 		echo '<script type="text/javascript">';
-			echo "createCalendarWidget('{$this->id}', {$months}, {$weeks}, '{$close}',
-					'$nodate', '$today');";
+		
+		echo "createCalendarWidget('{$this->id}', {$months}, {$weeks}, ",
+			"'{$close}', '$nodate', '$today');";
+			
 		echo '</script>';
 		
-		echo '<img src="swat/images/b_calendar.gif"
-				class="swat-calendar-icon"
-				id="'.$this->id.'_calendar"';
-		echo '	onmousedown="';
-		echo " clickWidgetIcon('{$this->id}','{$start_date}','{$end_date}');";
-		echo '" />';
-		echo '<br />';
-		echo '<div id="'.$this->id.'_div" class="swat-calendar-div-hide">';
-		echo '</div>';
+		$img_tag = new SwatHtmlTag('img');
+		$img_tag->src = 'swat/images/b_calendar.gif';
+		$img_tag->class = 'swat-calendar-icon';
+		$img_tag->id = $this_id.'_calendar';
+		$img_tag->onmousedown = "clickWidgetIcon('{$this->id}', ".
+			"'{$start_date}', '{$end_date}');";
 
+		$img_tag->display();
+		
+		echo '<br />';
+
+		$div_tag = new SwatHtmlTag('div');
+		$div_tag->id = $this->id.'_div';
+		$div_tag->class = 'swat-calendar-div-hide';
+		// TODO: try display() here
+		$div_tag->open();
+		$div_tag->close();
+
+	}
+
+	private function displayJavascript() {
+		echo '<script type="text/javascript">';
+		include_once('Swat/javascript/swat-find-index.js');
+		include_once('Swat/javascript/swat-calendar.js');
+		echo '</script>';
 	}
 }
 
