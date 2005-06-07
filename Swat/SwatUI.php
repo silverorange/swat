5e6a86d2417ff5174add89446a52c3a453665e37
Michael Gauthier <mike@silverorange.com>
2005-06-07 21:35:19 +0000

    Internal cleanup of this class. Lots of changes, but it shouldn't break anything.
    - added optional parameter to the SwatUI::__construct() method that accepts
      a container object reference to be the root widget.
    - renamed many variables and properties to be more descriptive between objects
      widgets and XML nodes.
    - only add widgets to the lookup array if they have an id set.
    - move node error checking into a separate private method.
    - cleaned up some error messages to use 'element' and 'attribute' where
      appropriate.
    - removed unused $widget variable from parseUI().
    
    Coding convention fixes
    - Added documentation to all properties and methods
    - Switch statement uses new indenting
    - brace style for functions and methods
    - no parentheses on require and include statements
    - lines wrap at 80 chars
    
    
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@788 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/SwatUI.php
+++ Swat/SwatUI.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once('Swat/SwatObject.php');
-require_once('Swat/SwatContainer.php');
+require_once 'Swat/SwatObject.php';
+require_once 'Swat/SwatContainer.php';
 
 /**
  * Generates a Swat widget tree from an XML UI file
@@ -10,24 +10,62 @@ require_once('Swat/SwatContainer.php');
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @copyright silverorange 2004
  */
-class SwatUI extends SwatObject {
+class SwatUI extends SwatObject
+{
+	/**
+	 * An array that maps other package classes to filenames
+	 *
+	 * The array is of the form:
+	 *    prefix => path
+	 * Where prefix is the classname prefix and path is the relative path where
+	 * the source file may be included from.
+	 *
+	 * @var array
+	 */
+	protected $classmap = array();
 
-	protected $classmap = null;
+	/**
+	 * An array of widgets populated when a UI file is parsed
+	 *
+	 * This array is used internally. The array is of the form:
+	 *    id => object reference
+	 * Where id is the unique identifier of the widget.
+	 *
+	 * @var array
+	 */
+	private $widgets = array();
 
-	private $widgets;
-	private $root;
+	/**
+	 * The root container of the widget tree created by this UI
+	 *
+	 * @var SwatContainer
+	 */
+	private $root = null;
 
-	function __construct() {
-		$this->widgets = array();
-		$this->root = new SwatContainer();
+	/**
+	 * Creates a new UI
+	 *
+	 * @param SwatContainer $container an optional reference to a container
+	 *                                  object that will be the root element of
+	 *                                  the widget tree.
+	 */
+	public function __construct($container = null)
+	{
+		if ($container !== null && $container instanceof SwatContainer)
+			$this->root = $container;
+		else
+			$this->root = new SwatContainer();
 	}
 
 	/**
-	 * Load a UI from and XML file
+	 * Loads a UI from an XML file
 	 *
-	 * @param string $filename Filename of the XML UI file to load.
+	 * @param string $filename the filename of the XML UI file to load.
+	 *
+	 * @throws SwatException
 	 */
-	public function loadFromXML($filename) {
+	public function loadFromXML($filename)
+	{
 		$xmlfile = null;
 
 		if (file_exists($filename)) {
@@ -44,7 +82,8 @@ class SwatUI extends SwatObject {
 		}
 
 		if ($xmlfile === null)
-			throw new SwatException('SwatUI: XML file not found: '.$filename);
+			throw new SwatException(sprintf(__CLASS__.
+				': XML file not found: %s', $filename));
 
 		$xml = simplexml_load_file($xmlfile);
 
@@ -52,17 +91,20 @@ class SwatUI extends SwatObject {
 	}
 
 	/**
-	 * Retrieve a widget
+	 * Retrieves a widget from the internal widget list
 	 *
-	 * Lookup a widget in the widget tree by id.
+	 * Looks up a widget in the widget list by the widget's unique identifier.
 	 *
-	 * @param string $id Id of the widget to retrieve.
-	 * @param boolean $silent If true, return null instead of throwing an 
-	 *        exception if the widget is not found.
+	 * @param string $id the id of the widget to retrieve.
+	 * @param boolean $silent if true, return null instead of throwing an 
+	 *                         exception if the widget is not found.
 	 *
-	 * @return SwatWidget A reference to the widget.
+	 * @return SwatWidget a reference to the widget.
+	 *
+	 * @throws SwatException
 	 */
-	public function getWidget($id, $silent = false) {
+	public function getWidget($id, $silent = false)
+	{
 		if (array_key_exists($id, $this->widgets))
 			return $this->widgets[$id];
 		else
@@ -74,56 +116,105 @@ class SwatUI extends SwatObject {
 	}
 
 	/**
-	 * Retrieve the top widget
+	 * Retrieves the topmost widget
 	 *
-	 * Lookup the widget at the root of the widget tree.
+	 * Looks up the widget at the root of the widget tree. The widget is
+	 * always a container.
 	 *
-	 * @return SwatWidget A reference to the widget.
+	 * @return SwatContainer a reference to the container widget.
 	 */
-	public function getRoot() {
+	public function getRoot()
+	{
 		return $this->root;
 	}
 
-	private function parseUI($node, $parent_widget) {
-		$widgets = array();
-	
-		foreach ($node->children() as $childname => $childnode) {
-			if ($childname == 'property')
-				$this->parseProperty($parent_widget, $childnode);
-			else {
-				$parsed_node = $this->parseNode($childnode);
+	/**
+	 * Recursivly parses an XML node into a widget tree
+	 *
+	 * Calls self on all node children.
+	 *
+	 * @param Object $node the XML node to begin with.
+	 * @param SwatContainer $parent_widget the parent widget to add parsed
+	 *                                      objects to.
+	 */
+	private function parseUI($node, $parent_widget)
+	{
+		foreach ($node->children() as $child_name => $child_node) {
+			if ($child_name == 'property') {
+				$this->parseProperty($child_node, $parent_widget);
+			} else {
+				$parsed_object = $this->parseNode($child_node);
 
-				if ($childname == 'widget') {
-					if (class_exists('SwatWidget') && $parsed_node instanceof SwatWidget
-						 && $parsed_node->id != null) {
+				$this->checkParsedObject($parsed_object, $child_name);
 
-						if (isset($this->widgets[$parsed_node->id]))
-							throw new SwatException(__CLASS__.
-								": widget with an id of '{$parsed_node->id}' ".
-								"already exists.");
-
-						$this->widgets[$parsed_node->id] = $parsed_node;
-
-					} elseif (!(class_exists('SwatWidget') && $parsed_node instanceof SwatWidget)) {
-						throw new SwatException(__CLASS__.
-							': '.get_class($parsed_node).' is declared as a widget '.
-							'but is not an instance of SwatWidget.');
-					}
-				} elseif ($childname == 'object') {
-					if (class_exists('SwatWidget') && $parsed_node instanceof SwatWidget) {
-						throw new SwatException(__CLASS__.
-							': '.get_class($parsed_node).' is declared as an object '.
-							'but is an instance of SwatWidget and should be declared as a widget.');
-					}
+				/*
+				 * No exceptions were thrown and the widget has an id
+				 * so add to widget list to make it look-up-able.
+				 */
+				if ($child_name == 'widget' && $parsed_object->id !== null) {
+					$this->widgets[$parsed_object->id] = $parsed_object;
 				}
-	
-				$this->attachToParent($parsed_node, $parent_widget);
-				$this->parseUI($childnode, $parsed_node);
+				
+				$this->attachToParent($parsed_object, $parent_widget);
+				$this->parseUI($child_node, $parsed_object);
 			}
 		}
 	}
 
-	private function attachToParent($widget, $parent) {
+	/**
+	 * Does some error checking on a parsed object
+	 *
+	 * Checks to make sure widget objects are created from widget elements
+	 * and other objects are created from object elements.
+	 *
+	 * @param SwatObject $parsed_object an object that has been parsed from XML.
+	 * @param string $element_name the name of the XML element node the object
+	 *                              was parsed from.
+	 *
+	 * @throws SwatException
+	 */
+	private function checkParsedObject($parsed_object, $element_name)
+	{
+		if ($element_name == 'widget') {
+			if (class_exists('SwatWidget') &&
+				$parsed_object instanceof SwatWidget &&
+				$parsed_object->id !== null) {
+
+				// make sure id is unique
+				if (isset($this->widgets[$parsed_object->id]))
+					throw new SwatException(__CLASS__.
+						": widget with an id of '{$parsed_object->id}' ".
+						"already exists.");
+
+			} elseif (!class_exists('SwatWidget') ||
+				!$parsed_object instanceof SwatWidget) {
+				
+				throw new SwatException(__CLASS__.
+					': '.get_class($parsed_object).' is declared as a widget '.
+					'but is not an instance of SwatWidget.');
+			}
+		} elseif ($childname == 'object') {
+			if (class_exists('SwatWidget') &&
+				$parsed_object instanceof SwatWidget) {
+				
+				throw new SwatException(__CLASS__.
+					': '.get_class($parsed_object).' is declared as an object '.
+					'but is an instance of SwatWidget and should be '.
+					'declared as a widget.');
+			}
+		}
+	}
+
+	/**
+	 * Attaches a widget to a parent widget in the widget tree
+	 *
+	 * @param SwatWidget $wiget the widget to attach.
+	 * @param SwatUIParent $parent the parent to attach the widget to.
+	 *
+	 * @throws SwatException
+	 */
+	private function attachToParent($widget, $parent)
+	{
 		if ($parent instanceof SwatUIParent)
 			$parent->addChild($widget);
 		else
@@ -132,22 +223,31 @@ class SwatUI extends SwatObject {
 
 	}
 
-	private function parseNode($node) {
+	/**
+	 * Parses a single XML node into a PHP object
+	 *
+	 * @param array $node the XML node data to parse.
+	 *
+	 * @return a reference to the object created.
+	 */
+	private function parseNode($node)
+	{
 		if (isset($node['class']))
 			$class = (string)$node['class'];
 		else
-			throw new SwatException("Widget/Object is missing 'class' property.");
+			throw new SwatException(__CLASS__.
+				": widget or object element is missing 'class' attribute.");
 	
 		$classfile = "Swat/{$class}.php";
 
-		if ($this->classmap !== null) {
+		if (count($this->classmap)) {
 			foreach ($this->classmap as $prefix => $path) {
 				if (strncmp($class, $prefix, strlen($prefix)) == 0)
 					$classfile = "{$path}/{$class}.php";
 			}
 		}
 
-		require_once($classfile);
+		require_once $classfile;
 		$node_class = new $class();
 	
 		if (isset($node['id']))
@@ -156,66 +256,103 @@ class SwatUI extends SwatObject {
 		return $node_class;
 	}
 
-	private function parseProperty($widget, $property) {
-		$classvars = get_class_vars(get_class($widget));
+	/**
+	 * Parses a single XML property node and applies it to an object
+	 *
+	 * @param array $property_node the XML property node data to parse.
+	 * @param SwatObject $object the object to apply the property to.
+	 *
+	 * @throws SwatException
+	 */
+	private function parseProperty($property_node, $object)
+	{
+		$class_properties = get_class_vars(get_class($object));
 		
-		if (!isset($property['name'])) {
+		if (!isset($property_node['name'])) {
 			throw new SwatException(sprintf(__CLASS__.
-				": property missing 'name' ".
-				"'%s' for class %s"), $attrvalue, get_class($widget));
+				": property element missing 'name' attribute for class %s",
+				get_class($object)));
 		
-		} elseif (!isset($property['value'])) {
+		} elseif (!isset($property_node['value'])) {
 			throw new SwatException(sprintf(__CLASS__.
-				": property missing 'value' ".
-				"'%s' for class %s"), $attrvalue, get_class($widget));
+				": property element missing 'value' attribute for class %s",
+				get_class($object)));
 		
-		} elseif (!array_key_exists((string)$property['name'], $classvars)) {
+		} elseif (!array_key_exists((string)$property_node['name'],
+			$class_properties)) {
+			
 			throw new SwatException(sprintf(__CLASS__.
-				": no attribute named '%s' in class %s",
-				(string)$property['name'], get_class($widget)));
+				": no property named '%s' in class %s",
+				(string)$property_node['name'], get_class($object)));
 				
 		} else {
-			$name = (string)$property['name'];
-			$value = (string)$property['value'];
-			$translatable = (isset($property['translatable'])
-				&& strtolower((string)$property['translatable']) == 'yes');
+			$name = (string)$property_node['name'];
+			$value = (string)$property_node['value'];
+			$translatable = (isset($property_node['translatable']) &&
+				strtolower((string)$property_node['translatable']) == 'yes');
 
-			$type = (isset($property['type'])) ?
-				(string)$property['type'] : null;
+			$type = (isset($property_node['type'])) ?
+				(string)$property_node['type'] : null;
 			
-			$widget->$name = $this->parseValue($name, $value, $type,
-				$translatable, $widget);
+			$object->$name =
+				$this->parseValue($name, $value, $type, $translatable, $object);
 		}
 	}
 
-	private function parseValue($name, $value, $type, $translatable, $widget) {
+	/**
+	 * Further parses a property
+	 *
+	 * Also does error notification in the event of a missing or unknown type
+	 * attribute.
+	 *
+	 * @param string $name the name of the property.
+	 * @param string $value the value of the property.
+	 * @param string $type the type of the value.
+	 * @param boolean translatable whether the property is translatable.
+	 * @param SwatObject $object the object the property applies to.
+	 *
+	 * @return mixed the value of the property as an appropriate PHP datatype.
+	 */
+	private function parseValue($name, $value, $type, $translatable, $object)
+	{
 		switch ($type) {
-			case 'boolean':
-				return ($value == 'true')  ? true : false;
-			case 'integer':
-				return intval($value);
-			case 'float':
-				return floatval($value);
-			case 'string':
-				return $this->translateValue($value, $translatable);
-			case 'data':
-				$widget->parent->linkField($widget, $value, $name);
-				return null;
-			default:
-				if ($value == 'false' || $value == 'true' )
-					trigger_error(__CLASS__.": Possible missing 'boolean:' ".
-						"on attribute {$name}", E_USER_NOTICE);
+		case 'boolean':
+			return ($value == 'true') ? true : false;
+		case 'integer':
+			return intval($value);
+		case 'float':
+			return floatval($value);
+		case 'string':
+			return $this->translateValue($value, $translatable);
+		case 'data':
+			$object->parent->linkField($object, $value, $name);
+			return null;
+		default:
+			if ($value == 'false' || $value == 'true' )
+				trigger_error(__CLASS__.': Possible missing type="boolean" '.
+					'attribute on property element', E_USER_NOTICE);
 
-				if (is_numeric($value))
-					trigger_error(__CLASS__.": Possible missing 'integer:' or ".
-						"'float:' on attribute {$name}", E_USER_NOTICE);
-				
-				return $this->translateValue($value, $translatable);
+			if (is_numeric($value))
+				trigger_error(__CLASS__.': Possible missing type="integer" '.
+					' or type="float" attribute on property element',
+					E_USER_NOTICE);
+
+			// default to returnsing a string
+			return $this->translateValue($value, $translatable);
 		}
-
 	}
 
-	private function translateValue($value, $translatable) {
+	/**
+	 * Translates a property value if possible
+	 *
+	 * @param string $value the value to be translated.
+	 * @param boolean $translatable whether or not it is possible to translate
+	 *                               the value.
+	 *
+	 * @return string the translated value if possible, otherwise $value.
+	 */
+	private function translateValue($value, $translatable)
+	{
 		if ($translatable)
 			return _S($value);
 		else
--- Swat/SwatUIParent.php
+++ Swat/SwatUIParent.php
@@ -7,10 +7,14 @@
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @copyright silverorange 2004
  */
-interface SwatUIParent {
-
+interface SwatUIParent
+{
+	/**
+	 * Adds a child object to this object
+	 *
+	 * @param SwatObject $child the child object to add to this object.
+	 */
 	public function addChild($child);
-
 }
 
 ?>
