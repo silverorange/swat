a7ccd9aa7ef1f65bf92a7def8290bd1e41a77451
Michael Gauthier <mike@silverorange.com>
2005-06-07 18:48:43 +0000

    Updated these files to new so coding conventions. - no parentheses for require and include statements - class and function brace style - very small documentation fixes
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@778 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/SwatObject.php
+++ Swat/SwatObject.php
@@ -1,26 +1,28 @@
 <?php
 
-require_once('Swat/SwatException.php');
+require_once 'Swat/SwatException.php';
 
 /**
- * The base object type.
+ * The base object type
  *
  * @package Swat
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @copyright silverorange 2004
  */
-class SwatObject {
-	
+class SwatObject
+{
 }
 
-function _S($msgid) {
+function _S($msgid)
+{
 	// TODO: use this for gettext translation
 	return $msgid;
 }
 
-function _nS($msgid1, $msgid2, $n) {
+function _nS($msgid1, $msgid2, $n)
+{
 	// TODO: use this for gettext translation
-	return ($n == 1 ? $msgid1 : $msgid2);
+	return ($n == 1) ? $msgid1 : $msgid2;
 }
 
 ?>
