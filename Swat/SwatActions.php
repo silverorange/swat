6803cd4dd453ef6143703aa0cd4f55e2cd06947d
Michael Gauthier <mike@silverorange.com>
2005-06-07 17:29:07 +0000

    Updated these files to new so coding conventions. - added docblocks to all properties and methods - removed parantheses from require and include statements - moved private methods after public methods - class and methods brace style - declared some properties as their type - removed setting property as array in init() method as this is not required
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@773 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/SwatActions.php
+++ Swat/SwatActions.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once('Swat/SwatControl.php');
-require_once('Swat/SwatButton.php');
-require_once('Swat/SwatFlydown.php');
-require_once('Swat/SwatActionItem.php');
-require_once('Swat/SwatUIParent.php');
+require_once 'Swat/SwatControl.php';
+require_once 'Swat/SwatButton.php';
+require_once 'Swat/SwatFlydown.php';
+require_once 'Swat/SwatActionItem.php';
+require_once 'Swat/SwatUIParent.php';
 
 /**
  * Actions widget
@@ -13,12 +13,13 @@ require_once('Swat/SwatUIParent.php');
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @copyright silverorange 2004
  */
-class SwatActions extends SwatControl implements SwatUIParent {
-	
+class SwatActions extends SwatControl implements SwatUIParent
+{
 	/**
 	 * Selected action
 	 * 
 	 * The currently selected action item, or null.
+	 *
 	 * @var SwatActionItem
 	 */
 	public $selected = null;
@@ -27,6 +28,7 @@ class SwatActions extends SwatControl implements SwatUIParent {
 	 * Show blank
 	 *
 	 * Whether to show an inital blank option in the flydown.
+	 *
 	 * @var boolean
 	 */
 	public $show_blank = true;
@@ -36,21 +38,51 @@ class SwatActions extends SwatControl implements SwatUIParent {
 	 *
 	 * Whether to auto reset the action flydown to the default action
 	 * after processing.
+	 *
 	 * @var boolean
 	 */
 	public $auto_reset = true;
 
-	private $action_flydown;
+	/**
+	 * A reference to an internal flydown widget that displays available
+	 * actions.
+	 *
+	 * @var SwatFlydown
+	 */
+	private $action_flydown = null;
+
+	/**
+	 * A reference to an internal button that is clicked to carry out the
+	 * currently selected action.
+	 *
+	 * @var SwatButton
+	 */
 	private $apply_button;
 
-	private $action_items;
-	private $created = false;
+	/**
+	 * The available actions for this actions selector.
+	 *
+	 * @var array
+	 */
+	private $action_items = array();
 
-	public function init() {
-		$this->action_items = array();
-	}
-		
-	public function display() {
+	/**
+	 * An internal flag that is set to true when internal widgets have been
+	 * created.
+	 *
+	 * @var boolean
+	 */
+	private $widgets_created = false;
+	
+	/**
+	 * Displays this list of actions
+	 *
+	 * Internal widgets are automatically created if they do not exist.
+	 * Javascript is displayed, then the display methods of the internal
+	 * widgets are called.
+	 */
+	public function display()
+	{
 		if (!$this->visible)
 			return;
 
@@ -99,7 +131,15 @@ class SwatActions extends SwatControl implements SwatUIParent {
 		
 	}
 	
-	public function process() {
+	/**
+	 * Figures out what action item is selected
+	 *
+	 * This method creates internal widgets if they do not exist, and then
+	 * determines what SwatActionItem was selected by the user by calling
+	 * the process methods of the internal widgets.
+	 */
+	public function process()
+	{
 		$this->createWidgets();
 
 		$this->action_flydown->process();
@@ -117,21 +157,52 @@ class SwatActions extends SwatControl implements SwatUIParent {
 	}
 
 	/**
-	 * Add an action item.
+	 * Adds an action item
 	 *
 	 * Adds a SwatActionItem to this SwatActions widget.
 	 *
-	 * @param SwatActionItem $item A reference to the item to add.
+	 * @param SwatActionItem $item a reference to the item to add.
+	 *
+	 * @see SwatActionItem
 	 */
-	public function addActionItem(SwatActionItem $item) {
+	public function addActionItem(SwatActionItem $item)
+	{
 		$this->action_items[$item->id] = $item;
 	}
 
-	private function createWidgets() {	
-		if ($this->created)
+	/**
+	 * Adds a child object
+	 * 
+	 * This method fulfills the {@link SwatUIParent} interface. It is used 
+	 * by {@link SwatUI} when building a widget tree and should not need to be
+	 * called elsewhere. To add an action item to an actions object use 
+	 * {@link SwatActions::addActionItem()}.
+	 *
+	 * @param $child A reference to a child object to add.
+	 *
+	 * @see SwatUIParent, SwatUI, SwatActions::addActionItem()
+	 */
+	public function addChild($child)
+	{
+		if ($child instanceof SwatActionItem)
+			$this->addActionItem($child);
+		else
+			throw new SwatException('SwatActions: Only '.
+				'SwatActionItems can be nested within SwatActions');
+	}
+
+	/**
+	 * Creates internal widgets
+	 *
+	 * Widgets references are assigned to private class properties. Widgets are
+	 * only created once even if this method is called multiple times.
+	 */
+	private function createWidgets()
+	{
+		if ($this->widgets_created)
 			return;
 		
-		$this->created = true;
+		$this->widgets_created = true;
 
 		$this->action_flydown = new SwatFlydown($this->id.'_action_flydown');
 		$this->action_flydown->onchange =
@@ -146,30 +217,18 @@ class SwatActions extends SwatControl implements SwatUIParent {
 		$this->apply_button->setTitleFromStock('apply');
 	}
 
-	private function displayJavascript() {
+	/**
+	 * Loads the Javascript required for this control
+	 *
+	 * Javascript is only loaded once to make downloads faster and so that
+	 * variable names do not collide in the javascript.
+	 */
+	private function displayJavascript()
+	{
 		echo '<script type="text/javascript">';
-		include_once('Swat/javascript/swat-actions.js');
+		include_once 'Swat/javascript/swat-actions.js';
 		echo '</script>';
 	}
-
-	/**
-	 * Add a child object
-	 * 
-	 * This method fulfills the {@link SwatUIParent} interface.  It is used 
-	 * by {@link SwatUI} when building a widget tree and should not need to be
-	 * called elsewhere.  To add an action item to an actions object use 
-	 * {@link SwatActions::addActionItem()}.
-	 *
-	 * @param $child A reference to a child object to add.
-	 */
-	public function addChild($child) {
-		if ($child instanceof SwatActionItem)
-			$this->addActionItem($child);
-		else
-			throw new SwatException('SwatActions: Only '.
-				'SwatActionItems can be nested within SwatActions');
-	}
-
 }
 
 ?>
