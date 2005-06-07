0b7928c495a74288db2a8f8ad35e6b6e9594379e
Michael Gauthier <mike@silverorange.com>
2005-06-07 17:29:07 +0000

    Updated these files to new so coding conventions. - added docblocks to all properties and methods - removed parantheses from require and include statements - moved private methods after public methods - class and methods brace style - declared some properties as their type - removed setting property as array in init() method as this is not required
    svn commit r773

--- Swat/SwatActionItem.php
+++ Swat/SwatActionItem.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once('Swat/SwatControl.php');
-require_once('Swat/SwatUIParent.php');
+require_once 'Swat/SwatControl.php';
+require_once 'Swat/SwatUIParent.php';
 
 /**
  * A single entry in a SwatActions widget
@@ -9,22 +9,64 @@ require_once('Swat/SwatUIParent.php');
  * @package Swat
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @copyright silverorange 2004
+ *
+ * @see SwatActions
  */
-class SwatActionItem extends SwatControl implements SwatUIParent {
-
+class SwatActionItem extends SwatControl implements SwatUIParent
+{
+	/**
+	 * A unique identifier for this action item
+	 *
+	 * @var string
+	 */
 	public $id;
+
+	/**
+	 * A human readable title displayed for this item
+	 *
+	 * @var string
+	 */
 	public $title = '';
+
+	/**
+	 * A SwatWidget associated with this action item
+	 *
+	 * @var SwatWidget
+	 */
 	public $widget = null;
 
-	function __construct($id = '') {
+	/**
+	 * Creates a new item for a SwatActions widget
+	 *
+	 * @param string $id a unique identifier for this item
+	 */
+	public function __construct($id = '')
+	{
 		$this->id = $id;
 	}
 
-	public function display() {
+	/**
+	 * Displays this item
+	 *
+	 * Calls this item's widget display method.
+	 */
+	public function display()
+	{
 		$this->widget->display();
 	}
 	
-	public function setWidget(SwatWidget $widget) {
+	/**
+	 * Sets the widget to use for this item
+	 *
+	 * Each SwatActionItem can have one associated SwatWidget. This method
+	 * sets the widget for this item.
+	 *
+	 * @param SwatWidget $widget the widget associated with this action.
+	 *
+	 * @throws SwatException
+	 */
+	public function setWidget(SwatWidget $widget)
+	{
 		if ($this->widget != null)
 			throw new SwatException('SwatUI: Only one widget can be nested '.
 				'within an SwatActionItem');
@@ -33,25 +75,27 @@ class SwatActionItem extends SwatControl implements SwatUIParent {
 	}
 
 	/**
-	 * Add a child object
+	 * Adds a child object
 	 * 
-	 * This method fulfills the {@link SwatUIParent} interface.  It is used 
+	 * This method fulfills the {@link SwatUIParent} interface. It is used 
 	 * by {@link SwatUI} when building a widget tree and should not need to be
-	 * called elsewhere.  To set the a widget in an action item, use 
+	 * called elsewhere. To set the a widget in an action item, use 
 	 * {@link SwatActionItem::setWidget()}.
 	 *
-	 * @param $child A reference to a child object to add.
+	 * @param $child a reference to a child object to add.
+	 *
+	 * @throws SwatException
+	 *
+	 * @see SwatUIParent, SwatUI, SwatActionItem::setWidget()
 	 */
-	public function addChild($child) {
-
+	public function addChild($child)
+	{
 		if ($child instanceof SwatWidget)
 			$this->setWidget($child);
 		else
 			throw new SwatException('SwatActionItem: Only '.
 				'SwatWidgets can be nested within SwatActionItem');
 	}
-
 }
 
 ?>
-
