435e43fe7a2cad967c09d834246ba9cf0fdfa8cb
Michael Gauthier <mike@silverorange.com>
2005-06-08 17:42:49 +0000

    Cleanup of this file.
    Also added support for more methods than post.
    
    I expereiced what I believe to be a PHP5 bug that emptied the post vars while
    testing this but it seems to have stopped, so I am committing now.
    
    
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@798 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/SwatForm.php
+++ Swat/SwatForm.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once('Swat/SwatContainer.php');
-require_once('Swat/SwatHtmlTag.php');
+require_once 'Swat/SwatContainer.php';
+require_once 'Swat/SwatHtmlTag.php';
 
 /**
  * Toplevel which can contain other widgets
@@ -10,8 +10,11 @@ require_once('Swat/SwatHtmlTag.php');
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @copyright silverorange 2004
  */
-class SwatForm extends SwatContainer {
-
+class SwatForm extends SwatContainer
+{
+	const METHOD_POST = 'post';
+	const METHOD_GET  = 'get';
+	
 	/**
 	 * The action attribute of the HTML form tag
 	 *
@@ -20,43 +23,93 @@ class SwatForm extends SwatContainer {
 	public $action = '#';
 
 	/**
-	 * The method attribute of the HTML form tag
-	 *
-	 * @var string
-	 */
-	public $method = 'post';
-
-	/**
 	 * Encoding type of the form
+	 *
+	 * Used for multipart forms for file uploads.
 	 *
 	 * @var string
 	 */
 	public $enctype = null;
 
 	/**
-	 * The button that was clicked to submit the form, or null (read only)
+	 * Hidden form fields
+	 *
+	 * An array of the form:
+	 *    name => value
+	 * where all the values are passed as hidden fields in this form.
+	 *
+	 * @var array
+	 */
+	protected $hidden_fields = array();
+	
+	/**
+	 * The method to use for this form
+	 *
+	 * Is one of SwatForm::METHOD_* constants.
+	 *
+	 * @var string
+	 */
+	private $method = SwatForm::METHOD_POST;
+
+	/**
+	 * A reference to the button that was clicked to submit the form,
+	 * or null if the button is not set.
+	 *
+	 * You usually do not want to explicitly set this in your code because
+	 * other parts of Swat set this proprety automatically.
 	 *
 	 * @var SwatButton
 	 */
 	public $button = null;
 
 	/**
-	 * Whether the form has been processed
+	 * Whether this form has been processed
 	 *
-	 * @return boolean True if the form has been processed.
+	 * @var boolean
 	 */
-	public function hasBeenProcessed() {
+	private $processed = false;
+
+	/**
+	 * Returns true if this form has been processed
+	 *
+	 * @return boolean true if the form has been processed.
+	 */
+	public function hasBeenProcessed()
+	{
 		return $this->processed;
 	}
 
-	protected $hidden_fields;
-	private $processed = false;
+	/**
+	 * Sets the HTTP method this form uses to send data
+	 *
+	 * @param string $method a method constant. Must be one of
+	 *                        SwatForm::METHOD_* otherwise an error is thrown.
+	 *
+	 * @throws SwatException
+	 */
+	public function setMethod($method)
+	{
+		$valid_methods = array(SwatForm::METHOD_POST, SwatForm::METHOD_GET);
 
-	public function init() {
-		$this->hidden_fields = array();
+		if (!in_array($method, $valid_methods))
+			throw new SwatException(sprintf(__CLASS__."'%s' is not a valid ".
+				'form method.', $method));
+
+		$this->method = $method;
 	}
-
-	public function display() {
+	
+	/**
+	 * Displays this form
+	 *
+	 * Outputs the HTML form tag and calls the display() method on each child
+	 * widget of this form. Then, after all the child widgets are displayed,
+	 * displays all hidden fields.
+	 *
+	 * This method also adds a hidden field called 'process' that is given
+	 * the unique identifier of this form as a value.
+	 */
+	public function display()
+	{
 		if (!$this->visible)
 			return;
 
@@ -77,7 +130,145 @@ class SwatForm extends SwatContainer {
 		$form_tag->close();
 	}
 
-	private function displayHiddenFields() {
+	/**
+	 * Processes this form
+	 *
+	 * If this form has been submitted then calls the process() method on
+	 * each child widget. Then processes hidden form fields.
+	 *
+	 * @return true if this form was actually submitted, false otherwise.
+	 */
+	public function process()
+	{
+		$raw_data = $this->getRawFormData();
+		
+		if (!isset($raw_data['process']) || $raw_data['process'] != $this->id)
+			return false;
+
+		$this->processed = true;
+
+		foreach ($this->children as &$child)
+			$child->process();
+
+		$this->processHiddenFields();
+
+		return true;
+	}
+
+	/**
+	 * Adds a hidden form field
+	 *
+	 * Adds a form field to this form that is not shown to the user.
+	 * Hidden form fields are outputted as type="hidden" input tags.
+	 *
+	 * @param string $name the name of the field.
+	 * @param mixed $value the value of the field, either a string or an array.
+	 */
+	public function addHiddenField($name, $value)
+	{
+		$this->hidden_fields[$name] = $value;
+	}
+
+	/**
+	 * Gets the value of a hidden form field
+	 *
+	 * @param string $name the name of the field whose value to get.
+	 *
+	 * @return mixed $value the value of the field, either a string or an 
+	 *        array, or null if the field does not exist.
+	 */
+	public function getHiddenField($name)
+	{
+		if (isset($this->hidden_fields[$name]))
+			return $this->hidden_fields[$name];
+
+		return null;
+	}
+
+	/**
+	 * Clears all hidden fields
+	 */
+	public function clearHiddenFields()
+	{
+		$this->hidden_fields = array();
+	}
+
+	/**
+	 * Adds a widget within a new SwatFormField
+	 *
+	 * This is a convenience method that does the following:
+	 * - creates a new SwatFormField,
+	 * - adds the widget as a child of the form field,
+	 * - and then adds the SwatFormField to this form.
+	 *
+	 * @param SwatWidget $widget a reference to a widget to add.
+	 * @param string $title the visible title of the form field.
+	 */
+	public function addWithField(SwatWidget $widget, $title)
+	{
+		$field = new SwatFormField();
+		$field->add($widget);
+		$field->title = $title;
+		$this->add($field);
+	}
+
+	/**
+	 * Adds a widget within a new SwatDiv
+	 *
+	 * This is a convenience method that does the following:
+	 * - creates a new SwatDiv,
+	 * - adds the widget as a child of the div,
+	 * - and then adds the SwatDiv to this form.
+	 *
+	 * @param SwatWidget $widget a reference to a widget to add.
+	 * @param string $title the CSS class of the HTML div tag.
+	 */
+	public function addWithDiv(SwatWidget $widget, $class)
+	{
+		$field = new SwatDiv();
+		$field->add($widget);
+		$field->class = $class;
+		$this->add($field);
+	}
+
+	/**
+	 * Checks submitted form data for hidden fields
+	 *
+	 * Checks submitted form data for hidden fields. If hidden fields are
+	 * found, properly re-adds them to this form.
+	 */
+	protected function processHiddenFields()
+	{
+		$raw_data = $this->getRawFormData();
+		
+		if (isset($raw_data[$this->id.'_hidden_fields']))
+			$fields = $raw_data[$this->id.'_hidden_fields'];
+		else
+			return;
+
+		if (!is_array($fields))
+			return;
+
+		foreach ($fields as $name) {
+			if (isset($raw_data[$name])) {
+				$value = $raw_data[$name];
+				$this->addHiddenField($name, $value);
+			}
+		}
+	}
+
+	/**
+	 * Displays hidden form fields
+	 *
+	 * Displays hiden form fields as <input type="hidden" /> XHTML elements.
+	 * This method automatically handles array type values so they will be
+	 * returned correctly as arrays.
+	 *
+	 * This methods also generates an array of hidden field names and passes
+	 * them as hidden fields as well.
+	 */
+	private function displayHiddenFields()
+	{
 		$input_tag = new SwatHtmlTag('input');
 		$input_tag->type = 'hidden';
 
@@ -105,101 +296,29 @@ class SwatForm extends SwatContainer {
 		echo '</div>';
 	}
 
-	public function process() {
-		if (!isset($_POST['process']) || $_POST['process'] != $this->id)
-			return false;
+	/**
+	 * Returns the super-global array with this form's data
+	 *
+	 * Returns a reference to the super-global array containing this
+	 * form's data. The array is chosen based on this form's method.
+	 *
+	 * @return array a reference to the super-global array containing this
+	 *                form's data.
+	 */
+	public function getRawFormData()
+	{
+		$data = null;
 
-		$this->processed = true;
-
-		foreach ($this->children as &$child)
-			$child->process();
-
-		$this->processHiddenFields();
-
-		return true;
-	}
-
-	protected function processHiddenFields() {
-		if (isset($_POST[$this->id.'_hidden_fields']))
-			$fields = $_POST[$this->id.'_hidden_fields'];
-		else
-			return;
-
-		if (!is_array($fields))
-			return;
-
-		foreach ($fields as $name) {
-			if (isset($_POST[$name])) {
-				$value = $_POST[$name];
-				$this->addHiddenField($name, $value);
-			}
+		switch ($this->method) {
+		case SwatForm::METHOD_POST:
+			$data = &$_POST;
+			break;
+		case SwatForm::METHOD_GET:
+			$data = &$_GET;
+			break;
 		}
-	}
 
-	/**
-	 * Add a hidden form field
-	 *
-	 * Add an HTML input type=hidden field to this form.
-	 *
-	 * @param string $name The name of the field.
-	 * @param mixed $value The value of the field, either a string or an array.
-	 */
-	public function addHiddenField($name, $value) {
-		$this->hidden_fields[$name] = $value;
-	}
-
-	/**
-	 * Get the value of a hidden form field
-	 *
-	 * @param string $name The name of the field.
-	 *
-	 * @return mixed $value The value of the field, either a string or an 
-	 *        array, or null if the field does not exist.
-	 */
-	public function getHiddenField($name) {
-		if (isset($this->hidden_fields[$name]))
-			return $this->hidden_fields[$name];
-
-		return null;
-	}
-
-	/**
-	 * Clear the hidden fields.
-	 */
-	public function clearHiddenFields() {
-		$this->hidden_fields = array();
-	}
-
-	/**
-	 * Add a widget within a new SwatFormField
-	 *
-	 * Convenience function to create a new SwatFormField, add the widget as a
-	 * child of the form field, and then add the formfield to this form.
-	 *
-	 * @param SwatWidget $widget A reference to a widget to add.
-	 * @param string $title The visible title of the form field.
-	 */
-	public function addWithField(SwatWidget $widget, $title) {
-		$field = new SwatFormField();
-		$field->add($widget);
-		$field->title = $title;
-		$this->add($field);
-	}
-
-	/**
-	 * Add a widget within a new SwatDiv
-	 *
-	 * Convenience function to create a new SwatDiv, add the widget as a child
-	 * of the div, and then add the div to this form.
-	 *
-	 * @param SwatWidget $widget A reference to a widget to add.
-	 * @param string $title The class of the HTML div tag.
-	 */
-	public function addWithDiv(SwatWidget $widget, $class) {
-		$field = new SwatDiv();
-		$field->add($widget);
-		$field->class = $class;
-		$this->add($field);
+		return $data;
 	}
 }
 
