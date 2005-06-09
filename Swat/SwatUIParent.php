bcd60fe8bd182f7ee699942a4ee99a5d14628bff
Michael Gauthier <mike@silverorange.com>
2005-06-09 16:35:25 +0000

    Updated documentation.
    svn commit r812

--- Swat/SwatUIParent.php
+++ Swat/SwatUIParent.php
@@ -12,6 +12,10 @@ interface SwatUIParent
 	/**
 	 * Adds a child object to this object
 	 *
+	 * This method is used by {@link SwatUI} when building a widget tree and
+	 * does not need to be called elsewhere. To add a field to a field view,
+	 * use {@link SwatFieldView::appendField()}.
+	 *
 	 * @param SwatObject $child the child object to add to this object.
 	 */
 	public function addChild($child);
