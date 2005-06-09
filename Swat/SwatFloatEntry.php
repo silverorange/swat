06016af6d4ae4a998f64ddb6ca6e28e8009d502c
Michael Gauthier <mike@silverorange.com>
2005-06-09 16:58:32 +0000

    Cleaned up these two files. Added documentation and conformed to new coding styles.
    Added extra integer check to SwatIntegerEntry
    
    
    svn commit r814

--- Swat/SwatFloatEntry.php
+++ Swat/SwatFloatEntry.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('Swat/SwatEntry.php');
+require_once 'Swat/SwatEntry.php';
 
 /**
  * A float entry widget
@@ -9,13 +9,26 @@ require_once('Swat/SwatEntry.php');
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @copyright silverorange 2004
  */
-class SwatFloatEntry extends SwatEntry {
-
-	public function init() {
+class SwatFloatEntry extends SwatEntry
+{
+	/**
+	 * Initializes this widget
+	 *
+	 * Sets the input size to 10 by default.
+	 */
+	public function init()
+	{
 		$this->size = 10;
 	}
-
-	public function process() {
+	
+	/**
+	 * Checks to make sure value is a number
+	 *
+	 * If the value of this widget is not a number then an error message is
+	 * attached to this widget.
+	 */
+	public function process()
+	{
 		parent::process();
 
 		if (is_numeric($this->value))
