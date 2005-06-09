a57c0808a623380b8d5f9c764247312e11c22648
Michael Gauthier <mike@silverorange.com>
2005-06-09 18:55:40 +0000

    Updated documentation for init() method
    svn commit r826

--- Swat/SwatYesNoFlydown.php
+++ Swat/SwatYesNoFlydown.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('Swat/SwatFlydown.php');
+require_once 'Swat/SwatFlydown.php';
 
 /**
  * A flydown (aka combo-box) selection widget for a Yes/No option.
@@ -9,11 +9,16 @@ require_once('Swat/SwatFlydown.php');
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @copyright silverorange 2005
  */
-class SwatYesNoFlydown extends SwatFlydown {
-
+class SwatYesNoFlydown extends SwatFlydown
+{
 	const NO = 1;
 	const YES = 2;
 	
+	/**
+	 * Initializes this yes/no flydown
+	 *
+	 * Sets the options of this flydown to be yes and no.
+	 */
 	public function init() {
 		$this->options = array(self::NO => _S("No"),
 		                       self::YES => _S("Yes"));
@@ -21,19 +26,24 @@ class SwatYesNoFlydown extends SwatFlydown {
 		parent::init();
 	}
 
-	public function getValueAsBoolean() {
+	/**
+	 * Gets the value of this yes/no flywodn as a boolean
+	 *
+	 * If the value is not set, this methods returns null.
+	 *
+	 * @return boolean the value of this yes/no flywdown.
+	 */
+	public function getValueAsBoolean()
+	{
 		switch ($this->value) {
-			case self::NO:
-				return false;
-
-			case self::YES:
-				return true;
-
-			default:
-				return null;
+		case self::NO:
+			return false;
+		case self::YES:
+			return true;
+		default:
+			return null;
 		}
 	}
-
 }
 
 ?>
