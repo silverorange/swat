af55ab997b9193134f7586de455b177baf29e647
Nick Burka <nick@silverorange.com>
2005-06-09 16:34:17 +0000

    Changed value to be part of the data array
    svn commit r811

--- Swat/SwatCheckboxTree.php
+++ Swat/SwatCheckboxTree.php
@@ -1,6 +1,7 @@
 <?php
-require_once('Swat/SwatCheckboxArray.php');
-require_once('Swat/SwatState.php');
+
+require_once 'Swat/SwatCheckboxList.php';
+require_once 'Swat/SwatState.php';
 
 /**
  * A checkbox array widget formatted into a tree
@@ -9,7 +10,7 @@ require_once('Swat/SwatState.php');
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @copyright silverorange 2004
  */
-class SwatCheckboxTree extends SwatControl implements SwatState {
+class SwatCheckboxTree extends SwatCheckboxList implements SwatState {
 
 	/**
 	 * Checkbox tree structure
@@ -39,41 +40,47 @@ class SwatCheckboxTree extends SwatControl implements SwatState {
 		$input_tag = new SwatHtmlTag('input');
 		$input_tag->type = 'checkbox';
 		$input_tag->name = $this->id.'[]';
-		
+	
 		foreach ($this->options as $key => $data) {
 			$key_array = explode('/',$key);
 		
 			$div_tag->style='margin-left: '.((count($key_array) - 1)).'em;';
-		
-			$input_tag->id = $key;
-			$input_tag->value = $key;
-			
-			if (in_array($this->values, $this->value))
-				$input_tag->checked = 'checked';
-
-			$label_tag->for = $key;
-			$label_tag->open();
-
 			$div_tag->open();
+			
+			if (isset($data['value'])) {
+				$input_tag->id = $key;
+				$input_tag->value = $data['value'];
+			
+				if (in_array($data['value'], $this->values))
+					$input_tag->checked = 'checked';
+
+				$label_tag->for = $key;
+
 				$input_tag->display();
 			
 				$label_tag->open();
 					echo $data['title'];
 				$label_tag->close();
+			} else {
+				echo $data['title'];
+			}
+			
 			$div_tag->close();
 		}
 	}
 
 	public function process() {
-		$this->path = explode('/',$this->value);
-		$this->value = $this->path[count($this->path)-1];
+		parent::process();
+
+		//$this->path = explode('/',$this->value);
+		//$this->value = $this->path[count($this->path)-1];
 	}
 
-	public setState($state) {
+	public function setState($state) {
 		$this->value = $state;
 	}
 	
-	public getState() {
+	public function getState() {
 		return $this->value;
 	}
 }
