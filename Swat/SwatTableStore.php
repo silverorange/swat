c9f13cb544f349d053ab3e31d8acc5c20ef7dcf6
Michael Gauthier <mike@silverorange.com>
2005-06-12 15:13:22 +0000

    Added documentation to these classes
    svn commit r853

--- Swat/SwatTableStore.php
+++ Swat/SwatTableStore.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once('Swat/SwatObject.php');
-require_once('Swat/SwatTableModel.php');
+require_once 'Swat/SwatObject.php';
+require_once 'Swat/SwatTableModel.php';
 
 /**
  * A data structure that can be used with the SwatTableView
@@ -10,23 +10,47 @@ require_once('Swat/SwatTableModel.php');
  * @copyright 2004-2005 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-class SwatTableStore extends SwatObject implements SwatTableModel {
-
+class SwatTableStore extends SwatObject implements SwatTableModel
+{
+	/**
+	 * The indvidual rows for this data structure
+	 *
+	 * @var array
+	 */
 	private $rows = array();
 
-	public function __construct() {
-
-	}
-
-	public function getRowCount() {
+	/**
+	 * Gets the number of rows in this data structure
+	 *
+	 * @see SwatTableModel::getRowCount()
+	 */
+	public function getRowCount()
+	{
 		return count($this->rows);
 	}
 
-	public function &getRows() {
+	/**
+	 * Gets the rows of this data structure as an array
+	 *
+	 * @return array the rows of this data structure
+	 *
+	 * @see SwatTableModel::getRows()
+	 */
+	public function &getRows()
+	{
 		return $this->rows;
 	}
 
-	public function addRow($data, $id = null) {
+	/**
+	 * Adds a row to this data structure
+	 *
+	 * @param $data the data of the row to add.
+	 * @param $id an optional uniqueid of the row to add.
+	 *
+	 * @see SwatTableModel::addRow()
+	 */
+	public function addRow($data, $id = null)
+	{
 		if ($id === null)
 			$this->rows[] = $data;
 		else
