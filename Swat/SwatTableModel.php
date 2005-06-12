8b99710ec7fdd8f5f457fac0d745fb12890009e7
Michael Gauthier <mike@silverorange.com>
2005-06-12 15:13:22 +0000

    Added documentation to these classes
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@853 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/SwatTableModel.php
+++ Swat/SwatTableModel.php
@@ -9,14 +9,29 @@ require_once 'Swat/SwatObject.php';
  * @copyright 2004-2005 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-interface SwatTableModel {
-
+interface SwatTableModel
+{
+	/**
+	 * Gets all rows of this tabel model as an array
+	 *
+	 * @return array the rows of this table model.
+	 */
 	public function &getRows();
 
+	/**
+	 * Gets the number of rows in this table model
+	 *
+	 * @return integer the number of rows in this table model.
+	 */
 	public function getRowCount();
 
+	/**
+	 * Adds a row to this table data model
+	 *
+	 * @param mixed $data the data for the row to add.
+	 * @param string $id a unique id for this row.
+	 */
 	public function addRow($data, $id = null);
-
 }
 
 ?>
