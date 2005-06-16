cbfbd25e8e363362f8752eb7116940a2a1371955
Michael Gauthier <mike@silverorange.com>
2005-06-16 19:52:33 +0000

    Changed the way property_map works for cell renderers so there is nolonger a dynamic undocumentable class property being set. Renderers are now added to the internal array with a property map as well. All renderers must have an id as renderers are indexed by id.
    This may need to be revised.
    
    Also, added documentation.
    
    
    svn commit r865

--- Swat/SwatDetailsViewField.php
+++ Swat/SwatDetailsViewField.php
@@ -4,8 +4,6 @@ require_once 'Swat/SwatObject.php';
 require_once 'Swat/SwatHtmlTag.php';
 require_once 'Swat/SwatUIParent.php';
 
-// TODO: Document this class
-
 /**
  * A visible field in a SwatDetailsView
  *
@@ -36,8 +34,20 @@ class SwatDetailsViewField extends SwatObject implements SwatUIParent
 	 */
 	public $view = null;
 
+	/**
+	 * An array of renderers for this field of the form:
+	 *    [$id] => array('renderer' => $renderer, 'property_map' => array())
+	 *
+	 * @var array
+	 */
 	protected $renderers = array();
 
+	/**
+	 * Creates a new details view field
+	 *
+	 * @param string $id an optional unique ideitifier for this details view
+	 *                    field.
+	 */
 	public function __construct($id = null)
 	{
 		$this->id = $id;
@@ -46,32 +56,50 @@ class SwatDetailsViewField extends SwatObject implements SwatUIParent
 	/**
 	 * Links a data field to a cell renderer property
 	 *
-	 * @param SwatCellRenderer $renderer a reference to the cell renderer
-	 *                                    that the data field is mapped to.
-	 * @param string $model_field the field of the data model to map to the
-	 *                             renderer property.
-	 * @param string $render_property the property of the cell renderer that
-	 *                                 data field is mapped to.
+	 * @param string $renderer_id the unique id of the cell renderer
+	 *                             that the data field is mapped to.
+	 * @param string $field the field of the data model to map to the
+	 *                       renderer property.
+	 * @param string $property the property of the cell renderer that
+	 *                          data field is mapped to.
+	 *
+	 * @throws SwatException
 	 */
-	public function linkField($renderer, $model_field, $renderer_property)
+	public function linkField($renderer_id, $field, $property)
 	{
-		if (!isset($renderer->_property_map) ||
-			!is_array($renderer->_property_map))
-				$renderer->_property_map = array();
-
-		$renderer->_property_map[$renderer_property] = $model_field;
+		if (isset($this->renderers[$renderer_id]))
+			$this->renderers[$renderer_id]['property_map'][$property] =
+				$field;
+		else
+			throw new SwatException(sprintf(__CLASS__.': renderer with an id '.
+				"of '%s' does not exist in this details view field.",
+				$renderer_id));
 	}
 
+	/**
+	 * Adds a cell renderer to this field
+	 *
+	 * @param SwatCellRenderer $renderer a reference to the renderer to add.
+	 *
+	 * @throws SwatException
+	 */
 	public function addRenderer(SwatCellRenderer $renderer)
 	{
-		$this->renderers[] = $renderer;
+		if ($render->id !== null)
+			$this->renderers[$renderer->id] = array('renderer' => $renderer,
+				'property_map' => array());
+		else
+			throw new SwatException(__CLASS__.': Cannot add a cell renderer '.
+				'without an id.');
+		
 		$renderer->parent = $this;
-
-		if (!isset($renderer->_property_map) ||
-			!is_array($renderer->_property_map))
-				$renderer->_property_map = array();
 	}
 
+	/**
+	 * Displays this details view field
+	 *
+	 * @param Object $data the data object to display in this field.
+	 */
 	public function display($data)
 	{
 		$tr_tag = new SwatHtmlTag('tr');
@@ -81,6 +109,9 @@ class SwatDetailsViewField extends SwatObject implements SwatUIParent
 		$tr_tag->close();
 	}
 
+	/**
+	 * Displays the header for this details view field
+	 */
 	public function displayHeader()
 	{
 		$th_tag = new SwatHtmlTag('th');
@@ -89,6 +120,11 @@ class SwatDetailsViewField extends SwatObject implements SwatUIParent
 		$th_tag->display();
 	}
 
+	/**
+	 * Displays the value for this details view field
+	 *
+	 * @param Object $data the data object to display in this field.
+	 */
 	public function displayValue($data)
 	{
 		if (count($this->renderers) == 0)
@@ -96,23 +132,31 @@ class SwatDetailsViewField extends SwatObject implements SwatUIParent
 				': no renderer has been provided.');
 
 		// Set the properties of the renderers to the value of the data field.
-		foreach ($this->renderers as $renderer)
-			foreach ($renderer->_property_map as $property => $field)
-				$renderer->$property = $data->$field;
+		foreach ($this->renderers as $renderer_id => $field_renderer)
+			foreach ($field_renderer['property_map'] as $property => $field)
+				$field_renderer['renderer']->$property = $data->$field;
 
 		$this->displayRenderers($data);
 	}
 
+	/**
+	 * Renders each cell renderer in this details view field
+	 *
+	 * @param Object $data the data object to render with the cell renderers
+	 *                      in this field.
+	 */
 	protected function displayRenderers($data)
 	{
 		reset($this->renderers);
-		$first_renderer = current($this->renderers);
+		$first_field_renderer = current($this->renderers);
+		$first->renderer = $first_field_renderer['renderer'];
 		$td_tag = new SwatHtmlTag('td', $first_renderer->getTdAttributes());
 		$td_tag->open();
 
 		$prefix = ($this->view->id === null) ? '' : $this->view->id.'_';
 
-		foreach ($this->renderers as $renderer) {
+		foreach ($this->renderers as $id => $field_renderer) {
+			$renderer = $field_renderer['renderer'];
 			$renderer->render($prefix);
 			echo ' ';
 		}
@@ -123,7 +167,7 @@ class SwatDetailsViewField extends SwatObject implements SwatUIParent
 	/**
 	 * Adds a child object to this object
 	 * 
-	 * @param $child A reference to a child object to add.
+	 * @param SwatObject $child the reference to the child object to add.
 	 *
 	 * @throws SwatException
 	 *
