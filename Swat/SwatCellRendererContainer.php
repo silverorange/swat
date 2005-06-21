b4dc724da0ee50a266a85c6e39242dcb3750e8b3
Michael Gauthier <mike@silverorange.com>
2005-06-21 01:42:23 +0000

    Separate out mapping specific stuff into its own interface that extends the container interface.
    svn commit r902

--- Swat/SwatCellRendererContainer.php
+++ Swat/SwatCellRendererContainer.php
@@ -4,13 +4,19 @@
  * All containers of cell renders must implement this interface
  *
  * @package   Swat
- * @copyright 2004-2005 silverorange
+ * @copyright 2005 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
- *
- * @see SwatDetailsViewField, SwatTableViewColumn
  */
 interface SwatCellRendererContainer
 {
+	/**
+	 * Adds a cell renderer to this container
+	 *
+	 * @param SwatCellRenderer $renderer a reference to the cell renderer to
+	 *                                    add to this container.
+	 */
+	public function addRenderer($renderer);
+
 	/**
 	 * Gets all cell renderers in this container as a flat array
 	 *
@@ -27,48 +33,6 @@ interface SwatCellRendererContainer
 	 * @return SwatCellRenderer a reference to the cell renderer.
 	 */
 	public function getRenderer($renderer_id);
-
-	/**
-	 * Adds a cell renderer to this container with a data-field property
-	 * mapping array
-	 *
-	 * The data-field property mapping array is of the form:
-	 *    array($renderer_property => $field_name);
-	 *
-	 * @param SwatCellRenderer $renderer a reference to the cell renderer to
-	 *                                    add to this container.
-	 * @param array $fields the data-field property mappings to add to the cell
-	 *                       renderer.
-	 */
-	public function addRendererWithMappings(SwatCellRenderer $renderer,
-		$mappings = null);
-
-	/**
-	 * Adds data-field property mappings to a cell renderer in this container
-	 *
-	 * The data-field property mapping array is of the form:
-	 *    array($renderer_property => $field_name);
-	 *
-	 * @param string $renderer_id the unique identifier of the cell renderer to
-	 *                             add data-field property mappings to.
-	 * @param array $fields the data-field property mappings to add to the cell
-	 *                       renderer.
-	 */
-	public function addRendererMappings($renderer_id, $mappings = null);
-
-	/**
-	 * Sets the data-field property mappings for a cell renderer in this
-	 * container
-	 *
-	 * The data-field property mapping array is of the form:
-	 *    array($renderer_property => $field_name);
-	 *
-	 * @param string $renderer_id the unique identifier of the cell renderer to
-	 *                             add data-field property mappings to.
-	 * @param array $fields the data-field property mappings to add to the cell
-	 *                       renderer.
-	 */
-	public function setRendererMappings($renderer_id, $mappings = null);
 }
 
 ?>
---
+++ Swat/SwatMappingCellRendererContainer.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once 'Swat/SwatCellRendererContainer.php';
+
+/**
+ * All containers of cell renders that do data-field-to-renderer-property
+ * mapping must implement this interface
+ *
+ * @package   Swat
+ * @copyright 2005 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ *
+ * @see SwatCellRendererContainer
+ */
+interface SwatMappingCellRendererContainer extends SwatCellRendererContainer
+{
+	/**
+	 * Adds a cell renderer to this container with a data-field property
+	 * mapping array
+	 *
+	 * The data-field property mapping array is of the form:
+	 *    array($renderer_property => $field_name);
+	 *
+	 * @param SwatCellRenderer $renderer a reference to the cell renderer to
+	 *                                    add to this container.
+	 * @param array $fields the data-field property mappings to add to the cell
+	 *                       renderer.
+	 */
+	public function addRendererWithMappings(SwatCellRenderer $renderer,
+		$mappings = null);
+
+	/**
+	 * Adds data-field property mappings to a cell renderer in this container
+	 *
+	 * The data-field property mapping array is of the form:
+	 *    array($renderer_property => $field_name);
+	 *
+	 * @param string $renderer_id the unique identifier of the cell renderer to
+	 *                             add data-field property mappings to.
+	 * @param array $fields the data-field property mappings to add to the cell
+	 *                       renderer.
+	 */
+	public function addRendererMappings($renderer_id, $mappings = null);
+
+	/**
+	 * Sets the data-field property mappings for a cell renderer in this
+	 * container
+	 *
+	 * The data-field property mapping array is of the form:
+	 *    array($renderer_property => $field_name);
+	 *
+	 * @param string $renderer_id the unique identifier of the cell renderer to
+	 *                             add data-field property mappings to.
+	 * @param array $fields the data-field property mappings to add to the cell
+	 *                       renderer.
+	 */
+	public function setRendererMappings($renderer_id, $mappings = null);
+}
+
+?>
