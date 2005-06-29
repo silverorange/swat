abcb12767a8bdfccac85fa68cccd2f0b7eb59ec3
Michael Gauthier <mike@silverorange.com>
2005-06-29 18:10:23 +0000

    Coding cleanup: - added vim expanders - added docblocks - updated docblocks - no parentheses on require statements - new brace style
    svn commit r964

--- Swat/SwatTableViewGroup.php
+++ Swat/SwatTableViewGroup.php
@@ -1,32 +1,69 @@
 <?php
 
-require_once('Swat/SwatTableViewColumn.php');
-require_once('Swat/SwatHtmlTag.php');
+require_once 'Swat/SwatTableViewColumn.php';
+require_once 'Swat/SwatHtmlTag.php';
 
 /**
- * A visible grouping of rows in a SwatTableView
+ * A visible grouping of rows in a table view
+ *
+ * This is a table view column that gets its own row. It usually makes sense
+ * to place it before other table view columns as it is always displayed on a
+ * row by itself and never mixed with other columns. This special column is
+ * only displayed when the value of the group_by field changes; it is not
+ * displayed once for every row.
  *
  * @package   Swat
  * @copyright 2004-2005 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-class SwatTableViewGroup extends SwatTableViewColumn {
+class SwatTableViewGroup extends SwatTableViewColumn
+{
+	// {{{ public properties
 
 	/**
 	 * The field of the table store to group rows by
+	 *
 	 * @var string
 	 */
 	public $group_by = null;
 
+	// }}}
+	// {{{ private properties
+
+	/**
+	 * The current value of the group_by field of the table store
+	 *
+	 * This value is used so that this column is not displayed for every row
+	 * and is only displayed when the value of the table store changes.
+	 *
+	 * @var mixed
+	 */
 	private $current = null;
 
-	protected function displayRenderers($row) {
+	// }}}
+	// {{{ protected function displayRenderers()
+	
+	/**
+	 * Displays the renderers for this column
+	 *
+	 * The renderes are only displayed once for every time the value of the
+	 * group_by field changes and the renderers are displayed on their own
+	 * separate table row.
+	 *
+	 * @param Object $row a data object containing the data for a single row
+	 *                     in the table store for this group.
+	 *
+	 * @throws SwatException
+	 */
+	protected function displayRenderers($row)
+	{
 
 		if ($this->group_by === null)
 			throw new SwatException(__CLASS__.': group_by attribute not set');
 
 		$group_by = $this->group_by;
 
+		// only display this column if the value of the row has changed
 		if ($row->$group_by == $this->current)
 			return;
 
@@ -35,14 +72,13 @@ class SwatTableViewGroup extends SwatTableViewColumn {
 		$tr_tag = new SwatHtmlTag('tr');
 		$tr_tag->open();
 
-		reset($this->renderers);
-		$first_renderer = current($this->renderers);
+		$first_renderer = reset($this->renderers);
 		$td_tag = new SwatHtmlTag('td', $first_renderer->getTdAttributes());
 		$td_tag->colspan = $this->view->getColumnCount();
 		$td_tag->class = 'swat-table-view-group';
 		$td_tag->open();
 
-		$prefix = ($this->view === null)? '': $this->view->id.'_';
+		$prefix = ($this->view === null) ? '' : $this->view->id.'_';
 
 		foreach ($this->renderers as $renderer) {
 			$renderer->render($prefix);
@@ -50,8 +86,11 @@ class SwatTableViewGroup extends SwatTableViewColumn {
 		}
 
 		$td_tag->close();
+		
 		$tr_tag->close();
 	}
+
+	// }}}
 }
 
 ?>
