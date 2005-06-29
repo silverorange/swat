151fec1f9eb3a9ab2822af363a7f285a4056fde0
Michael Gauthier <mike@silverorange.com>
2005-06-29 18:18:16 +0000

    update copyright to reflect the correct years
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@966 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/SwatStyle.php
+++ Swat/SwatStyle.php
@@ -6,7 +6,7 @@ require_once 'Swat/SwatObject.php';
  * Style class for widgets
  *
  * @package   Swat
- * @copyright 2004-2005 silverorange
+ * @copyright 2005 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class SwatStyle extends SwatObject
