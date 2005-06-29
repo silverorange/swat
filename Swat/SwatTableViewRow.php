87ac6e671a1b642be40f1fdca1d6c6e78f4c916a
Michael Gauthier <mike@silverorange.com>
2005-06-29 18:17:02 +0000

    Coding cleanup: - added and updated docblocks - added vim folding markers - new brace style - remove parentheses on require statements
    svn commit r965

--- Swat/SwatTableViewRow.php
+++ Swat/SwatTableViewRow.php
@@ -1,24 +1,49 @@
 <?php
-require_once('Swat/SwatObject.php');
+
+require_once 'Swat/SwatObject.php';
 
 /**
- * Base class for a extra row displayed that the bottom of a SwatTableView
+ * Base class for a extra row displayed at the bottom of a table view
  *
  * @package   Swat
  * @copyright 2004-2005 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-abstract class SwatTableViewRow extends SwatObject {
+abstract class SwatTableViewRow extends SwatObject
+{
+	// {{{ public properties
 
 	/**
 	 * The {@link SwatTableView} associated with this row
+	 *
 	 * @var SwatTableView
 	 */
 	public $view = null;
 
+	// }}}
+	// {{{ public function display()
+	
+	/**
+	 * Displays this row
+	 *
+	 * @param array $columns an array of columns to render in this row.
+	 */
 	public abstract function display(&$columns);
 
-	public function init() {
-
+	// }}}
+	// {{{ public function init()
+	
+	/**
+	 * Initializes this row
+	 *
+	 * This method does nothing and is implemented here so subclasses do not
+	 * need to implement it.
+	 */
+	public function init()
+	{
 	}
+
+	// }}}	
 }
+
+?>
