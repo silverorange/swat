306466a42afe62b723d1e709aebac73d894dff19
Michael Gauthier <mike@silverorange.com>
2005-07-25 19:02:08 +0000

    Implemented generatePropertyHashes() and added isModified().
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@1166 94490950-a86c-4493-9ccb-530f72933e47

--- SwatDB/SwatDBDataObject.php
+++ SwatDB/SwatDBDataObject.php
@@ -24,7 +24,6 @@ class SwatDBDataObject
 	 */
 	public function initWrapper($rs);
 	{
-
 	}
 
 	/**
@@ -32,6 +31,28 @@ class SwatDBDataObject
 	 */
 	private function generatePropertyHashes();
 	{
+		$property_array = get_object_vars($this);
+
+		foreach ($property_array as $name => $value) {
+			$hashed_value = md5(serialize($value));
+			$this->property_hashes[$name] = $hashed_value;
+		}
+	}
+
+	/**
+	 *
+	 */
+	public function isModified()
+	{
+		$property_array = get_object_vars($this);
+
+		foreach ($property_array as $name => $value) {
+			$hashed_value = md5(serialize($value));
+			if (strcmp($hashed_value, $this->property_hashes[$name]) != 0)
+				return false;
+		}
+		
+		return true;
 	}
 }
 
