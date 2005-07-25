c223eb12fd12ee81c9552d18ecd13769d7c3b728
Michael Gauthier <mike@silverorange.com>
2005-07-25 19:16:20 +0000

    Implemented a generic initWrapper() and added documentation.
    svn commit r1167

--- SwatDB/SwatDBDataObject.php
+++ SwatDB/SwatDBDataObject.php
@@ -1,7 +1,11 @@
 <?php
 
 /**
+ * All public properties correspond to database fields
  *
+ * @package   SwatDB
+ * @copyright 2005 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class SwatDBDataObject
 {
@@ -15,32 +19,20 @@ class SwatDBDataObject
 	 */
 	public function __construct($rs = null);
 	{
-		if ($rs !== null)
+		if ($rs !== null) {
+			if (MDB2::isError($rs))
+				throw new Exception($rs->getMessage());
+				// TODO: change to StoreException
+
 			$this->initWrapper();
-	}
-
-	/**
-	 *
-	 */
-	public function initWrapper($rs);
-	{
-	}
-
-	/**
-	 *
-	 */
-	private function generatePropertyHashes();
-	{
-		$property_array = get_object_vars($this);
-
-		foreach ($property_array as $name => $value) {
-			$hashed_value = md5(serialize($value));
-			$this->property_hashes[$name] = $hashed_value;
 		}
 	}
 
 	/**
+	 * Returns true if this object has been modified since it was loaded
 	 *
+	 * @return boolean true if this object was modified and false if this
+	 *                  object was not modified.
 	 */
 	public function isModified()
 	{
@@ -53,6 +45,48 @@ class SwatDBDataObject
 		}
 		
 		return true;
+	}
+
+	/**
+	 * Takes a record set and sets the properties of this object according to
+	 * the values of the record set
+	 *
+	 * Subclasses can override this method to provide additional
+	 * functionality.
+	 *
+	 * @param MDB2_RecordSet $rs the record set to use
+	 */
+	protected function initWrapper($rs);
+	{
+
+		if ($rs->numrows() >= 1) {
+			$row = $rs->fetchRow(MDB2_FETCHMODE_ARRAY);
+
+			$property_array = get_object_vars($this);
+
+			foreach ($property_array as $name => $value) {
+				if (isset($row[$name]))
+					$this->$name = $row->name;
+			}
+
+			$this->generatePropertyHashes();
+		}
+	}
+
+	/**
+	 * Generates the set of md5 hashes for this data object
+	 *
+	 * The md5 hashes represent all the public properties of this object and
+	 * are used to tell if a property has been modified.
+	 */
+	private function generatePropertyHashes();
+	{
+		$property_array = get_object_vars($this);
+
+		foreach ($property_array as $name => $value) {
+			$hashed_value = md5(serialize($value));
+			$this->property_hashes[$name] = $hashed_value;
+		}
 	}
 }
 
