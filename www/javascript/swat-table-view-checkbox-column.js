c0ad2fbaa795e8e34cd959bd712c875bbad8e093
Nick Burka <nick@silverorange.com>
2005-08-18 19:13:21 +0000

    Completed change for CheckAll state to up-to-date on page load
    svn commit r1375

--- www/javascript/swat-table-view-checkbox-column.js
+++ www/javascript/swat-table-view-checkbox-column.js
@@ -27,18 +27,22 @@ function SwatTableViewCheckboxColumn(id, table) {
 		var node = (is_ie) ? event.srcElement : event.target;
 		self.highlightRow(node);
 
-		if (self.check_all == null)
-			return;
-
-		var count = 0;
-		for (i = 0; i < self.check_list.length; i++)
-			if (self.check_list[i].checked)
-				count++;
-			else if (count > 0)
-				break; //can't possibly be all checked or none checked
-
-		self.check_all.setState(count == self.check_list.length);
+		self.checkAllInit();
 	}
+}
+
+SwatTableViewCheckboxColumn.prototype.checkAllInit = function() {
+	if (this.check_all == null)
+		return;
+
+	var count = 0;
+	for (i = 0; i < this.check_list.length; i++)
+		if (this.check_list[i].checked)
+			count++;
+		else if (count > 0)
+			break; //can't possibly be all checked or none checked
+
+	this.check_all.setState(count == this.check_list.length);
 }
 
 SwatTableViewCheckboxColumn.prototype.checkAll = function(checked) {
