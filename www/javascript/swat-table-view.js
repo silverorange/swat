8232ce8a5c6d1f0248fd7e217af9b6217307655f
Nick Burka <nick@silverorange.com>
2005-08-18 19:09:04 +0000

    Line spacing
    svn commit r1373

--- www/javascript/swat-table-view.js
+++ www/javascript/swat-table-view.js
@@ -9,12 +9,11 @@ function SwatTableView(id) {
 
 SwatTableView.prototype.highlightRow = function(node, highlight) {
 	if (node.nodeName == 'TR') {
-
 		if (node.className == 'odd' || node.className == 'highlight-odd')
 			node.className = (highlight) ? 'highlight-odd' : 'odd';
 		else
 			node.className = (highlight) ? 'highlight' : '';
-			
+
 	} else if (node.parentNode)
 		this.highlightRow(node.parentNode, highlight);
 }
