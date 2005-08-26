dc42e16dd54fe7df23bf0ba780b2f6e0e7176094
Michael Gauthier <mike@silverorange.com>
2005-08-26 14:56:01 +0000

    - Added class level docblocks and put everything in its own package   SwatDemo to unclutter documentation. - Changed references from "Example" to "Demo"
    svn commit r1494

--- demo/include/pages/ExamplePage.php
+++ demo/include/pages/ExamplePage.php
@@ -2,9 +2,19 @@
 
 require_once 'Swat/SwatPage.php';
 require_once 'Swat/SwatUI.php';
-require_once '../include/ExampleMenu.php';
+require_once '../include/DemoMenu.php';
 
-class ExamplePage extends SwatPage
+/**
+ * A page in the Swat Demo Application
+ *
+ * Demo application pages can set widget properties that are not expressable
+ * in SwatML.
+ *
+ * @package   SwatDemo
+ * @copyright 2005 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class DemoPage extends SwatPage
 {
 	protected $ui = null;
 
@@ -53,7 +63,7 @@ class ExamplePage extends SwatPage
 		$this->layout->ui = ob_get_clean();
 
 		ob_start();
-		$this->menu = new ExampleMenu();
+		$this->menu = new DemoMenu();
 		$this->menu->display();
 		$this->layout->menu = ob_get_clean();
 
