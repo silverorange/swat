502f3ee737c3a2af941e0773a37566f735034899
Nathan Fredrickson <nathan@silverorange.com>
2005-08-26 14:16:13 +0000

    Update the demo
    svn commit r1493

--- demo/include/pages/MessageBox.php
+++ demo/include/pages/MessageBox.php
@@ -9,13 +9,13 @@ class MessageBox extends ExamplePage
 		$message_box = $this->ui->getWidget('message_box');
 
 		$message_box->messages = array(
-			new SwatMessage('This is an informational message.',
+			new SwatMessage('This is a notification message.',
 				SwatMessage::NOTIFICATION),
 			new SwatMessage('This is a warning message.',
 				SwatMessage::WARNING),
-			new SwatMessage('This is a user error message.',
+			new SwatMessage('This is an error message.',
 				SwatMessage::ERROR),
-			new SwatMessage('This is an error message.', SwatMessage::SYSTEM_ERROR)
+			new SwatMessage('This is a system error message.', SwatMessage::SYSTEM_ERROR)
 		);
 	}
 }
