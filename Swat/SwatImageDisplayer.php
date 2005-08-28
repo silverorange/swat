adcd10ed4db51db80cd3fd9472c148af48678d29
Michael Gauthier <mike@silverorange.com>
2005-08-28 20:14:10 +0000

    Rename this class to a noun rather than a verb. See SwatCellRenderer for example.
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@1576 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/SwatImageDisplayer.php
+++ Swat/SwatImageDisplayer.php
@@ -12,7 +12,7 @@ require_once 'Swat/SwatControl.php';
  * @copyright 2004-2005 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-class SwatImageDisplay extends SwatControl
+class SwatImageDisplayer extends SwatControl
 {
 	/**
 	 * Image
