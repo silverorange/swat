d847bc0671361690b05d22e3339ac43410e2ba45
Michael Gauthier <mike@silverorange.com>
2005-09-02 19:15:09 +0000

    Added documentation. required SwatObject.php in SwatHtmlHeadEntry.php
    svn commit r1685

--- Swat/SwatHtmlHeadEntry.php
+++ Swat/SwatHtmlHeadEntry.php
@@ -1,11 +1,43 @@
 <?php
 
+require_once 'Swat/SwatObject.php';
+
+/**
+ * Stores and outputs an HTML head entry
+ *
+ * Head entries are things like scripts and styles that belong in the HTML
+ * head section.
+ *
+ * @package   Swat
+ * @copyright 2004-2005 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
 class SwatHtmlHeadEntry extends SwatObject
 {
+	/**
+	 * A stylesheet head entry
+	 */
 	const TYPE_STYLE = 0;
+
+	/**
+	 * A JavaScript head entry
+	 */
 	const TYPE_JAVASCRIPT = 1;
 	
+	/**
+	 * The type of this head entry
+	 *
+	 * This should be a valid SwatHtmlHeadEntry::TYPE_* constant.
+	 *
+	 * @var integer
+	 */
 	public $type = self::TYPE_STYLE;
+
+	/**
+	 * The uri of this head entry
+	 *
+	 * @var string
+	 */
 	public $uri = ''
 
 	/**
