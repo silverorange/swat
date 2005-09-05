e346053dbe60bfc60f055a977b2f2a6fa87d023a
Michael Gauthier <mike@silverorange.com>
2005-09-05 17:09:25 +0000

    Fix copyright date.
    svn commit r1744

--- Swat/SwatHtmlHeadEntry.php
+++ Swat/SwatHtmlHeadEntry.php
@@ -9,7 +9,7 @@ require_once 'Swat/SwatObject.php';
  * head section.
  *
  * @package   Swat
- * @copyright 2004-2005 silverorange
+ * @copyright 2005 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class SwatHtmlHeadEntry extends SwatObject
