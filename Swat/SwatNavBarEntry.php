c9e73eca15cf446dcff60faf0ac532d7e8cabde1
Michael Gauthier <mike@silverorange.com>
2005-09-05 17:09:04 +0000

    Add documentation.
    svn commit r1743

--- Swat/SwatNavBarEntry.php
+++ Swat/SwatNavBarEntry.php
@@ -6,16 +6,36 @@ require_once 'Swat/SwatObject.php';
  * Entry for the navbar navigation tool
  *
  * @package   Swat
- * @copyright 2004-2005 silverorange
+ * @copyright 2005 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  *
  * @see SwatNavBar
  */
 class SwatNavBarEntry extends SwatObject
 {
+	/**
+	 * The visible title of this entry
+	 *
+	 * @var string
+	 */
 	public $title;
+
+	/**
+	 * The URI that this entry points to
+	 *
+	 * This property is optional. If it is not present this entry will not
+	 * display as a hyperlink.
+	 *
+	 * @var string
+	 */
 	public $uri;
 
+	/**
+	 * Creates a new navbar entry
+	 *
+	 * @param string $title the title of this entry.
+	 * @param string $uri the URI this entry points to.
+	 */
 	public function __construct($title, $uri = null)
 	{
 		$this->title = $title;
