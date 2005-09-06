30a58826f96f8180bf089fd500c6dacab78e2b0d
Steven Garrity <steven@silverorange.com>
2005-09-06 19:46:46 +0000

    move the footer
    svn commit r1794

--- demo/layouts/footer.php
+++ demo/layouts/footer.php
@@ -1,9 +1,7 @@
 </div>
-<div id="footer">
-PHP Execution Time: <?=$this->execution_time?> seconds
-</div>
 <div id="menu">
 	<?=$this->menu?>
+	<div id="footer">PHP Execution Time: <?=$this->execution_time?> seconds</div>
 </div>
 </body>
 </html>
