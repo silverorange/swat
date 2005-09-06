ea76953b5444b96e315a0895f08cb5917c390b83
Michael Gauthier <mike@silverorange.com>
2005-09-06 03:45:31 +0000

    Change references from "example" to "demo".
    svn commit r1775

--- demo/www/index.php
+++ demo/www/index.php
@@ -6,8 +6,8 @@ $work_dir = $uri_array[3];
 ini_set('include_path', ".:/so/packages/swat/{$work_dir}:/usr/lib/php");
 require_once '../include/DemoApplication.php';
 
-$app = new DemoApplication('example');
-$app->title = 'Swat Widget Gallery';
+$app = new DemoApplication('demo');
+$app->title = 'Swat Demo Site';
 $app->init();
 
 $page = $app->getPage();
