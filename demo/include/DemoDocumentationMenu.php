b7f39359d00ceb3204961c7b829244096dd43682
Michael Gauthier <mike@silverorange.com>
2005-09-15 16:46:38 +0000

    Move documentation links out of th eXML and into their own menu.
    svn commit r1955

---
+++ demo/include/DemoDocumentationMenu.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once 'DemoMenu.php';
+
+/**
+ * The menu for the Swat Demo Application
+ *
+ * This is a simple menu that takes a flat array of titles and links and
+ * displays them in an unordered list.
+ *
+ * @package   SwatDemo
+ * @copyright 2005 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class DemoDocumentationMenu extends DemoMenu
+{
+	public function __construct($entries)
+	{
+		$this->entries = $entries;
+	}
+
+	public function display()
+	{
+		echo '<div id="documentation-menu"><span class="menutitle">Documentation Links</span>:<ul>';
+
+		foreach ($this->entries as $class) {
+			echo '<li><a href="http://docs.silverorange.com/Swat/'.$class.'.html">'.$class.'</a></li>';
+		}
+
+		echo '</ul></div>';
+	}
+}
+
+?>
--- demo/layouts/footer.php
+++ demo/layouts/footer.php
@@ -1,6 +1,7 @@
 </div>
 <div id="menu">
 	<?=$this->menu?>
+	<?=$this->documentation_menu?>
 	<div id="footer">PHP Execution Time: <?=$this->execution_time?> seconds</div>
 </div>
 </body>
