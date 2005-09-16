ce12be4ce80451c6a99d965edf9acb6e3692ea6b
Michael Gauthier <mike@silverorange.com>
2005-09-16 04:49:57 +0000

    Added documentation link for SwatImageCellRenderer
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@1972 94490950-a86c-4493-9ccb-530f72933e47

--- demo/include/pages/DemoPage.php
+++ demo/include/pages/DemoPage.php
@@ -201,6 +201,7 @@ class DemoPage extends SwatPage
 				'SwatCheckboxCellRenderer',
 				'SwatCheckCellRenderer',
 				'SwatDateCellRenderer',
+				'SwatImageCellRenderer',
 				'SwatMoneyCellRenderer',
 				'SwatTextCellRenderer',
 				'SwatActions',
