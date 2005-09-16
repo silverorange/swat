95dd5166b5f4307d42b5fe5b707f20ae27e9bb8c
Steven Garrity <steven@silverorange.com>
2005-09-16 23:25:49 +0000

    Move documentation links to a comma-delimited list
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@1978 94490950-a86c-4493-9ccb-530f72933e47

--- demo/layouts/default.php
+++ demo/layouts/default.php
@@ -1,5 +1,6 @@
 <? include 'header.php'; ?>
 <?=$this->ui?>
+<?=$this->documentation_menu?>
 <div id="source_code">
 	<h3 class="source-title">SwatML for this example:</h3>
 	<pre><?=$this->source_code?></pre>
