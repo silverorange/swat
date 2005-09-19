8f5d1a1d1df81f556e460b97b215053b5abd78b7
Michael Gauthier <mike@silverorange.com>
2005-09-19 17:51:34 +0000

    Change base element from swat-interface to swatml.
    svn commit r1991

--- Swat/SwatUI.php
+++ Swat/SwatUI.php
@@ -132,7 +132,7 @@ class SwatUI extends SwatObject
 
 		// make sure we are using the correct document type
 		if ($document->doctype === null ||
-			strcmp($document->doctype->name, 'swat-interface') != 0) {
+			strcmp($document->doctype->name, 'swatml') != 0) {
 			throw new SwatException('SwatUI can only parse SwatML documents.');
 		}
 
