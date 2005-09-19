da148663966e723ccc19164e4683eb9557b9ca74
Michael Gauthier <mike@silverorange.com>
2005-09-19 20:35:53 +0000

    Renamed ControlCellRenderer as ImageLinkCellRenderer and made it extend ImageCellRenderer.
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@2004 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Swat/SwatImageLinkCellRenderer.php
@@ -0,0 +1,70 @@
+<?php
+
+require_once 'Swat/SwatImageCellRenderer.php';
+
+/**
+ * A renderer that displays a hyperlinked image
+ *
+ * @package   Swat
+ * @copyright 2004-2005 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SwatImageLinkCellRenderer extends SwatImageCellRenderer
+{
+	/**
+	 * Link href
+	 *
+	 * The href attribute in the XHTML anchor tag.
+	 *
+	 * The link may include a sprintf substitution tag. For example:
+	 * <code>
+	 * $renderer->link = 'MySection/MyPage?id=%s';
+	 * </code>
+	 *
+	 * @var string
+	 */
+	public $link;
+
+	/**
+	 * Link value
+	 *
+	 * A value to substitute into the link.
+	 *
+	 * @var string
+	 */
+	public $value = null;
+
+	/**
+	 * Renders the contents of this cell
+	 *
+	 * @see SwatCellRenderer::render()
+	 */
+	public function render()
+	{
+		$anchor = new SwatHtmlTag('a');
+
+		if ($this->value === null)
+			$anchor->href = $this->link;
+		else
+			$anchor->href = sprintf($this->link, $this->value);
+
+		$anchor->open();
+		parent::render();
+		$anchor->close();
+	}
+
+	/**
+	 * Gets TD-tag attributes
+	 *
+	 * @return array an array of attributes to apply to the TD tag of this cell
+	 *                renderer.
+	 *
+	 * @see SwatCellRenderer::getTdAttributes()
+	 */
+	public function &getTdAttributes()
+	{
+		return array('class' => 'swat-image-link-cell-renderer');
+	}
+}
+
+?>
