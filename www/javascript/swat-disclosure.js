7187497e378cbc7996a11b160b3f4736a0f9e51c
Michael Gauthier <mike@silverorange.com>
2005-09-22 02:26:53 +0000

    Split strings out of functions and into static properties. This allows Swat to translate the strings. See the calendar for another example of this.
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@2046 94490950-a86c-4493-9ccb-530f72933e47

--- www/javascript/swat-disclosure.js
+++ www/javascript/swat-disclosure.js
@@ -11,16 +11,19 @@ function SwatDisclosure(id)
 	}
 }
 
+SwatDisclosure.open_text = 'open';
+SwatDisclosure.close_text = 'close';
+
 SwatDisclosure.prototype.toggle = function()
 {
 	if (this.opened) {
 		this.div.className = 'swat-disclosure-container-closed';
 		this.image.src = 'swat/images/disclosure-closed.png';
-		this.image.alt = 'open';
+		this.image.alt = SwatDisclosure.open_text;
 	} else {
 		this.div.className = 'swat-disclosure-container-opened';
 		this.image.src = 'swat/images/disclosure-open.png';
-		this.image.alt = 'close';
+		this.image.alt = SwatDisclosure.close_text;
 	}
 	this.opened = !this.opened;
 }
