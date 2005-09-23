1abfc1a504fbbb07898b7d946ab7727f0730fb19
Michael Gauthier <mike@silverorange.com>
2005-09-23 19:14:15 +0000

    Added SwatExpandableCheckboxTree.
    svn commit r2062

--- demo/include/pages/Checkbox.php
+++ demo/include/pages/Checkbox.php
@@ -43,6 +43,33 @@ class Checkbox extends DemoPage
 
 		$checkbox_tree = $this->ui->getWidget('checkbox_tree');
 		$checkbox_tree->tree = $tree;
+
+		$tree = new SwatTreeNode(array('title' => 'test'));
+
+		$apples = new SwatTreeNode(array('title' => 'Apple', 'value' => 12));
+		$apples->addChild(new SwatTreeNode(array('title' => 'Mackintosh', 'value' => 0)));
+		$apples->addChild(new SwatTreeNode(array('title' => 'Courtland', 'value' => 1)));
+		$apples->addChild(new SwatTreeNode(array('title' => 'Golden Delicious', 'value' => 2)));
+		$apples->addChild(new SwatTreeNode(array('title' => 'Fuji', 'value' => 3)));
+		$apples->addChild(new SwatTreeNode(array('title' => 'Granny Smith', 'value' => 4)));
+		
+		$oranges = new SwatTreeNode(array('title' => 'Orange', 'value' => 13));
+		$oranges->addChild(new SwatTreeNode(array('title' => 'Navel', 'value' => 5)));
+
+		$blood_oranges = new SwatTreeNode(array('title' => 'Blood', 'value' => 14));
+		$blood_oranges->addChild(new SwatTreeNode(array('title' => 'Doble Fina', 'value' => 6)));
+		$blood_oranges->addChild(new SwatTreeNode(array('title' => 'Entrefina', 'value' => 7)));
+		$blood_oranges->addChild(new SwatTreeNode(array('title' => 'Sanguinelli', 'value' => 8)));
+		$oranges->addChild($blood_oranges);
+
+		$oranges->addChild(new SwatTreeNode(array('title' => 'Florida', 'value' => 9)));
+		$oranges->addChild(new SwatTreeNode(array('title' => 'California', 'value' => 10)));
+		$oranges->addChild(new SwatTreeNode(array('title' => 'Mandarin', 'value' => 11)));
+		
+		$tree->addChild($apples);
+		$tree->addChild($oranges);
+		$expandable_checkbox_tree = $this->ui->getWidget('expandable_checkbox_tree');
+		$expandable_checkbox_tree->tree = $tree;
 	}
 }
 
