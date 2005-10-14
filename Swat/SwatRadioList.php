2585d43e93cac838b4be2ffba7dd68473c48e4f1
Isaac Grant <isaac@silverorange.com>
2005-10-14 23:26:13 +0000

    update to make it a subclass of SwatFlydown, to allow for things like addOptionsByArray
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@2250 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/SwatRadioList.php
+++ Swat/SwatRadioList.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'Swat/SwatControl.php';
+require_once 'Swat/SwatFlydown.php';
 require_once 'Swat/SwatHtmlTag.php';
 require_once 'Swat/SwatState.php';
 
@@ -11,36 +11,8 @@ require_once 'Swat/SwatState.php';
  * @copyright 2004-2005 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-class SwatRadioList extends SwatControl implements SwatState
+class SwatRadioList extends SwatFlydown implements SwatState
 {
-	/**
-	 * Radio list options
-	 *
-	 * An array of options for the radio list in the form:
-	 *    value => title.
-	 *
-	 * @var array
-	 */
-	public $options = null;
-
-	/**
-	 * List value 
-	 *
-	 * The value of the selected item, or null.
-	 *
-	 * @var string
-	 */
-	public $value = null;
-
-	/**
-	 * On change
-	 *
-	 * The onchange attribute of the XHTML input type=radio tags, or null.
-	 *
-	 * @var string
-	 */
-	public $onchange = null;
-
 	/**
 	 * Displays this radio list
 	 */
@@ -49,82 +21,48 @@ class SwatRadioList extends SwatControl implements SwatState
 		if (!$this->visible)
 			return;
 
+		$options = $this->getOptions();
+		
+		// Empty string XHTML option value is assumed to be null
+		// when processing.
+		if ($this->show_blank)
+			$options = array_merge(
+				array(new SwatFlydownOption(0, $this->blank_title)),
+				$options);
+		
 		$input_tag = new SwatHtmlTag('input');
 		$input_tag->type = 'radio';
 		$input_tag->name = $this->id;
+		
 		if ($this->onchange !== null)
 			$input_tag->onchange = $this->onchange;
 
 		$label_tag = new SwatHtmlTag('label');
 		$label_tag->class = 'swat-control';
 
-		if ($this->options !== null) {
-			foreach ($this->options as $value => $title) {
-
-				$input_tag->value = (string)$value;
-				$input_tag->removeAttribute('checked');
-
-				if ((string)$this->value === (string)$value)
-					$input_tag->checked = "checked";
-
-				$input_tag->id = $this->id.'_'.$input_tag->value;
-				$input_tag->display();
-
-				$label_tag->for = $this->id.'_'.$input_tag->value;
-				$label_tag->content = $title;
-
-				$label_tag->display();
-
-				echo '<br />';
+		if (count($options)) {
+			foreach ($options as $flydown_option) {	
+				if ($flydown_option instanceof SwatFlydownDivider) {
+					//ignore these for now TODO: make dividers work with radiolists
+				} else {
+					$input_tag->value = (string)$flydown_option->value;
+					$input_tag->removeAttribute('checked');
+	
+					if ((string)$this->value === (string)$flydown_option->value)
+						$input_tag->checked = "checked";
+	
+					$input_tag->id = $this->id.'_'.$input_tag->value;
+					$input_tag->display();
+	
+					$label_tag->for = $this->id.'_'.$input_tag->value;
+					$label_tag->content = $flydown_option->title;
+	
+					$label_tag->display();
+				
+					echo '<br />';
+				}
 			}
 		}
-	}
-
-	/**
-	 * Processes this radio list
-	 */
-	public function process()
-	{
-		if (isset($_POST[$this->id]))
-			$this->value = $_POST[$this->id];
-		else
-			$this->value = null;
-	}
-
-	/**
-	 * Resets this radio list
-	 *
-	 * Resets this list to its default state. This methods is useful to call
-	 * from a display() method when form persistence is not desired.
-	 */
-	public function reset()
-	{
-		reset($this->options);
-		$this->value = key($this->options);
-	}
-
-	/**
-	 * Gets the current state of this radio list
-	 *
-	 * @return boolean the current state of this radio list.
-	 *
-	 * @see SwatState::getState()
-	 */
-	public function getState()
-	{
-		return $this->value;
-	}
-
-	/**
-	 * Sets the current state of this radio list
-	 *
-	 * @param boolean $state the new state of this radio list.
-	 *
-	 * @see SwatState::setState()
-	 */
-	public function setState($state)
-	{
-		$this->value = $state;
 	}
 }
 
