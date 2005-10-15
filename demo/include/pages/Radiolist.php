9555a5ef20bf0f1005be95cee09468696bb3a8c1
Nathan Fredrickson <nathan@silverorange.com>
2005-10-15 03:38:39 +0000

    Update demo to show new SwatRadioList
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@2260 94490950-a86c-4493-9ccb-530f72933e47

---
+++ demo/include/pages/Radiolist.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once 'DemoPage.php';
+require_once 'Swat/SwatTreeNode.php';
+require_once 'Swat/SwatFlydownOption.php';
+
+/**
+ * A demo using a radiolist
+ *
+ * This page sets up the a radiolist.
+ *
+ * @package   SwatDemo
+ * @copyright 2005 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class RadioList extends DemoPage
+{
+	public function initUI()
+	{
+		$radiolist = $this->ui->getWidget('radiolist');
+		$radiolist->addOptionsByArray(array(
+			0=>'Apple',
+			1=>'Orange',
+			2=>'Banana',
+			3=>'Pear',
+			4=>'Pineapple',
+			5=>'Kiwi',
+			6=>'Tangerine',
+			7=>'Grapefruit',
+			8=>'Strawberry'));
+	}
+}
+
+?>
