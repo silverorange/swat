370f72b688c1cb89139b1bcb9cb69fafde03797c
Nathan Fredrickson <nathan@silverorange.com>
2005-10-21 00:02:08 +0000

    Demo will use the installed version of swat so we can package it. Change your include path in your working directory if you want to use a working copy of Swat.
    svn commit r2381

--- demo/www/index.php
+++ demo/www/index.php
@@ -1,9 +1,5 @@
 <?php
 
-$uri_array = explode('/', $_SERVER['REQUEST_URI']);
-$work_dir = $uri_array[3];
-
-ini_set('include_path', ".:/so/packages/swat/{$work_dir}:/usr/lib/php");
 require_once '../include/DemoApplication.php';
 
 $app = new DemoApplication('demo');
