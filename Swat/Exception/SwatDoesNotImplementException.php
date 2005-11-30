1276549e519ff7ab97dee87b4fa2ce9793ffe67b
Michael Gauthier <mike@silverorange.com>
2005-11-30 02:56:57 +0000

    Actually add the exceptions I mentioned in the last commit.
    svn commit r2615

---
+++ Swat/Exception/SwatDoesNotImplementException.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once 'Swat/SwatException.php';
+
+/**
+ * Thrown when an object does not implement a required interface
+ *
+ * @package   Swat
+ * @copyright 2004-2005 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SwatDoesNotImplementException extends SwatException
+{
+	/**
+	 * The object that does not implement a required interface
+	 *
+	 * @var mixed
+	 */
+	protected $object = null;
+
+	/**
+	 * Creates a new does not implement exception
+	 *
+	 * @param string $message the message of the exception.
+	 * @param integer $code the code of the exception.
+	 * @param mixed $object the object that does not implement a required
+	 *                       interface.
+	 */
+	public function __construct($message = null, $code = 0, $object = null)
+	{
+		parent::__construct($message, $code);
+		$this->object = $object;
+	}
+
+	/**
+	 * Gets the object that does not implement a required interface
+	 *
+	 * @return mixed the object that does not implement a required interface.
+	 */
+	public function getObject()
+	{
+		return $this->object;
+	}
+}
+
+?>
