8ceb0f6a14a3509c90b5f5c9fb377cc79f5527c6
Michael Gauthier <mike@silverorange.com>
2005-11-30 02:59:02 +0000

    Rename this directory.
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@2616 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Swat/exceptions/SwatInvalidConstantExpressionException.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once 'Swat/SwatException.php';
+
+/**
+ * Thrown when an invalid constant expression is used
+ *
+ * @package   Swat
+ * @copyright 2004-2005 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SwatInvalidConstantExpressionException extends SwatException
+{
+	/**
+	 * The constant expression that is invalid
+	 *
+	 * @var string
+	 */
+	protected $expression= null;
+
+	/**
+	 * Creates a new class not found exception
+	 *
+	 * @param string $message the message of the exception.
+	 * @param integer $code the code of the exception.
+	 * @param string $expression the constant expression that is invalid.
+	 */
+	public function __construct($message = null, $code = 0, $expression = null)
+	{
+		parent::__construct($message, $code);
+		$this->expression = $expression;
+	}
+
+	/**
+	 * Gets the constant expression that is invalid
+	 *
+	 * @return string the constant expression that is invalid.
+	 */
+	public function getExpression()
+	{
+		return $this->expression;
+	}
+}
+
+?>
