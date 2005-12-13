3c3aa598a67623240c44acdfcf661193fa2b8988
Michael Gauthier <mike@silverorange.com>
2005-12-13 17:30:11 +0000

    A details view demo page courtesy of griffbrad.
    svn commit r2737

---
+++ demo/include/pages/DetailsView.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once 'DemoPage.php';
+require_once 'Swat/SwatDate.php';
+
+/**
+ * A demo using a details view
+ *
+ * @package   SwatDemo
+ * @copyright 2005 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class DetailsView extends DemoPage
+{
+	public function initUI()
+	{
+		$details_view = $this->ui->getWidget('details_view');
+        
+		$fruit = new FruitObject();
+     
+		$fruit->align        = 'middle';
+		$fruit->title        = 'Apple';   
+		$fruit->image        = 'images/apple.png';
+		$fruit->image_width  = 28;
+		$fruit->image_height = 28;
+		$fruit->makes_jam    = true;
+		$fruit->makes_pie    = true;
+		$fruit->harvest_date = '2005-10-31';
+		$fruit->cost         = .5;
+
+		$details_view->data = $fruit;
+	}
+}
+
+/**
+ * A demo using a details view
+ *
+ * @package   SwatDemo
+ * @copyright 2005 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class FruitObject
+{
+	public $align = '';
+	public $image = '';
+	public $image_width = 0;
+	public $image_height = 0;
+	public $title = '';
+	public $color = '';
+	public $makes_jam = false;
+	public $makes_pie = false;
+	public $harvest_date = null;
+	public $cost = 0;
+}
+
+?>
