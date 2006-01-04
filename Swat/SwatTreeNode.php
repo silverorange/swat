cde7e638bfef29c14f66cabcc2f3f2918919bbfe
Michael Gauthier <mike@silverorange.com>
2006-01-04 14:39:09 +0000

    Implement the recursive tree iterator interface. Now you can do something like:
     $iterator = new RecursiveIteratorIterator($tree);
     foreach ($iterator as $node) {
         echo $node->getIndex();
     }
    
    to visit each tree node.
    
    See http://www.php.net/~helly/php/ext/spl/classRecursiveIteratorIterator.html
    for details about the RecursiveIteratorIterator class.
    
    
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@2854 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/SwatTreeNode.php
+++ Swat/SwatTreeNode.php
@@ -11,7 +11,7 @@ require_once 'Swat/SwatObject.php';
  * @copyright 2004-2005 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-abstract class SwatTreeNode extends SwatObject
+abstract class SwatTreeNode extends SwatObject implements RecursiveIterator
 {
 	// {{{ protected properties
 
@@ -108,6 +108,8 @@ abstract class SwatTreeNode extends SwatObject
 	/**
 	 * Gets this node's children
 	 *
+	 * This method is needed to fulfill the RecursiveIterator interface.
+	 *
 	 * @return this node's children.
 	 */
 	public function getChildren()
@@ -120,6 +122,8 @@ abstract class SwatTreeNode extends SwatObject
 
 	/**
 	 * Whether or not this tree node has children
+	 *
+	 * This method is needed to fulfill the RecursiveIterator interface.
 	 *
 	 * @return boolean true if this node has children or false if this node
 	 *                  does not have children.
@@ -140,6 +144,90 @@ abstract class SwatTreeNode extends SwatObject
 	public function getIndex()
 	{
 		return $this->index;
+	}
+
+	// }}}
+	// {{{ public function current()
+
+	/**
+	 * Gets the current child node in this node
+	 *
+	 * This method is needed to fulfill the RecursiveIterator interface.
+	 *
+	 * @return mixed the current child node in this node as a
+	 *                {@link SwatTreeNode} object. If the current child node is
+	 *                invalid, false is returned.
+	 */
+	public function current()
+	{
+		return current($this->children);
+	}
+
+	// }}}
+	// {{{ public function key()
+
+	/**
+	 * Gets the key of the current child node in this node
+	 *
+	 * This method is needed to fulfill the RecursiveIterator interface.
+	 *
+	 * @reutrn integer the key (index) of the current child node in this node.
+	 */
+	public function key()
+	{
+		return key($this->children);
+	}
+
+	// }}}
+	// {{{ public function next()
+
+	/**
+	 * Gets the next child node in this node and moves the internal array
+	 * pointer forward
+	 *
+	 * This method is needed to fulfill the RecursiveIterator interface.
+	 *
+	 * @return mixed the next child node in this node as a
+	 *                {@link SwatTreeNode} object. If the next child node is
+	 *                invalid, false is returned.
+	 */
+	public function next()
+	{
+		return next($this->children);
+	}
+
+	// }}}
+	// {{{ public function rewind()
+
+	/**
+	 * Sets the internal pointer in the child nodes array back to the
+	 * beginning
+	 *
+	 * This method is needed to fulfill the RecursiveIterator interface.
+	 *
+	 * @return mixed the first child node in this node as a
+	 *                {@link SwatTreeNode} object. If there are no child nodes,
+	 *                false is returned.
+	 */
+	public function rewind()
+	{
+		return reset($this->children);
+	}
+	
+	// }}}
+	// {{{ public function valid()
+
+	/**
+	 * Whether the current child node in this node is valid
+	 *
+	 * This method is needed to fulfill the RecursiveIterator interface.
+	 *
+	 * @return boolean true if the current child node is valid and false if it
+	 *                  is not.
+	 */
+	public function valid()
+	{
+		return ($this->current() !== false);
 	}
 
 	// }}}
