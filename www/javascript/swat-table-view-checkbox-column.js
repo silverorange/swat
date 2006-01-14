02bd17978152900ad3abc2b821505b857c2f3a0a
Michael Gauthier <mike@silverorange.com>
2006-01-14 20:04:33 +0000

    Make this better. - Fix documentation and brace style. - Use array.push() instead of adding with indexes. I can't believe this ever   worked. - Put initialization loop stuff inside the list finding loop. This is a bit   more efficient.
    svn commit r2952

--- www/javascript/swat-table-view-checkbox-column.js
+++ www/javascript/swat-table-view-checkbox-column.js
@@ -1,40 +1,44 @@
 /**
  * Javascript SwatTableViewCheckboxColumn component
  *
- * @param id string Id of the matching {@link SwatTableViewCheckboxColumn}.
- * @param table SwatTableView A reference to the javascript object that
- * 		  represents the {@link SwatTableView}
- **/
-function SwatTableViewCheckboxColumn(id, table) {
+ * @param string id the unique identifier of the checkbox column.
+ * @param SwatTableView table the javascript object that represents the
+ *                             table-view.
+ */
+function SwatTableViewCheckboxColumn(id, table)
+{
 	var self = this;
 
 	this.id = id;
 	this.table = table;
-	this.check_all = null; //a reference to a checkall widget (if it exists - set by the SwatCheckAll widget)
+
+	// a reference to a checkall widget (if it exists - set by the SwatCheckAll widget)
+	this.check_all = null;
 	this.check_list = new Array();
 
-	//get all checkboxes with name = id + [] and that are contained in the
-	//correct table view. Note: getElementsByName doesn't work from a node
-	//element.
+	/*
+	 * Get all checkboxes with name = id + [] and that are contained in the
+	 * correct table-view. Note: getElementsByName() does not work from a node
+	 * element.
+	 */
 	var table_node = document.getElementById(this.table.id);
 	var items = table_node.getElementsByTagName('input');
-	for (i = 0; i < items.length; i++)
-		if (items[i].name == id + '[]')
-			this.check_list[i] = items[i]; 
+	for (i = 0; i < items.length; i++) {
+		if (items[i].name == id + '[]') {
+			this.check_list.push(items[i]);
+			this.highlightRow(items[i]);
 
+			if (is_ie)
+				items[i].attachEvent("onclick", eventHandler);
+			else 
+				items[i].addEventListener("change", eventHandler, false);
+		}
+	}
 
 	var is_ie = (document.addEventListener) ? false : true;
 
-	for (i = 0; i < this.check_list.length; i++) {
-		this.highlightRow(this.check_list[i]);
-
-		if (is_ie)
-			this.check_list[i].attachEvent("onclick", eventHandler);
-		else 
-			this.check_list[i].addEventListener("change", eventHandler, false);
-	}
-
-	function eventHandler(event) {
+	function eventHandler(event)
+	{
 		var node = (is_ie) ? event.srcElement : event.target;
 		self.highlightRow(node);
 
@@ -42,7 +46,8 @@ function SwatTableViewCheckboxColumn(id, table) {
 	}
 }
 
-SwatTableViewCheckboxColumn.prototype.checkAllInit = function() {
+SwatTableViewCheckboxColumn.prototype.checkAllInit = function()
+{
 	if (this.check_all == null)
 		return;
 
@@ -56,14 +61,16 @@ SwatTableViewCheckboxColumn.prototype.checkAllInit = function() {
 	this.check_all.setState(count == this.check_list.length);
 }
 
-SwatTableViewCheckboxColumn.prototype.checkAll = function(checked) {
+SwatTableViewCheckboxColumn.prototype.checkAll = function(checked)
+{
 	for (i = 0; i < this.check_list.length; i++) {
 		this.check_list[i].checked = checked;
 		this.highlightRow(this.check_list[i]);
 	}
 }
 
-SwatTableViewCheckboxColumn.prototype.highlightRow = function(node) {
+SwatTableViewCheckboxColumn.prototype.highlightRow = function(node)
+{
 	if (this.table)
 		this.table.highlightRow(node, node.checked);
 }
