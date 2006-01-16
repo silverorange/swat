f85ce01f9f640fad6df26605e26c01c192fa4ac9
Michael Gauthier <mike@silverorange.com>
2006-01-16 16:19:19 +0000

    Refactor the check-all widget and widgets that use the check-all widget. - The check-all javascript is now initialized in two stages. - Widgets that use the check-all treat it as a child widget rather than only   creating an instance in their respective display() methods. This makes   javascript includes, init()  and process() cleaner/more correct. - The check-all widget no longer needs/uses the controller property.
    svn commit r2957

--- www/javascript/swat-check-all.js
+++ www/javascript/swat-check-all.js
@@ -1,17 +1,53 @@
 /**
- * Javascript SwatCheckAll component
+ * Creates a new check-all object
  *
- * @param id string Id of the matching {@link SwatCheckAll} object.
- * @param controller SwatObject A reference to the javascript object that
- * 		  represents the checkboxlist
- **/
-function SwatCheckAll(id, controller) {
+ * The check-all object is responsible for handling change events and
+ * notifying its controller on state change.
+ *
+ * @param string id the unique identifier of this check-all object.
+ */
+function SwatCheckAll(id)
+{
+	this.id = id;
 	this.check_all = document.getElementById(id);
+	this.controller = null;
+}
+
+/**
+ * Set the state of this check-all object
+ *
+ * SwatCheckboxList uses this method to update the check-all's state when all
+ * checkbox list items are checked/unchecked.
+ *
+ * @param boolean checked the new state of this check-all object.
+ */
+SwatCheckAll.prototype.setState = function(checked)
+{
+	this.check_all.checked = checked;
+}
+
+/**
+ * Sets the controlling checkbox list
+ *
+ * This adds an event handler to the check-all to update the list when this
+ * check-all is checked/unchecked. 
+ *
+ * @param SwatCheckboxList controller the javascript object that represents the
+ *                          checkbox list.
+ */
+SwatCheckAll.prototype.setController = function(controller)
+{
 	var self = this;
-
 	var is_ie = (this.check_all.addEventListener) ? false : true;
-	controller.check_all = this;
 
+	if (this.controller !== null) {
+		// TODO: remove old event handlers
+		// this means we have to remember the old event handler somehow.
+	}
+
+	this.controller = controller;
+
+	controller.check_all = this;
 	controller.checkAllInit();
 
 	if (is_ie)
@@ -19,11 +55,9 @@ function SwatCheckAll(id, controller) {
 	else
 		this.check_all.addEventListener("change", eventHandler, false);
 
-	function eventHandler(event) {
+	function eventHandler(event)
+	{
+		// check all checkboxes in the controller object
 		controller.checkAll(self.check_all.checked);
 	}
-}
-
-SwatCheckAll.prototype.setState = function(checked) {
-	this.check_all.checked = checked;
 }
