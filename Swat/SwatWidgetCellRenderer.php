25d1cfce86f044f4669362bc43ad16231646099b
Nathan Fredrickson <nathan@silverorange.com>
2006-01-18 22:16:25 +0000

    Initial commit of a working widget cell renderer.
    svn commit r3017

---
+++ Swat/SwatWidgetCellRenderer.php
@@ -0,0 +1,170 @@
+<?php
+
+require_once 'Swat/SwatCellRenderer.php';
+require_once 'Swat/SwatUIParent.php';
+require_once 'Swat/exceptions/SwatInvalidClassException.php';
+require_once 'Swat/exceptions/SwatException.php';
+
+/**
+ *
+ *
+ */
+class SwatWidgetCellRenderer extends SwatCellRenderer implements SwatUIParent
+{
+	public $replicator_id;
+
+	/**
+	 * A reference to the widget for this cell
+	 */
+	private $widget = null;
+
+	private $mappings = array();
+	private $clones = array();
+
+	/**
+	 * fufills addChild
+	 *
+	 * @throws SwatException
+	 */
+	public function addChild(SwatObject $child)
+	{
+		if ($this->widget === null)
+			$this->setWidget($child);
+		else
+			throw new SwatException('Can only add one widget to a widget cell '.
+				'renderer');
+	}
+
+	public function getPropertyNameToMap(SwatUIObject $object, $name)
+	{
+		$mangled_name = $name;
+		$suffix = 0;
+
+		while (array_key_exists($mangled_name, $this->mappings)) {
+			$mangled_name = $name.$suffix;
+			$suffix++;
+		}
+
+		$this->mappings[$mangled_name] = 
+			array('object' => $object, 'property' => $name);
+
+		return $mangled_name;
+	}
+
+	/**
+	 * Maps a data field to a property of a widget in the widget tree
+	 *
+	 * TODO: document me better
+	 */
+	public function __set($name, $value)
+	{
+		if (array_key_exists($name, $this->mappings)) {
+			$object = $this->mappings[$name]['object'];
+			$property = $this->mappings[$name]['property'];
+			$object->$property = $value;
+		} else {
+			// TODO: throw something meaningful
+			throw new SwatException();
+		}
+	}
+
+	/**
+	 * Initializes this cell renderer
+	 *
+	 * This calls {@link SwatWidget::init()} on this renderer's widget.
+	 */
+	public function init()
+	{
+		if ($this->widget !== null)
+			$this->widget->init();
+	}
+
+	/**
+	 *
+	 */
+	public function process()
+	{
+		$form = $this->getFirstAncestor('SwatForm');
+		$replicators = $form->getHiddenField($this->widget->id.'_replicators');
+
+		foreach ($replicators as $replicator) {
+			$widget = $this->getClonedWidget($replicator);
+			$widget->process();
+		}
+	}
+
+	/**
+	 *
+	 */
+	public function render()
+	{
+		$form = $this->getFirstAncestor('SwatForm');
+		$widget = $this->getClonedWidget($this->replicator_id);
+
+		// TODO: make sure there actually is an id
+		$form->addHiddenField($this->widget->id.'_replicators', array_keys($this->clones));
+
+		if ($widget !== null)
+			$widget->display();
+	}
+
+	private function getClonedWidget($replicator)
+	{
+		if (isset($this->clones[$replicator]))
+			return $this->clones[$replicator];
+
+		if ($this->widget === null)
+			return null;
+
+		$suffix = '_'.$replicator;
+		$new_widget = clone $this->widget;
+
+		if ($new_widget->id !== null) {
+			//$this->widgets[$id][$new_widget->id] = $new_widget;
+			$new_widget->id.= $suffix;
+		}
+
+		if ($new_widget instanceof SwatContainer) {
+			foreach ($new_widget->getDescendants() as $descendant) {
+				if ($descendant->id !== null) {
+					//$this->widgets[$id][$descendant->id] = $descendant;
+					$descendant->id.= $suffix;
+				}
+			}
+		}
+
+		$this->clones[$replicator] = $new_widget;
+		return $new_widget;
+	}
+
+	/**
+	 *
+	 * @param SwatWidget $widget
+	 */
+	public function setWidget(SwatWidget $widget)
+	{
+		$this->widget = $widget;
+	}
+
+	/** 
+	 * Gets the widget of this widget cell renderer
+	 *
+	 * @return SwatWidget the widget of this widget cell renderer.
+	 */
+	public function getWidget()
+	{
+		return $this->widget;
+	}
+
+	/** 
+	 * Gets an array of cloned widgets indexed by the replicator_id
+	 *
+	 * @return array an array of  widgets indexed by replicator_id
+	 */
+	public function getClonedWidgets()
+	{
+		return $this->clones;
+	}
+}
+
+?>
