499199086c6ba3f080a77dd12228feb1c386a600
Nathan Fredrickson <nathan@silverorange.com>
2006-02-01 16:32:12 +0000

    Fixup SwatMoneyCellRenderer: 	Add $display_currency property. 	Add CSS class swat-money-cell-renderer: Steven, can you align this to the right?
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@3186 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/SwatMoneyCellRenderer.php
+++ Swat/SwatMoneyCellRenderer.php
@@ -24,9 +24,18 @@ class SwatMoneyCellRenderer extends SwatCellRenderer
 	/**
 	 * Monetary value
 	 * 
-	 * @var string
+	 * @var float
 	 */
 	public $value;
+
+	/**
+	 * Whether to display currency unit
+	 *
+	 * If true, displays the international currency unit
+	 *
+	 * @var boolean
+	 */
+	public $display_currency = false;
 
 	// }}}
 	// {{{ public function render()
@@ -39,7 +48,40 @@ class SwatMoneyCellRenderer extends SwatCellRenderer
 	public function render()
 	{
 		echo SwatString::minimizeEntities(
-			SwatString::moneyFormat($this->value, $this->locale));
+			SwatString::moneyFormat(
+				$this->value, $this->locale, $this->display_currency));
+	}
+
+	// }}}
+	// {{{ public function getTdAttributes()
+
+	/**
+	 * Gets TD-tag attributes
+	 *
+	 * @return array an array of attributes to apply to the TD tag of this cell
+	 *                renderer.
+	 *
+	 * @see SwatCellRenderer::getTdAttributes()
+	 */
+	public function getTdAttributes()
+	{
+		return array('class' => 'swat-money-cell-renderer');
+	}
+
+	// }}}
+	// {{{ public function getThAttributes()
+
+	/**
+	 * Gets TH-tag attributes
+	 *
+	 * @return array an array of attributes to apply to the TH tag in the
+	 *                table header for this cell renderer.
+	 *
+	 * @see SwatCellRenderer::getThAttributes()
+	 */
+	public function getThAttributes()
+	{
+		return array('class' => 'swat-money-cell-renderer');
 	}
 
 	// }}}
