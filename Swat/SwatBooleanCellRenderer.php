016f46baef8fc7f60357017636d1fe1a2741d8c1
Steven Garrity <steven@silverorange.com>
2006-02-15 17:27:57 +0000

    Added stock types to SwatBooleanCellRenderer so it can display a checkmark/nothing, or explicit yes/no text (we can easily add other stock types, or override/subclass in special cases) - Nathan did some of this
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@3545 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/SwatBooleanCellRenderer.php
+++ Swat/SwatBooleanCellRenderer.php
@@ -41,7 +41,68 @@ class SwatBooleanCellRenderer extends SwatCellRenderer
 	 *
 	 * @var string
 	 */
-	public $content_type;
+	public $content_type = null;
+
+	/**
+	 * The stock id of this SwatBooleanCellRenderer
+	 *
+	 * Specifying a stock id initializes this boolean cell renderer with a set of
+	 * stock values.
+	 *
+	 * @var string
+	 *
+	 * @see SwatBooleanCellRenderer::setFromStock()
+	 */
+	public $stock_id = null;
+
+	/**
+	 * Sets the values of this boolean cell renderer to a stock type
+	 *
+	 * Valid stock type ids are:
+	 *
+	 * - check-only
+	 * - yes-no
+	 *
+	 * @param string $stock_id the identifier of the stock type to use.
+	 * @param boolean $overwrite_properties whether to overwrite properties if
+	 *                                       they are already set.
+	 *
+	 * @throws SwatUndefinedStockTypeException
+	 */
+	public function setFromStock($stock_id, $overwrite_properties = true)
+	{
+		$content_type = 'text/plain';
+
+		switch ($stock_id) {
+		case 'yes-no':
+			$false_content = 'No';
+			$true_content = 'Yes';
+			break;
+
+		case 'check-only':
+			$content_type = 'text/xml';
+			$false_content = '&nbsp;';
+
+			ob_start();
+			$this->displayCheck();
+			$true_content = ob_get_clean();
+			break;
+
+		default:
+			throw new SwatUndefinedStockTypeException(
+				"Stock type with id of '{$stock_id}' not found.",
+				0, $stock_id);
+		}
+
+		if ($overwrite_properties || ($this->false_content === null))
+			$this->false_content = $false_content;
+		
+		if ($overwrite_properties || ($this->true_content === null))
+			$this->true_content = $true_content;
+
+		if ($overwrite_properties || ($this->content_type === null))
+			$this->content_type = $content_type;
+	}
 
 	/**
 	 * Renders the contents of this cell
@@ -50,6 +111,14 @@ class SwatBooleanCellRenderer extends SwatCellRenderer
 	 */
 	public function render()
 	{
+		if ($this->stock_id === null)
+			$this->setFromStock('check-only', false);
+		else
+			$this->setFromStock($this->stock_id, false);
+
+		if ($this->content_type = null)
+			$this->content_type = 'text/plain';
+
 		if ((boolean)$this->value)
 			$this->renderTrue();
 		else
@@ -58,31 +127,28 @@ class SwatBooleanCellRenderer extends SwatCellRenderer
 
 	protected function renderTrue()
 	{
-		if ($this->true_content !== null) {
-			if ($this->content_type === 'text/plain')
-				echo SwatString::minimizeEntities($this->true_content);
-			else
-				echo $this->true_content;
-		} else {
-			$image_tag = new SwatHtmlTag('img');
-			$image_tag->src = 'swat/images/check.png';
-			$image_tag->alt = Swat::_('Yes');
-			$image_tag->height = '14';
-			$image_tag->width = '14';
-			$image_tag->display();
-		}
+		if ($this->content_type === 'text/plain')
+			echo SwatString::minimizeEntities($this->true_content);
+		else
+			echo $this->true_content;
 	}
 
 	protected function renderFalse()
 	{
-		if ($this->false_content !== null) {
-			if ($this->content_type === 'text/plain')
-				echo SwatString::minimizeEntities($this->false_content);
-			else
-				echo $this->false_content;
-		} else {
-			echo '&nbsp;';
-		}
+		if ($this->content_type === 'text/plain')
+			echo SwatString::minimizeEntities($this->false_content);
+		else
+			echo $this->false_content;
+	}
+
+	protected function displayCheck()
+	{
+		$image_tag = new SwatHtmlTag('img');
+		$image_tag->src = 'swat/images/check.png';
+		$image_tag->alt = Swat::_('Yes');
+		$image_tag->height = '14';
+		$image_tag->width = '14';
+		$image_tag->display();
 	}
 }
 
