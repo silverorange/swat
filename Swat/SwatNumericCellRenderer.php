f9fc79fde35b7cd77c8209c0d52a87daa5f90e3e
Nick Burka <nick@silverorange.com>
2006-02-21 05:10:24 +0000

    A new Renderer to ouptut number-formatted values - STEVEN: needs CSS class (see TODO note) - allows for optional precision parameter - should be implemented anywhere we output numeric values, because it   right-aligns values and locale-formats them
    svn commit r3675

---
+++ Swat/SwatNumericCellRenderer.php
@@ -0,0 +1,74 @@
+<?php
+
+require_once 'Swat/SwatCellRenderer.php';
+require_once 'Swat/SwatString.php';
+
+/**
+ * A numeric cell renderer
+ *
+ * @package   Swat
+ * @copyright 2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SwatNumericCellRenderer extends SwatCellRenderer
+{
+	// {{{ public properties
+
+	/**
+	 * Value can be either a float or an integer
+	 * 
+	 * @var float
+	 */
+	public $value;
+
+	/**
+	 * Precision
+	 *
+	 * Optionally round the value to a certain precision
+	 * 
+	 * @var integer
+	 */
+	public $precision = null;
+
+	// }}}
+	// {{{ public function render()
+
+	/**
+	 * Renders the contents of this cell
+	 *
+	 * @see SwatCellRenderer::render()
+	 */
+	public function render()
+	{
+		$div = new SwatHTMLTag('div');
+		//TODO: change this into a css class
+		$div->align = 'right';
+		$div->open();
+
+		echo $this->getDisplayValue();
+
+		$div->close();
+	}
+
+	// }}}
+	// {{{ public function getDisplayValue()
+	protected function getDisplayValue()
+	{
+		if ($this->precision === null) {
+			$lc = localeconv();
+			$decimal_pos = strpos($this->value, $lc['decimal_point']);
+			$decimals = ($decimal_pos !== false) ?
+				strlen($this->value) - $decimal_pos - strlen($lc['decimal_point']) : 0;
+		} else {
+			$decimals = $this->precision;
+		}
+
+		if (is_numeric($this->value))
+			return SwatString::numberFormat($this->value, $decimals);
+		else
+			return $this->value;
+	}
+	// }}}
+}
+
+?>
