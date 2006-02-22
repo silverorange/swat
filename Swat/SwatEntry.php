023617f99ec22f42338926567b1aa60010929fa5
Michael Gauthier <mike@silverorange.com>
2006-02-22 18:45:24 +0000

    Added a method getFocusableHtmlId() to get the right id for the for attribute of label tags for swat widgets.
    This fixes many validation errors.
    
    
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@3709 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/SwatEntry.php
+++ Swat/SwatEntry.php
@@ -167,6 +167,21 @@ class SwatEntry extends SwatInputControl implements SwatState
 	{
 		return $this->value;
 	}
+
+	/**
+	 * Gets the id attribute of the XHTML element displayed by this widget
+	 * that should receive focus
+	 *
+	 * @return string the id attribute of the XHTML element displayed by this
+	 *                 widget that should receive focus or null if there is
+	 *                 no such element.
+	 *
+	 * @see SwatWidget::getFocusableHtmlId()
+	 */
+	public function getFocusableHtmlId()
+	{
+		return $this->id;
+	}
 }
 
 ?>
