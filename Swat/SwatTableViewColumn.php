8d85118d8200af6c30e7422f8bc36e2b331c57ee
Michael Gauthier <mike@silverorange.com>
2006-04-25 15:30:39 +0000

    Wrap long line.
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@4923 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/SwatTableViewColumn.php
+++ Swat/SwatTableViewColumn.php
@@ -15,7 +15,8 @@ require_once 'Swat/exceptions/SwatInvalidClassException.php';
  * @copyright 2004-2006 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-class SwatTableViewColumn extends SwatCellRendererContainer implements SwatUIParent
+class SwatTableViewColumn extends SwatCellRendererContainer
+	implements SwatUIParent
 {
 	/**
 	 * Unique identifier of this column
