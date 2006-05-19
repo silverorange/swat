78af468654f7e15d66cb55162d234163a297c7a3
Steven Garrity <steven@silverorange.com>
2006-05-19 18:58:24 +0000

    Some brute-force styles to make sure SwatExceptions are readable even when mixed in with partial other HTML/styles
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@5844 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/exceptions/SwatException.php
+++ Swat/exceptions/SwatException.php
@@ -268,7 +268,7 @@ class SwatException extends Exception
 	protected function displayStyleSheet()
 	{
 		echo '<style>';
-		echo ".swat-exception { border: 1px solid #d43; margin: 1em; font-family: sans-serif; }\n";
+		echo ".swat-exception { border: 1px solid #d43; margin: 1em; font-family: sans-serif; background: #fff !important; z-index: 9999 !important; color: #000; text-align: left; min-width: 400px; }\n";
 		echo ".swat-exception h3 { background: #e65; margin: 0; padding: 5px; border-bottom: 2px solid #d43; color: #fff; }\n";
 		echo ".swat-exception-body { padding: 0.8em; }\n";
 		echo ".swat-exception-message { margin-left: 2em; padding: 1em; }\n";
