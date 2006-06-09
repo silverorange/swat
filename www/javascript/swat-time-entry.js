6269c5b97b6e5e370c4dcc313c4c22b39deac116
Michael Gauthier <mike@silverorange.com>
2006-06-09 19:52:33 +0000

    Do the same thing I did for dates for times.
    svn commit r6964

--- www/javascript/swat-time-entry.js
+++ www/javascript/swat-time-entry.js
@@ -8,6 +8,53 @@ function SwatTimeEntry(id)
 	this.ampm = document.getElementById(id + '_ampm');
 
 	this.swat_date = null;
+
+	var is_ie = (document.addEventListener) ? false : true;
+	var self = this;
+
+	function handleChange(event)
+	{
+		var target;
+		if (!event)
+			var event = window.event;
+
+		if (event.target)
+			target = event.target;
+		else if (event.srcElement)
+			target = event.srcElement;
+
+		// fix Safari bug
+		if (target.nodeType == 3)
+			target = target.parentNode;
+
+		self.update(target);
+	}
+
+	if (is_ie) {
+		if (this.hour)
+			this.hour.attachEvent('onchange', handleChange);
+
+		if (this.minute)
+			this.minute.attachEvent('onchange', handleChange);
+
+		if (this.second)
+			this.second.attachEvent('onchange', handleChange);
+
+		if (this.ampm)
+			this.ampm.attachEvent('onchange', handleChange);
+	} else {
+		if (this.hour)
+			this.hour.addEventListener('change', handleChange, true);
+
+		if (this.minute)
+			this.minute.addEventListener('change', handleChange, true);
+
+		if (this.second)
+			this.second.addEventListener('change', handleChange, true);
+
+		if (this.ampm)
+			this.ampm.addEventListener('change', handleChange, true);
+	}
 }
 
 SwatTimeEntry.prototype.setSwatDate = function(swat_date)
@@ -21,10 +68,17 @@ SwatTimeEntry.prototype.setSwatDate = function(swat_date)
 
 SwatTimeEntry.prototype.reset = function(reset_date)
 {
-	if (this.hour) this.hour.selectedIndex = 0;
-	if (this.minute) this.minute.selectedIndex = 0;
-	if (this.second) this.second.selectedIndex = 0;
-	if (this.ampm) this.ampm.selectedIndex = 0;
+	if (this.hour)
+		this.hour.selectedIndex = 0;
+
+	if (this.minute)
+		this.minute.selectedIndex = 0;
+
+	if (this.second)
+		this.second.selectedIndex = 0;
+
+	if (this.ampm)
+		this.ampm.selectedIndex = 0;
 
 	if (this.swat_date && reset_date)
 		this.swat_date.reset(false);
@@ -43,21 +97,54 @@ SwatTimeEntry.prototype.setNow = function(set_date)
 	}
 	
 	if (this.hour && this.hour.selectedIndex == 0)
-		this.hour.selectedIndex = find_index(this.hour, hour_out);
+		this.hour.selectedIndex = this.getHourIndex(hour_out);
 		
 	if (this.minute && this.minute.selectedIndex == 0)
-		this.minute.selectedIndex =
-			find_index(this.minute, now.getMinutes());
+		this.minute.selectedIndex = this.getMinuteIndex(now.getMinutes());
 		
 	if (this.second && this.second.selectedIndex == 0)
-		this.second.selectedIndex =
-			find_index(this.second, now.getSeconds());
+		this.second.selectedIndex = this.getSecondIndex(now.getSeconds());
 		
 	if (this.ampm && this.ampm.selectedIndex == 0)
 		this.ampm.selectedIndex = ampm_out;
 	
 	if (this.swat_date && set_date)
 		this.swat_date.setNow(false);
+}
+
+SwatTimeEntry.prototype.parseInt = function(serialized_integer)
+{
+	var value = parseInt(serialized_integer.replace(/[^\d]*/, ''));
+	if (isNaN(value))
+		return null;
+
+	return value;
+}
+
+SwatTimeEntry.prototype.getIntegerIndex = function(flydown, value)
+{
+	var flydown_value;
+	for (i = 0; i < flydown.options.length; i++) {
+		flydown_value = this.parseInt(flydown.options[i].value);
+		if (flydown_value == value)
+			return i;
+	}
+	return null;
+}
+
+SwatTimeEntry.prototype.getHourIndex = function(hour)
+{
+	return this.getIntegerIndex(this.hour, hour);
+}
+
+SwatTimeEntry.prototype.getMinuteIndex = function(minute)
+{
+	return this.getIntegerIndex(this.minute, minute);
+}
+
+SwatTimeEntry.prototype.getSecondIndex = function(second)
+{
+	return this.getIntegerIndex(this.second, second);
 }
 
 SwatTimeEntry.prototype.setDefault = function(set_date)
@@ -78,21 +165,19 @@ SwatTimeEntry.prototype.setDefault = function(set_date)
 		this.swat_date.setDefault(false);
 }
 
-SwatTimeEntry.prototype.set = function(active_flydown)
+SwatTimeEntry.prototype.update = function(active_flydown)
 {
 	// hour is required for this, so stop if it doesn't exist
 	if (!this.hour) return;
 	
-	if (active_flydown.value == '') {
-		//this.reset(true);
-	} else {
+	if (this.parseInt(active_flydown.value) != null) {
 		var now = new Date();	
 		var this_hour = now.getHours();
 		
 		if (this_hour > 11)
 			this_hour = this_hour - 12;
 		
-		if (this.hour.value == this_hour)
+		if (this.parseInt(this.hour.value) == this_hour)
 			this.setNow(true);
 		else
 			this.setDefault(true);
