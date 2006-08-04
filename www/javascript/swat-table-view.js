eea4b70fa9dca53f06dc8dc64e1cc0036025d89f
Michael Gauthier <mike@silverorange.com>
2006-08-04 13:37:42 +0000

    Better class switching.
    svn commit r9048

--- www/javascript/swat-table-view.js
+++ www/javascript/swat-table-view.js
@@ -13,45 +13,22 @@ SwatTableView.prototype.highlightRow = function(node, highlight)
 {
 	if (node.nodeName == 'TR') {
 		var class_names = node.className.split(' ');
-		if (highlight && findClass(class_names, 'odd'))
-			class_names = replaceClass(class_names, 'odd', 'highlight-odd');
-		else if (findClass(class_names, 'highlight-odd'))
-			class_names = replaceClass(class_names, 'highlight-odd', 'odd');
-		else if (highlight)
-			class_names[class_names.length] = 'highlight';
-		else
-			class_names = replaceClass(class_names, 'highlight', '');
 
-		node.className = classToString(class_names);
+		if (node.className.match(/odd/)) {
+			if (highlight) {
+				node.className.replace(/ *odd/, '');
+				node.className += ' highlight-odd';
+			} else {
+				node.className.replace(/ *highlight-odd/, '');
+				node.className += ' odd';
+			}
+		} else {
+			if (highlight)
+				node.className += ' highlight';
+			else
+				node.className.replace(/ *highlight/, '');
+		}
 	} else if (node.parentNode) {
 		this.highlightRow(node.parentNode, highlight);
-	}
-
-	function findClass(class_names, name)
-	{
-		for (var i = 0; i < class_names.length; i++)
-			if (class_names[i] == name)
-				return true;
-
-		return false;
-	}
-
-	function replaceClass(class_names, from, to)
-	{
-		for (var i = 0; i < class_names.length; i++)
-			if (class_names[i] == from)
-				class_names[i] = to;
-
-		return class_names;
-	}
-
-	function classToString(class_names)
-	{
-		var out = '';
-
-		for (var i = 0; i < class_names.length; i++)
-			out += class_names[i] + ' ';
-
-		return out;
 	}
 }
