a96db330972cbec4b855123a62d28b7520b9aa51
Michael Gauthier <mike@silverorange.com>
2006-08-04 13:26:51 +0000

    Coding conventions.
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@9047 94490950-a86c-4493-9ccb-530f72933e47

--- www/javascript/swat-table-view.js
+++ www/javascript/swat-table-view.js
@@ -1,34 +1,34 @@
 /**
- * Javascript SwatTableView component
+ * JavaScript for the SwatTableView widget 
  *
  * @param id string Id of the matching {@link SwatTableView}.
- **/
-function SwatTableView(id) {
+ */
+function SwatTableView(id)
+{
 	this.id = id;
 	var self = this;
 }
 
-SwatTableView.prototype.highlightRow = function(node, highlight) {
+SwatTableView.prototype.highlightRow = function(node, highlight)
+{
 	if (node.nodeName == 'TR') {
 		var class_names = node.className.split(' ');
 		if (highlight && findClass(class_names, 'odd'))
 			class_names = replaceClass(class_names, 'odd', 'highlight-odd');
-
 		else if (findClass(class_names, 'highlight-odd'))
 			class_names = replaceClass(class_names, 'highlight-odd', 'odd');
-
 		else if (highlight)
 			class_names[class_names.length] = 'highlight';
-
 		else
 			class_names = replaceClass(class_names, 'highlight', '');
 
 		node.className = classToString(class_names);
-
-	} else if (node.parentNode)
+	} else if (node.parentNode) {
 		this.highlightRow(node.parentNode, highlight);
+	}
 
-	function findClass(class_names, name) {
+	function findClass(class_names, name)
+	{
 		for (var i = 0; i < class_names.length; i++)
 			if (class_names[i] == name)
 				return true;
@@ -36,7 +36,8 @@ SwatTableView.prototype.highlightRow = function(node, highlight) {
 		return false;
 	}
 
-	function replaceClass(class_names, from, to) {
+	function replaceClass(class_names, from, to)
+	{
 		for (var i = 0; i < class_names.length; i++)
 			if (class_names[i] == from)
 				class_names[i] = to;
@@ -44,9 +45,10 @@ SwatTableView.prototype.highlightRow = function(node, highlight) {
 		return class_names;
 	}
 
-	function classToString(class_names) {
+	function classToString(class_names)
+	{
 		var out = '';
-		
+
 		for (var i = 0; i < class_names.length; i++)
 			out += class_names[i] + ' ';
 
