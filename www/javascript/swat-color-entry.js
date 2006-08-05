c256de1fa0252c75806ea38048d325410cf453b4
Michael Gauthier <mike@silverorange.com>
2006-08-05 17:02:35 +0000

    Respect Swat changes to ColorEntry in JavaScript.
    svn commit r9137

--- www/javascript/swat-color-entry.js
+++ www/javascript/swat-color-entry.js
@@ -6,7 +6,7 @@ function SwatColorEntry(id)
 	this.hex = Array('0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F');
 	this.shades = new Array(1, 0.80, 0.60, 0.40, 0.20, 0);
 
-	this.entryElement = document.getElementById(this.id);
+	this.entryElement = document.getElementById(this.id + '_value');
 	
 	if (this.entryElement.value.length == 6) {
 		this.setHex(this.entryElement.value);
