69624992b374700bbf4431a9a492da9b9149c388
Steven Garrity <steven@silverorange.com>
2006-08-08 13:41:29 +0000

    Move SwatNullTextCellRenderer from AdminNullTextCellRenderer
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@9227 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Swat/SwatNullTextCellRenderer.php
@@ -0,0 +1,73 @@
+<?php
+
+require_once 'Swat/SwatTextCellRenderer.php';
+
+/**
+ * A cell renderer that displays a message if it is asked to display
+ * null text
+ *
+ * @package   Swat
+ * @copyright 2005-2006 silverorange
+ */
+class SwatNullTextCellRenderer extends SwatTextCellRenderer
+{
+	// {{{ public properties
+
+	/**
+	 * The text to display in this cell if the
+	 * {@link SwatTextCellRenderer::$text} proeprty is null when the render()
+	 * method is called
+	 *
+	 * @var string
+	 */
+	public $null_text = '&lt;none&gt;';
+
+	/**
+	 * Whether to test the {@link SwatTextCellRenderer::$text} property for
+	 * null using strict equality.
+	 *
+	 * @var boolean
+	 */
+	public $strict = false;
+
+	// }}}
+	// {{{ public function __construct()
+
+	/**
+	 * Creates a null text cell renderer
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+		$this->addStyleSheet('packages/swat/styles/swat-null-text-cell-renderer.css');
+	}
+
+	// }}}
+	// {{{ public function render()
+
+	/**
+	 * Renders this cell renderer
+	 */
+	public function render()
+	{
+		if (!$this->visible)
+			return;
+
+		if (($this->strict && $this->text === null) ||
+			(!$this->strict && $this->text == null)) {
+
+			$this->text = $this->null_text;
+
+			echo '<span class="swat-null-text-cell-renderer">';
+			parent::render();
+			echo '</span>';
+
+		} else {
+			parent::render();
+		}
+	}
+
+	// }}}
+}
+
+?>
