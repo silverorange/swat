08c1efece0124ad741276a341e604b90edcd0949
Michael Gauthier <mike@silverorange.com>
2006-09-13 21:24:01 +0000

    Two new classes for custom logging and displaying of errors respectively.
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@10962 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Swat/exceptions/SwatExceptionDisplayer.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once 'Swat/exceptions/SwatException.php';
+
+/**
+ * Abstract base class for displaying SwatException objects
+ *
+ * A custom exception displayer can be used to change how uncaught exceptions
+ * are displayed in an application. For example, you may want to display
+ * exceptions in a separate file or display them using different XHTML
+ * markup.
+ *
+ * @package   Swat
+ * @copyright 2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ * @see       SwatException::setDisplayer()
+ */
+abstract class SwatExceptionDisplayer
+{
+	// {{{ public abstract function display()
+
+	/**
+	 * Displays a SwatException
+	 *
+	 * This is called by SwatException::process().
+	 */
+	public abstract function display(SwatException $e);
+
+	// }}}
+}
+
+?>
