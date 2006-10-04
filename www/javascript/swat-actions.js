7c663abd147d91fa78eff60ef02748349b6af1d3
Michael Gauthier <mike@silverorange.com>
2006-10-04 20:37:41 +0000

    Use Yahoo UI Library to abstract away some clunky code for css-class manipulation and event handling.
    svn commit r11816

--- www/javascript/swat-actions.js
+++ www/javascript/swat-actions.js
@@ -1,7 +1,6 @@
 function SwatActions(id, values, selected)
 {
 	var self = this;
-	var is_ie = (document.addEventListener) ? false : true;
 
 	this.id = id;
 	this.flydown = document.getElementById(id + '_action_flydown');
@@ -12,21 +11,15 @@ function SwatActions(id, values, selected)
 
 	function handleChange(event)
 	{
-		if (self.selected_element) {
-			self.selected_element.className += ' swat-hidden';
-		}
+		if (self.selected_element)
+			YAHOO.util.Dom.addClass(self.selected_element, 'swat-hidden');
 
 		var id = self.id + '_' + self.values[self.flydown.selectedIndex];
 		self.selected_element = document.getElementById(id);
 
-		if (self.selected_element) {
-			self.selected_element.className =
-				self.selected_element.className.replace(/ *swat-hidden/, '');
-		}
+		if (self.selected_element)
+			YAHOO.util.Dom.removeClass(self.selected_element, 'swat-hidden');
 	}
 
-	if (is_ie)
-		this.flydown.attachEvent('onchange', handleChange);
-	else
-		this.flydown.addEventListener('change', handleChange, true);
+	YAHOO.util.Event.addListener(this.flydown, 'change', handleChange);
 }
