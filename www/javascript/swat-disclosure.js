5d108afc85d1b5f57e3658389ec2ada01e837629
Michael Gauthier <mike@silverorange.com>
2006-10-07 15:40:46 +0000

    web-twenny fade for opening and closing disclosures
    svn commit r11858

--- www/javascript/swat-disclosure.js
+++ www/javascript/swat-disclosure.js
@@ -3,6 +3,7 @@ function SwatDisclosure(id)
 	this.image = document.getElementById(id + '_img');
 	this.input = document.getElementById(id + '_input');
 	this.div = document.getElementById(id);
+	this.content_div = this.div.firstChild.nextSibling;
 
 	// get initial state
 	if (this.input.value == 'opened') {
@@ -26,7 +27,10 @@ SwatDisclosure.prototype.toggle = function()
 
 SwatDisclosure.prototype.close = function()
 {
-	this.div.className = 'swat-disclosure-control-closed';
+	var attributes = { opacity: { from: 1, to: 0} }; 
+	var fade_animation = new YAHOO.util.Anim(this.content_div, attributes, 0.25); 
+	fade_animation.animate();
+	fade_animation.onComplete.subscribe(SwatDisclosure.handleClose, this.div);
 	this.image.src = 'packages/swat/images/swat-disclosure-closed.png';
 	this.image.alt = SwatDisclosure.open_text;
 	this.input.value = 'closed';
@@ -35,9 +39,17 @@ SwatDisclosure.prototype.close = function()
 
 SwatDisclosure.prototype.open = function()
 {
+	var attributes = { opacity: { from: 0, to: 1} }; 
+	var fade_animation = new YAHOO.util.Anim(this.content_div, attributes, 0.5); 
+	fade_animation.animate();
 	this.div.className = 'swat-disclosure-control-opened';
 	this.image.src = 'packages/swat/images/swat-disclosure-open.png';
 	this.image.alt = SwatDisclosure.close_text;
 	this.input.value = 'opened';
 	this.opened = true;
+}
+
+SwatDisclosure.handleClose = function(type, args, div)
+{
+	div.className = 'swat-disclosure-control-closed';
 }
