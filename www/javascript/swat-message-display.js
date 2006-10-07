aa9f7164304b597a2574df3e3ae80ff776013f86
Michael Gauthier <mike@silverorange.com>
2006-10-07 05:16:22 +0000

    use web-twenny magic to hide notification messages
    svn commit r11852

--- www/javascript/swat-message-display.js
+++ www/javascript/swat-message-display.js
@@ -9,6 +9,22 @@ SwatMessageDisplay.prototype.hideMessage = function(message_index)
 	var message = document.getElementById(id);
 
 	if (message !== null) {
-		message.style.display = 'none';
+		var houdini_div = document.createElement('div');
+		message.parentNode.insertBefore(houdini_div, message);
+		houdini_div.appendChild(message);
+		houdini_div.style.overflow = 'hidden';
+		var attributes = { 
+			height: { to: 0 }, 
+		}; 
+		var myAnim = new YAHOO.util.Anim(houdini_div, attributes, 0.25); 
+		myAnim.animate();
+		myAnim.onComplete.subscribe(SwatMessageDisplay.removeMessage,
+			houdini_div);
 	}
+}
+
+SwatMessageDisplay.removeMessage = function(type, args, houdini_div)
+{
+	var removed_node = houdini_div.parentNode.removeChild(houdini_div);
+	removed_node = null;
 }
