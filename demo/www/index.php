5fe6719a6205a22ef827c94145f3ae505790c104
Michael Gauthier <mike@silverorange.com>
2006-10-28 03:04:40 +0000

    remove PEAR::Date from PackageConfig
    svn commit r12385

--- demo/www/index.php
+++ demo/www/index.php
@@ -5,7 +5,6 @@ if (class_exists('PackageConfig')) {
 	PackageConfig::setWorkDirPosition(3);
 	PackageConfig::addPackage('site');
 	PackageConfig::addPackage('swat');
-	PackageConfig::addPath('/so/packages/pear/pear/Date');
 }
 
 require_once 'Swat/SwatAutoloader.php';
