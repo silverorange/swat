c0ccc6755269c5e1cdabcbf0899e051435b83450
Michael Gauthier <mike@silverorange.com>
2006-10-28 19:49:40 +0000

    add radio table demo
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@12391 94490950-a86c-4493-9ccb-530f72933e47

--- demo/include/pages/RadioList.php
+++ demo/include/pages/RadioList.php
@@ -28,6 +28,18 @@ class RadioList extends DemoPage
 			6 => 'Tangerine',
 			7 => 'Grapefruit',
 			8 => 'Strawberry'));
+
+		$radiotable = $this->ui->getWidget('radiotable');
+		$radiotable->addOptionsByArray(array(
+			0 => 'Apple',
+			1 => 'Orange',
+			2 => 'Banana',
+			3 => 'Pear',
+			4 => 'Pineapple',
+			5 => 'Kiwi',
+			6 => 'Tangerine',
+			7 => 'Grapefruit',
+			8 => 'Strawberry'));
 	}
 
 	// }}}
