04d3d8f332cc23d5c4684ca03220762ef10eb0a4
Michael Gauthier <mike@silverorange.com>
2006-11-14 05:36:02 +0000

    coding conventions
    svn commit r12999

--- www/javascript/swat-cascade.js
+++ www/javascript/swat-cascade.js
@@ -1,34 +1,41 @@
-function SwatCascade(parent, child) {
+function SwatCascade(parent, child)
+{
 	this.parent = document.getElementById(parent);
 	this.child = document.getElementById(child);
 	this.init();
 }
 
-SwatCascade.prototype.update = function() {
+SwatCascade.prototype.update = function()
+{
 	var display = this.children[this.parent.value];
 
 	//reset the options
-	for (i = this.child.options.length-1; i >= 0; i--)
+	for (var i = 0; i < this.child.options.length; i++)
 		this.child.options[i] = null;
 	
-	if (!display) {
-		this.child.options[0] = this.blank_option;
-		this.child.disabled = true;
-	} else {
+	if (display) {
 		this.child.disabled = false;
 
 		for (i = 0; i < display.length; i++)
-			this.child.options[i] = new Option(display[i].title, display[i].value);
+			this.child.options[i] =
+				new Option(display[i].title, display[i].value);
+	} else {
+		this.child.options[0] = this.blank_option;
+		this.child.disabled = true;
 	}
 }
 
-SwatCascade.prototype.addChild = function(parent, value, title, selected) {
-	if (!this.children[parent]) this.children[parent] = new Array();
+SwatCascade.prototype.addChild = function(parent, value, title, selected)
+{
+	if (!this.children[parent])
+		this.children[parent] = [];
+
 	this.children[parent].push(new SwatCascadeChild(value, title, selected));
 }
 
-SwatCascade.prototype.init = function() {
-	this.children = new Array();
+SwatCascade.prototype.init = function()
+{
+	this.children = [];
 	this.blank_option = this.child[0];
 	
 	var child_id = this.child.id;
@@ -36,13 +43,15 @@ SwatCascade.prototype.init = function() {
 	if (!this.parent.value) 
 		this.child.disabled = true;
 
+	// TODO: use DOM event handling
 	this.parent.onchange = function() {
 		obj = eval(child_id + '_cascade');
 		obj.update();
 	}
 }
 
-function SwatCascadeChild(value, title, selected) {
+function SwatCascadeChild(value, title, selected)
+{
 	this.value = value;
 	this.title = title;
 	this.selected = selected;
