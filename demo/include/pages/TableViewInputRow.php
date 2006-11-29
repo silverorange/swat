cd7366948783fe5d787b945b7b40a5267a47e46c
Michael Gauthier <mike@silverorange.com>
2006-11-29 21:56:31 +0000

    update copyright
    svn commit r13475

--- demo/include/pages/TableViewInputRow.php
+++ demo/include/pages/TableViewInputRow.php
@@ -8,7 +8,7 @@ require_once 'DemoPage.php';
  * A demo using a table view
  *
  * @package   SwatDemo
- * @copyright 2005-2006 silverorange
+ * @copyright 2006 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class TableViewInputRow extends DemoPage
@@ -45,7 +45,7 @@ class TableViewInputRow extends DemoPage
  * A demo using a table view
  *
  * @package   SwatDemo
- * @copyright 2005-2006 silverorange
+ * @copyright 2006 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class FruitObject
