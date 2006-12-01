6a9f7c071f3cd1624fafbcffbc66f142acebf64d
Michael Gauthier <mike@silverorange.com>
2006-12-01 16:08:44 +0000

    - add animation for expandable checkbox tree nodes (Fixes #300) - refactor events so self is not needed - add missing var declarations - whitespace
    svn commit r13541

--- www/javascript/swat-expandable-checkbox-tree.js
+++ www/javascript/swat-expandable-checkbox-tree.js
@@ -1,6 +1,5 @@
 function SwatExpandableCheckboxTree(id, dependent_boxes)
 {
-	var self = this;
 	this.id = id;
 
 	// This flag sets the behaviour of checkboxes. If it is true then checking
@@ -12,40 +11,38 @@ function SwatExpandableCheckboxTree(id, dependent_boxes)
 
 		for (var i = 0; i < this.check_list.length; i++) {
 			YAHOO.util.Event.addListener(this.check_list[i], 'change',
-				handleClick);
+				SwatExpandableCheckboxTree.handleClick, this);
 		}
-	}
-
-	function handleClick(event)
-	{
-		self.handleClick(this);
 	}
 }
 
 SwatExpandableCheckboxTree.open_text = 'open';
 SwatExpandableCheckboxTree.close_text = 'close';
 
+SwatExpandableCheckboxTree.handleClick = function(event, tree)
+{
+	var checkbox = YAHOO.util.Event.getTarget(event);
+	tree.handleClick(checkbox);
+}
+
 SwatExpandableCheckboxTree.prototype.handleClick = function(checkbox)
 {
 	// get path of checkbox from id
 	var path = checkbox.id.substr(this.id.length + 1);
-
-	branch = document.getElementById(this.id + '_' + path + '_branch');
+	var branch = document.getElementById(this.id + '_' + path + '_branch');
 
 	// check all sub-elements
 	// ignore leaves
 	if (branch) {
-		
-		checkboxes = branch.getElementsByTagName('input');
+		var checkboxes = branch.getElementsByTagName('input');
 
 		for (var i = 0; i < checkboxes.length; i++) {
 			if (checkboxes[i].getAttribute('type') == 'checkbox') {
 				checkboxes[i].checked = checkbox.checked;
 			}
 		}
-
 	}
-	
+
 	// check parent elements
 	// split path into pieces
 	var path_exp = path.split('/');
@@ -71,7 +68,7 @@ SwatExpandableCheckboxTree.prototype.handleClick = function(checkbox)
 
 		var checkboxes = branch.getElementsByTagName('input');
 		var all_checked = true;
-		
+
 		// get state of all checkboxes below parent
 		for (var j = 0; j < checkboxes.length; j++) {
 			if (checkboxes[j].getAttribute('type') == 'checkbox') {
@@ -88,25 +85,87 @@ SwatExpandableCheckboxTree.prototype.handleClick = function(checkbox)
 
 SwatExpandableCheckboxTree.prototype.toggleBranch = function(branch_id)
 {
-	branch = document.getElementById(this.id + '_' + branch_id + '_branch');
-	image = document.getElementById(this.id + '_' + branch_id + '_img');
-
-	opened = YAHOO.util.Dom.hasClass(branch,
+	var branch = document.getElementById(this.id + '_' + branch_id + '_branch');
+	var opened = YAHOO.util.Dom.hasClass(branch,
 		'swat-expandable-checkbox-tree-opened');
 
 	if (opened) {
-		YAHOO.util.Dom.addClass(branch, 'swat-expandable-checkbox-tree-closed');
-		YAHOO.util.Dom.removeClass(branch,
-			'swat-expandable-checkbox-tree-opened');
-
-		image.src = 'packages/swat/images/swat-disclosure-closed.png';
-		image.alt = SwatExpandableCheckboxTree.open_text;
+		this.closeBranch(branch_id);
 	} else {
-		YAHOO.util.Dom.addClass(branch, 'swat-expandable-checkbox-tree-opened');
-		YAHOO.util.Dom.removeClass(branch,
-			'swat-expandable-checkbox-tree-closed');
-
-		image.src = 'packages/swat/images/swat-disclosure-open.png';
-		image.alt = SwatExpandableCheckboxTree.closed_text;
+		this.openBranch(branch_id);
 	}
+}
+
+SwatExpandableCheckboxTree.prototype.openBranch = function(branch_id)
+{
+	var branch = document.getElementById(this.id + '_' + branch_id + '_branch');
+	var image = document.getElementById(this.id + '_' + branch_id + '_img');
+
+	YAHOO.util.Dom.removeClass(branch, 'swat-expandable-checkbox-tree-closed');
+	YAHOO.util.Dom.addClass(branch, 'swat-expandable-checkbox-tree-opened');
+
+	// get display height
+	branch.parentNode.style.overflow = 'hidden';
+	branch.parentNode.style.height = '0';
+	branch.style.visibility = 'hidden';
+	branch.style.overflow = 'hidden';
+	branch.style.display = 'block';
+	branch.style.height = '';
+	var height = branch.offsetHeight;
+	branch.style.height = '0';
+	branch.style.visibility = 'visible';
+	branch.parentNode.style.height = '';
+	branch.parentNode.style.overflow = 'visible';
+
+	var attributes = { height: { to: height, from: 0 } };
+	var animation = new YAHOO.util.Anim(branch, attributes, 0.25,
+		YAHOO.util.Easing.easeOut);
+
+	animation.onComplete.subscribe(
+		SwatExpandableCheckboxTree.handleBranchOpen, [this, branch, image]);
+
+	animation.animate();
+}
+
+SwatExpandableCheckboxTree.prototype.closeBranch = function(branch_id)
+{
+	var branch = document.getElementById(this.id + '_' + branch_id + '_branch');
+	var image = document.getElementById(this.id + '_' + branch_id + '_img');
+
+	branch.style.overflow = 'hidden';
+	branch.style.height = '';
+
+	var attributes = { height: { to: 0 } };
+	var animation = new YAHOO.util.Anim(branch, attributes, 0.25,
+		YAHOO.util.Easing.easingIn);
+
+	animation.onComplete.subscribe(
+		SwatExpandableCheckboxTree.handleBranchClose, [this, branch, image]);
+
+	animation.animate();
+}
+
+SwatExpandableCheckboxTree.handleBranchOpen = function(type, args, data)
+{
+	var tree = data[0];
+	var branch = data[1];
+	var image = data[2];
+
+	branch.style.height = '';
+
+	image.src = 'packages/swat/images/swat-disclosure-open.png';
+	image.alt = SwatExpandableCheckboxTree.closed_text;
+}
+
+SwatExpandableCheckboxTree.handleBranchClose = function(type, args, data)
+{
+	var tree = data[0];
+	var branch = data[1];
+	var image = data[2];
+
+	image.src = 'packages/swat/images/swat-disclosure-closed.png';
+	image.alt = SwatExpandableCheckboxTree.open_text;
+
+	YAHOO.util.Dom.addClass(branch, 'swat-expandable-checkbox-tree-closed');
+	YAHOO.util.Dom.removeClass(branch, 'swat-expandable-checkbox-tree-opened');
 }
