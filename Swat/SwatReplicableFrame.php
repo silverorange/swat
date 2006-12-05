47d26360667ec1605fe0c45866a7e838e0e2ff33
Michael Gauthier <mike@silverorange.com>
2006-12-05 21:04:55 +0000

    Add a replicable frame. Fixes #353.
    svn commit r13631

---
+++ Swat/SwatReplicableFrame.php
@@ -0,0 +1,46 @@
+<?php
+
+/* vim: set noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+
+require_once 'Swat/SwatFrame.php';
+require_once 'Swat/SwatReplicableContainer.php';
+
+/**
+ * A form field container that replicates its children
+ *
+ * The form field can dynamically create widgets based on an array of
+ * replicators identifiers.
+ *
+ * @package   Swat
+ * @copyright 2005-2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SwatReplicableFrame extends SwatReplicableContainer
+{
+	// {{{ protected function getContainer()
+
+	/**
+	 * Gets a container to contain replicated widgets for this replicable
+	 * container
+	 *
+	 * @param string $id the replicator id for the container.
+	 * @param stirng $title the title of the container. The container may or
+	 *                       may not use this title.
+	 *
+	 * @return SwatContainer the container object to which replciated widgets
+	 *                        are added. The container is added to the widget
+	 *                        tree after adding the replicated widgets to the
+	 *                        container. If null is returned, the widgets are
+	 *                        replicated directly in the widget tree.
+	 */
+	protected function getContainer($id, $title)
+	{
+		$form_field = new SwatFrame();
+		$form_field->title = $title;
+		return $form_field;
+	}
+
+	// }}}
+}
+
+?>
