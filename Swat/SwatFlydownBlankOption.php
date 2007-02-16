cf932b6110716242e5403d14785b25c54d69b450
Nick Burka <nick@silverorange.com>
2007-02-16 19:53:47 +0000

    Add a special class for blank options in flydowns
    svn commit r15099

---
+++ Swat/SwatFlydownBlankOption.php
@@ -0,0 +1,21 @@
+<?php
+
+/* vim: set noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+
+require_once 'Swat/SwatOption.php';
+
+/**
+ * A class representing a blank option in a flydown
+ *
+ * This class is for semantic purposed only. The flydown handles all the
+ * displaying of dividers and regular flydown options.
+ *
+ * @package   Swat
+ * @copyright 2005-2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SwatFlydownBlankOption extends SwatOption
+{
+}
+
+?>
