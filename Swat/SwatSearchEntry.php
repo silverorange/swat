5f00975f942c21fc469331e7bccc01fb0894ab7a
Charles Waddell <charles@silverorange.com>
2007-02-20 02:47:26 +0000

    Added a new search widget to the Swat toolkit.
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@15183 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Swat/SwatSearchEntry.php
@@ -0,0 +1,52 @@
+<?php
+
+/* vim: set noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+
+require_once 'Swat/SwatEntry.php';
+
+/**
+ * A single line search entry widget
+ *
+ * @package   Swat
+ * @copyright 2004-2007 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SwatSearch extends SwatEntry
+{
+	// {{{ public properties
+
+    /**
+     * SwatSearch title value
+     *
+     * Text content of the widget, or defaults to 'Enter Search'.
+     *
+     * @var string
+     */
+	public $title;
+
+	// }}}
+	// {{{ protected function getDisplayValue()
+
+	/**
+	 * Displays a value for SwatSearch
+	 *
+	 * The methond returns either the title or the correct search entry.
+	 *
+	 * @return string the display value
+	 */
+	protected function getDisplayValue()
+	{
+		if ($this->value === null){
+			if ($this->title === null)
+				return 'Enter Search...';
+			else
+				return $this->title;
+		} else {
+			return parent::getDisplayValue();
+		}
+	}
+
+	// }}}
+}
+
+?>
