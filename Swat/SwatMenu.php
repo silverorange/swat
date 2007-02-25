54bb151b16ebabbe151a64b89f86d7e3f8f4f702
Michael Gauthier <mike@silverorange.com>
2007-02-25 21:02:57 +0000

    Cleanup SwatMenu.php and remove class definitions that are now defined in seprate files.
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@15348 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/SwatMenu.php
+++ Swat/SwatMenu.php
@@ -1,133 +1,50 @@
 <?php
 
-require_once 'Swat/SwatControl.php';
+/* vim: set noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+
+require_once 'Swat/SwatAbstractMenu.php';
+require_once 'Swat/SwatMenuItem.php';
 require_once 'Swat/SwatHtmlTag.php';
-require_once 'Swat/SwatYUI.php';
 
-class SwatMenuItem extends SwatControl
-{
-	public $uri;
-	public $title;
-
-	/**
-	 * @var SwatMenu
-	 */
-	protected $sub_menu;
-
-	public function setSubMenu(SwatAbstractMenu $menu)
-	{
-		$this->sub_menu = $menu;
-	}
-
-	public function init()
-	{
-		if ($this->sub_menu !== null)
-			$this->sub_menu->init();
-	}
-
-	public function display()
-	{
-		if ($this->uri === null) {
-			echo SwatString::minimizeEntities($this->title);
-		} else {
-			$anchor_tag = new SwatHtmlTag('a');
-			$anchor_tag->href = $this->uri;
-			$anchor_tag->setContent($this->title);
-			$anchor_tag->display();
-		}
-
-		if ($this->sub_menu !== null) {
-			$this->sub_menu->display(false);
-		}
-	}
-}
-
-class SwatMenuGroup extends SwatControl
-{
-	public $title;
-
-	protected $items = array();
-
-	public function addItem(SwatMenuItem $item)
-	{
-		$this->items[] = $item;
-	}
-
-	public function display($first = false)
-	{
-		$header_tag = new SwatHtmlTag('h6');
-		if ($first)
-			$header_tag->class = 'first-of-type';
-
-		$header_tag->setContent($this->title);
-		$header_tag->display();
-
-		$ul_tag = new SwatHtmlTag('ul');
-		if ($first)
-			$ul_tag->class = 'first-of-type';
-
-		$ul_tag->open();
-
-		foreach ($this->items as $item) {
-			echo '<li class="yuimenuitem">';
-			$item->display();
-			echo '</li>';
-		}
-
-		$ul_tag->close();
-	}
-}
-
-abstract class SwatAbstractMenu extends SwatControl
-{
-	public $click_to_hide = true;
-	public $auto_sub_menu_display = true;
-
-	public function __construct($id = null)
-	{
-		parent::__construct($id);
-		$this->requires_id = true;
-
-		$yui = new SwatYUI(array('menu'));
-		$this->html_head_entry_set->addEntrySet($yui->getHtmlHeadEntrySet());
-		$this->addStyleSheet('packages/swat/styles/swat-menu.css',
-			Swat::PACKAGE_ID);
-	}
-
-	protected function getJavaScriptClass()
-	{
-		return 'YAHOO.widget.Menu';
-	}
-
-	protected function getInlineJavaScript()
-	{
-		$properties = sprintf('{ clicktohide: %s, autosubmenudisplay: %s, width: \'100px\', position: \'dynamic\' }',
-			$this->click_to_hide ? 'true' : 'false',
-			$this->auto_sub_menu_display ? 'true' : 'false');
-
-		$javascript = sprintf("var %s_obj = new %s('%s', %s);",
-			$this->id,
-			$this->getJavaScriptClass(),
-			$this->id,
-			$properties);
-
-		$javascript.= sprintf("\n%s_obj.render('new-menu');\n%s_obj.show();",
-			$this->id,
-			$this->id);
-
-		return $javascript;
-	}
-}
-
+/**
+ * A basic menu control
+ *
+ * @package   Swat
+ * @copyright 2007 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ *
+ * @see SwatMenuItem
+ */
 class SwatMenu extends SwatAbstractMenu
 {
+	// {{{ protected properties
+
+	/**
+	 * The set of SwatMenuItem objects contained in this menu
+	 *
+	 * @var array
+	 */
 	protected $items = array();
 
+	// }}}
+	// {{{ public function addItem()
+
+	/**
+	 * Adds a menu item to this menu
+	 *
+	 * @param SwatMenuItem $item the item to add.
+	 */
 	public function addItem(SwatMenuItem $item)
 	{
 		$this->items[] = $item;
 	}
 
+	// }}}
+	// {{{ public function init()
+
+	/**
+	 * Initializes this menu
+	 */
 	public function init()
 	{
 		parent::init();
@@ -135,6 +52,15 @@ class SwatMenu extends SwatAbstractMenu
 			$item->init();
 	}
 
+	// }}}
+	// {{{ public function display()
+
+	/**
+	 * Displays this menu
+	 *
+	 * @param boolean $top_level optional. Whether or not this menu is a
+	 *                            top-level menu. Defaults to true.
+	 */
 	public function display($top_level = true)
 	{
 		$displayed_classes = array();
@@ -165,60 +91,8 @@ class SwatMenu extends SwatAbstractMenu
 		if ($top_level)
 			$this->displayInlineJavaScript($this->getInlineJavaScript());
 	}
-}
 
-class SwatMenuBar extends SwatMenu
-{
-	protected function getJavaScriptClass()
-	{
-		return 'YAHOO.widget.MenuBar';
-	}
-}
-
-class SwatGroupedMenu extends SwatAbstractMenu
-{
-	protected $groups = array();
-
-	public function init()
-	{
-		parent::init();
-		foreach ($this->groups as $group)
-			$group->init();
-	}
-
-	public function addGroup(SwatMenuGroup $group)
-	{
-		$this->groups[] = $group;
-	}
-
-	public function display($top_level = true)
-	{
-		$displayed_classes = array();
-
-		$div_tag = new SwatHtmlTag('div');
-		$div_tag->id = $this->id;
-		$div_tag->class = 'yuimenu';
-		$div_tag->open();
-
-		echo '<div class="bd">';
-
-		$first = true;
-		foreach ($this->groups as $group) {
-			if ($first) {
-				$group->display(true);
-				$first = false;
-			} else {
-				$group->display();
-			}
-		}
-
-		echo '</div>';
-
-		$div_tag->close();
-
-		if ($top_level)
-			$this->displayInlineJavaScript($this->getInlineJavaScript());
-	}
+	// }}}
 }
 
 ?>
