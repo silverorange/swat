16ecae7718d098768cbe303d5637210f3fceb690
Michael Gauthier <mike@silverorange.com>
2007-03-20 00:53:04 +0000

    Code cleanups
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@16051 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/SwatNoteBookPage.php
+++ Swat/SwatNoteBookPage.php
@@ -3,35 +3,33 @@
 /* vim: set noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
 
 require_once 'Swat/SwatContainer.php';
-require_once 'Swat/SwatHtmlTag.php';
 
 /**
- * Page class for entry into SwatNoteBook
+ * A page in a {@link SwatNoteBook}
  *
  * @package   Swat
  * @copyright 2007 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ * @see       SwatNoteBook
  */
 class SwatNoteBookPage extends SwatContainer
 {
-	// {{{ public properties	
-	
+	// {{{ public properties
+
 	/**
-	 * Title
-	 *
-	 * The title of the tab.
+	 * The title of this page
 	 *
 	 * @var string
 	 */
-	public $title = null;
+	public $title;
 
 	// }}}
-	// {{{ public function __construct
+	// {{{ public function __construct()
 
 	/**
-	 * Creates a new SwatNoteBookPage
+	 * Creates a new notebook page
 	 *
-	 * @param string $id a non-visable id for this widget.
+	 * @param string $id a non-visable id for this page.
 	 */
 	public function __construct($id = null)
 	{
@@ -41,26 +39,27 @@ class SwatNoteBookPage extends SwatContainer
 	}
 
 	// }}}
-	// {{{ public function display
+	// {{{ public function display()
 
 	/**
-	 * Displays this widget
+	 * Displays this notebook page
 	 *
-	 * Displays this widget as well as recursively displays and child
-	 * widgets of this widget.
+	 * Displays this notebook page as well as recursively displaying all child-
+	 * widgets of this page.
 	 */
 	public function display()
 	{
 		if (!$this->visible)
 			return;
-		
+
 		$div_tag = new SwatHtmlTag('div');
 		$div_tag->id = $this->id;
 		$div_tag->open();
 		parent::display();
-		$div_tag->close();	
+		$div_tag->close();
 	 }
 
 	// }}}
 }
+
 ?>
