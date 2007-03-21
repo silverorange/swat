d457d41b152b360d4bc249bd894c71a7e1eae7df
Charles Waddell <charles@silverorange.com>
2007-03-21 19:14:28 +0000

    Added more widgets to the demo to show many ways SwatNoteBook can be used.
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@16080 94490950-a86c-4493-9ccb-530f72933e47

---
+++ demo/include/demos/NoteBookDemo.php
@@ -0,0 +1,66 @@
+<?php
+
+/* vim: set noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+
+require_once 'Demo.php';
+
+/**
+ * A demo using a message display
+ *
+ * @package   SwatDemo
+ * @copyright 2005-2007 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class NoteBookDemo extends Demo
+{
+	// {{{ public function buildDemoUI()
+
+	public function buildDemoUI(SwatUI $ui)
+	{
+		$short_message_display = $ui->getWidget('short_message_display');
+
+		$short_message_display->add(
+			new SwatMessage('This is a notification message.',
+			SwatMessage::NOTIFICATION));
+		
+		$short_message_display->add(new SwatMessage(
+			'This is a warning message.', SwatMessage::WARNING));
+		
+		$short_message_display->add(new SwatMessage(
+			'This is an error message.', SwatMessage::ERROR));
+		
+		$short_message_display->add(
+			new SwatMessage('This is a system error message.', 
+			SwatMessage::SYSTEM_ERROR));
+
+		$long_message_display = $ui->getWidget('long_message_display');
+
+		$message = new SwatMessage('This is a notification message.', 
+			SwatMessage::NOTIFICATION);
+
+		$message->secondary_content = 'This message has secondary content.';
+		$long_message_display->add($message);
+
+		$message = new SwatMessage('This is a warning message.', 
+			SwatMessage::WARNING);
+
+		$message->secondary_content = 'This message has secondary content.';
+		$long_message_display->add($message);
+		
+		$message = new SwatMessage('This is an error message.', 
+			SwatMessage::ERROR);
+
+		$message->secondary_content = 'This message has secondary content.';
+		$long_message_display->add($message);
+		
+		$message = new SwatMessage('This is a system error message.', 
+			SwatMessage::SYSTEM_ERROR);
+
+		$message->secondary_content = 'This message has secondary content.';
+		$long_message_display->add($message);
+	}
+
+	// }}}
+}
+
+?>
