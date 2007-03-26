2b17fbb0d3eaac6b998b607c2afeb5b1c1814967
Nathan Fredrickson <nathan@silverorange.com>
2007-03-26 16:52:47 +0000

    prepare for release of 1.2.13
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@16207 94490950-a86c-4493-9ccb-530f72933e47

--- package.php
+++ package.php
@@ -4,7 +4,7 @@
 
 require_once 'PEAR/PackageFileManager2.php';
 
-$version = '1.2.12';
+$version = '1.2.13';
 $notes = <<<EOT
 see ChangeLog
 EOT;
@@ -56,6 +56,7 @@ $package->addReplacement('Swat/Swat.php', 'pear-config', '@DATA-DIR@', 'data_dir
 $package->setPhpDep('5.1.5');
 $package->setPearinstallerDep('1.4.0');
 $package->addPackageDepWithChannel('required', 'Date', 'pear.silverorange.com', '1.4.7so1');
+$package->addPackageDepWithChannel('optional', 'Yui', 'pear.silverorange.com', '1.0.2');
 $package->addPackageDepWithChannel('optional', 'MDB2', 'pear.php.net', '2.2.2');
 $package->generateContents();
 
