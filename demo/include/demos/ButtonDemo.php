f9d51f7ae9b599a159f8561e6300ce6c213c08cc
Michael Gauthier <mike@silverorange.com>
2007-04-13 02:36:25 +0000

    Add processing throbber to button demo.
    svn commit r16808

---
+++ demo/include/demos/ButtonDemo.php
@@ -0,0 +1,36 @@
+<?php
+
+/* vim: set noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+
+require_once 'Demo.php';
+
+/**
+ * A demo using a button widgets
+ *
+ * This PHP is used to add a delay when submitting the form using throbber
+ * buttons.
+ *
+ * @package   SwatDemo
+ * @copyright 2007 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class ButtonDemo extends Demo
+{
+	// {{{ public function buildDemoUI()
+
+	public function buildDemoUI(SwatUI $ui)
+	{
+		$submit = $ui->getWidget('submit_throbber_button');
+		$confirm = $ui->getWidget('confirm_throbber_button');
+
+		$submit->process();
+		$confirm->process();
+
+		if ($submit->hasBeenClicked() || $confirm->hasBeenClicked())
+			sleep(2);
+	}
+
+	// }}}
+}
+
+?>
