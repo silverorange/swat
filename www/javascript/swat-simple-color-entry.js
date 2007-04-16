9ba4535a4b95817ed0ac0b222f26c3f578aea871
Michael Gauthier <mike@silverorange.com>
2007-04-16 19:29:17 +0000

    - better event handling for simple-color-entry JavaScript - remove use of setAttribute() as it is documented as flakey on MDC - s/cellspacing/cellSpacing to work in more browsers
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@16880 94490950-a86c-4493-9ccb-530f72933e47

--- www/javascript/swat-simple-color-entry.js
+++ www/javascript/swat-simple-color-entry.js
@@ -21,11 +21,11 @@ function SwatSimpleColorEntry(id, colors)
 	this.input_tag = document.getElementById(this.id + '_value');
 	this.swatch = document.getElementById(this.id + '_swatch');
 
-	// try to make a square palette
+	// this tries to make a square palette
 	this.columns = Math.ceil(Math.sqrt(this.colors.length));
 
 	this.current_color = null;
-	this.colorChangedEvent = new YAHOO.util.CustomEvent('colorchange');
+	this.colorChangeEvent = new YAHOO.util.CustomEvent('colorChange');
 
 	for (i = 0; i < this.colors.length; i++) {
 		if (this.input_tag.value == this.colors[i]) {
@@ -40,30 +40,32 @@ function SwatSimpleColorEntry(id, colors)
 		this.createOverlay, this, true)
 }
 
+// preload images
+SwatSimpleColorEntry.palette_image = new Image();
+SwatSimpleColorEntry.palette_image.src =
+	'packages/swat/images/color-palette.png';
+
 /**
  * Displays the toggle button for this simple color entry
  */
 SwatSimpleColorEntry.prototype.drawButton = function()
 {
 	var anchor = document.createElement('a');
-	anchor.setAttribute('href', 'javascript:' + this.id + '_obj.toggle();');
-	anchor.setAttribute('title', SwatSimpleColorEntry.open_text);
+	anchor.href = '#';
+	anchor.title = SwatSimpleColorEntry.open_text;
+	YAHOO.util.Event.addListener(anchor, 'click', this.toggle, this, true);
 
 	this.toggle_button = document.createElement('img');
-	this.toggle_button.setAttribute('id', this.id + '_toggle');
-	this.toggle_button.setAttribute('src',
-		'packages/swat/images/color-palette.png');
-
-	this.toggle_button.setAttribute('alt',
-		SwatSimpleColorEntry.toggle_alt_text);
-
+	this.toggle_button.id = this.id + '_toggle';
+	this.toggle_button.src = SwatSimpleColorEntry.palette_image.src;
+	this.toggle_button.alt = SwatSimpleColorEntry.toggle_alt_text;
 	YAHOO.util.Dom.addClass(this.toggle_button,
 		'swat-simple-color-entry-toggle');
 
 	anchor.appendChild(this.toggle_button);
 
 	this.palette = document.createElement('div');
-	this.palette.setAttribute('id', this.id + '_palette');
+	this.palette.id = this.id + '_palette';
 	YAHOO.util.Dom.addClass(this.palette, 'swat-simple-color-entry-palette');
 
 	var overlay_header = document.createElement('div');
@@ -82,7 +84,6 @@ SwatSimpleColorEntry.prototype.drawButton = function()
 	var container = document.getElementById(this.id);
 	container.appendChild(anchor);
 	container.appendChild(this.palette);
-
 }
 
 SwatSimpleColorEntry.open_text = 'open palette';
@@ -107,7 +108,7 @@ SwatSimpleColorEntry.prototype.createOverlay = function(event)
 SwatSimpleColorEntry.prototype.close = function()
 {
 	this.overlay.hide();
-	this.toggle_button.setAttribute('title', SwatSimpleColorEntry.open_text);
+	this.toggle_button.title = SwatSimpleColorEntry.open_text;
 	SwatZIndexManager.lowerElement(this.palette);
 	this.is_open = false;
 }
@@ -125,7 +126,7 @@ SwatSimpleColorEntry.prototype.open = function()
 	}
 
 	this.overlay.show();
-	this.toggle_button.setAttribute('title', SwatSimpleColorEntry.close_text);
+	this.toggle_button.title = SwatSimpleColorEntry.close_text;
 	SwatZIndexManager.raiseElement(this.palette);
 	this.is_open = true;
 }
@@ -136,7 +137,7 @@ SwatSimpleColorEntry.prototype.open = function()
 SwatSimpleColorEntry.prototype.drawPalette = function()
 {
 	var table = document.createElement('table');
-	table.setAttribute('cellspacing', '1');
+	table.cellSpacing = '1';
 
 	var tbody = document.createElement('tbody');
 
@@ -159,19 +160,18 @@ SwatSimpleColorEntry.prototype.drawPalette = function()
 		text = document.createTextNode(' '); // non-breaking UTF-8 space
 
 		if (i < this.colors.length) {
-			tcell.setAttribute('id', this.id + '_palette_' + i);
+			tcell.id = this.id + '_palette_' + i;
 			tcell.style.background = '#' + this.colors[i];
 			if (i == this.current_color)
 				YAHOO.util.Dom.addClass(tcell,
 					'swat-simple-color-entry-palette-selected');
 
 			anchor = document.createElement('a');
-			anchor.setAttribute('href', '#')
+			anchor.href = '#';
 			anchor.appendChild(text);
 
-			YAHOO.util.Event.addListener(anchor, 'click',
-				SwatSimpleColorEntry.handleClick,
-				{ entry: this, color_index: i });
+			YAHOO.util.Event.addListener(anchor, 'click', this.selectColor,
+				this, true);
 
 			tcell.appendChild(anchor);
 		} else {
@@ -199,11 +199,6 @@ SwatSimpleColorEntry.prototype.toggle = function()
 		this.open();
 }
 
-SwatSimpleColorEntry.handleClick = function(event, parameters)
-{
-	parameters['entry'].selectColor(parameters['color_index']);
-}
-
 /**
  * Sets the value of the color entry input tag to the selected color and
  * highlights the selected color
@@ -217,11 +212,19 @@ SwatSimpleColorEntry.prototype.setColor = function(color_index)
 	var changed = (this.current_color != color_index);
 	this.current_color = color_index;
 	if (changed)
-		this.colorChangedEvent.fire(this.getColor());
+		this.colorChangeEvent.fire(this.getColor());
 }
 
-SwatSimpleColorEntry.prototype.selectColor = function(color_index)
+/**
+ * Event handler that sets the color to the selected color
+ *
+ * @param Event the event that triggered this select.
+ */
+SwatSimpleColorEntry.prototype.selectColor = function(event)
 {
+	var cell = YAHOO.util.Event.getTarget(event);
+	var color_index = cell.parentNode.id.split('_palette_')[1];
+
 	if (this.current_color !== null) {
 		var old_palette_entry =
 			document.getElementById(this.id + '_palette_' + this.current_color);
