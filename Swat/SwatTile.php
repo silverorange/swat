2b766da60fcf1a1eece1ef2df4b565c068c04151
Michael Gauthier <mike@silverorange.com>
2007-04-27 15:16:56 +0000

    Add CSS classes to tile divs.
    svn commit r17219

--- Swat/SwatTile.php
+++ Swat/SwatTile.php
@@ -23,18 +23,24 @@ class SwatTile extends SwatCellRendererContainer
 	}
 	// }}}
 	// {{{ public function display()
+
 	public function display($data)
 	{
 		if (!$this->visible)
 			return;
 
-		echo '<div>';
+		$div_tag = new SwatHtmlTag('div');
+		$div_tag->class = $this->getCSSClassString();
+		$div_tag->open();
+
 		foreach ($this->renderers as $renderer){
 			$this->renderers->applyMappingsToRenderer($renderer, $data);
 			$renderer->render();
 		}
-		echo '</div>';
+
+		$div_tag->close();
 	}
+
 	// }}}
 	// {{{ public function init()
 	public function init()
@@ -81,6 +87,73 @@ class SwatTile extends SwatCellRendererContainer
 		
 		return $has_message;
 	}
+	// }}}
+	// {{{ protected function getCSSClassNames()
+
+	/**
+	 * Gets the array of CSS classes that are applied to this tile
+	 *
+	 * CSS classes are added to this tile in the following order:
+	 *
+	 * 1. hard-coded CSS classes from tile subclasses,
+	 * 2. user-specified CSS classes on this tile,
+	 * 3. the inheritance classes of the first cell renderer in this tile,
+	 * 4. hard-coded CSS classes from the first cell renderer in this tile,
+	 * 5. hard-coded data-specific CSS classes from the first cell renderer in
+	 *    this tile if this tile has data mappings applied,
+	 * 6. user-specified CSS classes on the first cell renderer in this tile.
+	 *
+	 * @return array the array of CSS classes that are applied to this tile.
+	 *
+	 * @see SwatCellRenderer::getInheritanceCSSClassNames()
+	 * @see SwatCellRenderer::getBaseCSSClassNames()
+	 * @see SwatUIObject::getCSSClassNames()
+	 */
+	protected function getCSSClassNames()
+	{
+		// base classes
+		$classes = $this->getBaseCSSClassNames();
+
+		// user-specified classes
+		$classes = array_merge($classes, $this->classes);
+
+		$first_renderer = $this->renderers->getFirst();
+		if ($first_renderer !== null) {
+			// renderer inheritance classes
+			$classes = array_merge($classes,
+				$first_renderer->getInheritanceCSSClassNames());
+
+			// renderer base classes
+			$classes = array_merge($classes,
+				$first_renderer->getBaseCSSClassNames());
+
+			// renderer data specific classes
+			if ($this->renderers->mappingsApplied())
+				$classes = array_merge($classes,
+					$first_renderer->getDataSpecificCSSClassNames());
+
+			// renderer user-specified classes
+			$classes = array_merge($classes, $first_renderer->classes);
+		}
+		return $classes;
+	}
+
+	// }}}
+	// {{{ protected function getBaseCSSClassNames()
+
+	/** 
+	 * Gets the base CSS class names of this tile
+	 *
+	 * This is the recommended place for column subclasses to add extra hard-
+	 * coded CSS classes.
+	 *
+	 * @return array the array of base CSS class names for this tile.
+	 */
+	protected function getBaseCSSClassNames()
+	{
+		return array();
+	}
+
 	// }}}
 }
 ?>
