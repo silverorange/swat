696a4b576cddf087d2286ff7a99c044877b7d44f
Nick Burka <nick@silverorange.com>
2007-05-16 00:50:23 +0000

    Add check-all to SwatTileView. I think this makes sense to add it directly in the view, versus doing it in some sort of Row (like SwatTableView). Still a little rough maybe?
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@17547 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/SwatTileView.php
+++ Swat/SwatTileView.php
@@ -4,6 +4,7 @@
 
 require_once 'Swat/SwatView.php';
 require_once 'Swat/SwatUIParent.php';
+require_once 'Swat/SwatCheckAll.php';
 require_once 'Swat/exceptions/SwatInvalidClassException.php';
 
 /**
@@ -16,6 +17,18 @@ require_once 'Swat/exceptions/SwatInvalidClassException.php';
  */
 class SwatTileView extends SwatView implements SwatUIParent
 {
+	// {{{ public properties
+
+	/**
+	 * Show check all
+	 *
+	 * Whether to show a "check all" widget.  For this option to work, the
+	 * table view must contain a column with an id of "checkbox".
+	 * @var boolean
+	 */
+	public $show_check_all = true;
+
+	// }}}
 	// {{{ private properties
 	
 	/**
@@ -25,6 +38,13 @@ class SwatTileView extends SwatView implements SwatUIParent
 	 */
 
 	private $tile = null;
+
+	/**
+	 * The check-all widget for this row 
+	 *
+	 * @var SwatCheckAll
+	 */
+	private $check_all;
 
 	// }}}
 	// {{{ public function __construct()
@@ -42,6 +62,13 @@ class SwatTileView extends SwatView implements SwatUIParent
 
 		$this->addStyleSheet('packages/swat/styles/swat-tile-view.css',
 			Swat::PACKAGE_ID);
+
+		$this->addJavaScript('packages/swat/javascript/swat-tile-view.js',
+			Swat::PACKAGE_ID);
+
+		$this->addJavaScript(
+			'packages/swat/javascript/swat-table-view-checkbox-column.js',
+			Swat::PACKAGE_ID);
 	}
 	// }}}
 	// {{{ public function init()
@@ -55,6 +82,8 @@ class SwatTileView extends SwatView implements SwatUIParent
 	 */
 	public function init()
 	{
+		$this->createEmbeddedWidgets();
+		$this->check_all->init();
 		$this->tile->init();
 	}
 	// }}}
@@ -109,22 +138,6 @@ class SwatTileView extends SwatView implements SwatUIParent
 			' add SwatTiles to this widget.', 0, $child);
 	}
 	// }}}
-	// {{{ protected function getCSSClassNames()
-
-	/**
-	 * Gets the array of CSS classes that are applied to this tile view
-	 *
-	 * @return array the array of CSS classes that are applied to this tile
-	 *                view.
-	 */
-	protected function getCSSClassNames()
-	{
-		$classes = array('swat-tile-view');
-		$classes = array_merge($classes, parent::getCSSClassNames());
-		return $classes;
-	}
-
-	// }}}
 	// {{{ public function display()
 
 	/**
@@ -143,7 +156,12 @@ class SwatTileView extends SwatView implements SwatUIParent
 		foreach ($datas as $data)
 			$this->tile->display($data);
 
+		if ($this->showCheckAll())
+			$this->check_all->display();
+
 		$tile_view_tag->close();
+
+		Swat::displayInlineJavaScript($this->getInlineJavaScript());
 	}
 	// }}}
 	// {{{ public function process()
@@ -157,6 +175,7 @@ class SwatTileView extends SwatView implements SwatUIParent
 	{
 		parent::process();
 
+		$this->check_all->process();
 		$this->tile->process();
 	}
 	// }}}
@@ -193,7 +212,104 @@ class SwatTileView extends SwatView implements SwatUIParent
 		return $has_message;
 	}
 	// }}}
-	
+	// {{{ public function getHtmlHeadEntrySet()
+
+	/**
+	 * Gets the SwatHtmlHeadEntry objects needed by this check-all row
+	 *
+	 * @return SwatHtmlHeadEntrySet the SwatHtmlHeadEntry objects needed by
+	 *                               this check-all row.
+	 *
+	 * @see SwatUIObject::getHtmlHeadEntrySet()
+	 */
+	public function getHtmlHeadEntrySet()
+	{
+		$set = parent::getHtmlHeadEntrySet();
+
+		if ($this->showCheckAll())
+			$set->addEntrySet($this->check_all->getHtmlHeadEntrySet());
+
+		return $set;
+	}
+
+	// }}}
+	// {{{ public function getInlineJavaScript()
+
+	/**
+	 * Gets the inline JavaScript required for this row
+	 *
+	 * @return string the inline JavaScript required for this row.
+	 *
+	 * @see SwatTableViewRow::getInlineJavaScript()
+	 */
+	public function getInlineJavaScript()
+	{
+		if (!$this->showCheckAll())
+			return '';
+
+		ob_start();
+
+		printf("var %s = new SwatTileView('%s');",
+			$this->id, $this->id);
+
+		// TODO: SwatTableViewCheckboxColumn has all the functionality we need,
+		// but it needs to somehow be renamed to be be shared between a
+		// TableView and TileView (SwatViewCheckbox maybe?)
+		printf("var %s = new SwatTableViewCheckboxColumn(%s, %s);",
+			$this->id, "'items'", $this->id);
+
+		echo $this->check_all->getInlineJavascript();
+
+		// set the controller of the check-all widget
+		printf("%s_obj.setController(%s);",
+			$this->check_all->id, $this->id);
+
+		return ob_get_clean();
+	}
+
+	// }}}
+	// {{{ protected function getCSSClassNames()
+
+	/**
+	 * Gets the array of CSS classes that are applied to this tile view
+	 *
+	 * @return array the array of CSS classes that are applied to this tile
+	 *                view.
+	 */
+	protected function getCSSClassNames()
+	{
+		$classes = array('swat-tile-view');
+		$classes = array_merge($classes, parent::getCSSClassNames());
+		return $classes;
+	}
+
+	// }}}
+	// {{{ protected function showCheckAll()
+
+	protected function showCheckAll()
+	{
+		if (!$this->show_check_all || $this->model->getRowCount() < 2)
+			return false;
+
+		foreach ($this->tile->getRenderers() as $renderer)
+			if ($renderer->id == 'items')
+				return true;
+
+		return false;
+	}
+	// }}}
+	// {{{ private function createEmbeddedWidgets()
+
+	/**
+	 * Creates internal widgets required for this check-all row
+	 */
+	private function createEmbeddedWidgets()
+	{ 
+			$this->check_all = new SwatCheckAll();
+			$this->check_all->parent = $this;
+	}
+
+	// }}}
 }
 
 ?>
