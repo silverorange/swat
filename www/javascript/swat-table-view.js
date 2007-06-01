8909fb582e4e5d91ad14abb12d7c3320cd04d6bc
Michael Gauthier <mike@silverorange.com>
2007-06-01 20:04:15 +0000

    Ticket #1013: Review use of checkbox column and checkboxes on TableView/TileView. Summary of changes: - using multiple selectors on a single view no longer breaks things, - new interface SwatViewSelector implemented by objects that perform selection on views, - new SwatView::getSelection() method instead of SwatView::$items property, - backwards compatible, but using the new API is encouraged, - simplify table-view checkbox column -- most functionality is moved to the cell renderer, - JavaScript handles multiple selectors on a single view, - table-view column JavaScript no longer used in tile view.
    svn commit r17840

--- www/javascript/swat-table-view.js
+++ www/javascript/swat-table-view.js
@@ -1,32 +1,112 @@
 /**
- * JavaScript for the SwatTableView widget 
+ * JavaScript for the SwatTableView widget
  *
  * @param id string Id of the matching {@link SwatTableView}.
  */
 function SwatTableView(id)
 {
-	this.id = id;
+	SwatTableView.superclass.constructor.call(this, id);
+
+	this.table_node = document.getElementById(this.id);
+
+	// look for tbody node
+	var tbody_node = null;
+	for (var i = 0; i < this.table_node.childNodes.lenght; i++) {
+		if (this.table_node.childNodes[i].nodeName == 'TBODY') {
+			tbody_node = this.tableNode.childNodes[i];
+			break;
+		}
+	}
+
+	// no tbody node, so item rows are directly in table node
+	if (tbody_node === null)
+		tbody_node = this.table_node;
+
+	for (var i = 0; i < tbody_node.childNodes.length; i++) {
+		if (tbody_node.childNodes[i].nodeName == 'TR')
+			this.items.push(tbody_node.childNodes[i]);
+	}
 }
 
-SwatTableView.prototype.highlightRow = function(node, highlight)
+YAHOO.lang.extend(SwatTableView, SwatView, {
+
+/**
+ * Gets an item node in a table-view
+ *
+ * The item node is the closest parent table row element.
+ *
+ * @param DOMElement node the arbitrary descendant node.
+ *
+ * @return DOMElement the item node.
+ */
+getItemNode: function(node)
 {
-	if (node.nodeName == 'TR') {
-		if (YAHOO.util.Dom.hasClass(node, 'odd') ||
-			YAHOO.util.Dom.hasClass(node, 'highlight-odd')) {
-			if (highlight) {
-				YAHOO.util.Dom.removeClass(node, 'odd');
-				YAHOO.util.Dom.addClass(node, 'highlight-odd');
-			} else {
-				YAHOO.util.Dom.removeClass(node, 'highlight-odd');
-				YAHOO.util.Dom.addClass(node, 'odd');
-			}
-		} else {
-			if (highlight)
-				YAHOO.util.Dom.addClass(node, 'highlight');
-			else
-				YAHOO.util.Dom.removeClass(node, 'highlight');
+	var row_node = node;
+
+	// search for containing table row element
+	while (row_node.nodeName != 'TR' && row_node.nodeName != 'BODY')
+		row_node = row_node.parentNode;
+
+	// we reached the body element without finding the row node
+	if (row_node.nodeName == 'BODY')
+		row_node = node;
+
+	return row_node;
+}
+
+});
+
+/**
+ * Selects an item node in this table-view
+ *
+ * For table-views, this method also highlights selected item rows.
+ *
+ * @param DOMElement node an arbitrary descendant node of the item node to be
+ *                         selected.
+ * @param String selector an identifier of the object that selected the item
+ *                         node.
+ */
+SwatTableView.prototype.selectItem = function(node, selector)
+{
+	SwatTableView.superclass.selectItem.call(this, node, selector);
+
+	var row_node = this.getItemNode(node);
+
+	// highlight table row of selected item in this view
+	if (this.isSelected(row_node)) {
+		if (YAHOO.util.Dom.hasClass(row_node, 'odd')) {
+			YAHOO.util.Dom.removeClass(row_node, 'odd');
+			YAHOO.util.Dom.addClass(row_node, 'highlight-odd');
+		} else if (!(YAHOO.util.Dom.hasClass(row_node, 'highlight-odd') ||
+			YAHOO.util.Dom.hasClass(row_node, 'highlight'))) {
+			YAHOO.util.Dom.addClass(row_node, 'highlight');
 		}
-	} else if (node.parentNode) {
-		this.highlightRow(node.parentNode, highlight);
+	}
+}
+
+/**
+ * Deselects an item node in this table-view
+ *
+ * For table-views, this method also unhighlights deselected item rows.
+ *
+ * @param DOMElement node an arbitrary descendant node of the item node to be
+ *                         deselected.
+ * @param String selector an identifier of the object that deselected the item
+ *                         node.
+ */
+SwatTableView.prototype.deselectItem = function(node, selector)
+{
+	SwatTableView.superclass.deselectItem.call(this, node, selector);
+
+	var row_node = this.getItemNode(node);
+
+	// unhighlight table row of item in this view
+	if (!this.isSelected(row_node)) {
+		if (YAHOO.util.Dom.hasClass(row_node, 'highlight-odd')) {
+			YAHOO.util.Dom.removeClass(row_node, 'highlight-odd');
+			YAHOO.util.Dom.addClass(row_node, 'odd');
+		} else if (YAHOO.util.Dom.hasClass(row_node, 'highlight')) {
+			YAHOO.util.Dom.removeClass(row_node, 'highlight');
+		}
 	}
 }
