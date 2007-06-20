0b0323eb53b395f40f578f821172c5dcde6bd589
Michael Gauthier <mike@silverorange.com>
2007-06-20 20:23:12 +0000

    Numerous fixes to the time entry widget. Too many to list here but it works better, trust me.
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@18105 94490950-a86c-4493-9ccb-530f72933e47

--- www/javascript/swat-time-entry.js
+++ www/javascript/swat-time-entry.js
@@ -5,7 +5,9 @@ function SwatTimeEntry(id)
 	this.hour = document.getElementById(id + '_hour');
 	this.minute = document.getElementById(id + '_minute');
 	this.second = document.getElementById(id + '_second');
-	this.ampm = document.getElementById(id + '_ampm');
+	this.am_pm = document.getElementById(id + '_am_pm');
+
+	this.twelve_hour = (this.hour !== null && this.am_pm !== null);
 
 	this.swat_date = null;
 
@@ -21,8 +23,8 @@ function SwatTimeEntry(id)
 		YAHOO.util.Event.addListener(this.second, 'change',
 			this.handleSecondChange, this, true);
 
-	if (this.ampm)
-		YAHOO.util.Event.addListener(this.ampm, 'change',
+	if (this.am_pm)
+		YAHOO.util.Event.addListener(this.am_pm, 'change',
 			this.handleAmPmChange, this, true);
 
 	this.lookup_table = {};
@@ -46,7 +48,7 @@ SwatTimeEntry.prototype.handleSecondChange = function()
 
 SwatTimeEntry.prototype.handleAmPmChange = function()
 {
-	this.update('ampm');
+	this.update('am_pm');
 }
 
 SwatTimeEntry.prototype.addLookupTable = function(table_name, table)
@@ -88,8 +90,8 @@ SwatTimeEntry.prototype.reset = function(reset_date)
 	if (this.second)
 		this.second.selectedIndex = 0;
 
-	if (this.ampm)
-		this.ampm.selectedIndex = 0;
+	if (this.am_pm)
+		this.am_pm.selectedIndex = 0;
 
 	if (this.swat_date && reset_date)
 		this.swat_date.reset(false);
@@ -98,13 +100,15 @@ SwatTimeEntry.prototype.reset = function(reset_date)
 SwatTimeEntry.prototype.setNow = function(set_date)
 {
 	var now = new Date();	
-	
-	if (now.getHours() < 12) {
-		var hour = now.getHours();
-		var ampm = 1;
-	} else {
-		var hour = (now.getHours() - 12);
-		var ampm = 2;
+
+	if (this.twelve_hour) {
+		if (now.getHours() < 12) {
+			var hour = now.getHours();
+			var am_pm = 1;
+		} else {
+			var hour = (now.getHours() - 12);
+			var am_pm = 2;
+		}
 	}
 	
 	if (this.hour && this.hour.selectedIndex == 0)
@@ -116,8 +120,8 @@ SwatTimeEntry.prototype.setNow = function(set_date)
 	if (this.second && this.second.selectedIndex == 0)
 		this.second.selectedIndex = this.lookup('second', now.getSeconds());
 		
-	if (this.ampm && this.ampm.selectedIndex == 0)
-		this.ampm.selectedIndex = ampm;
+	if (this.am_pm && this.am_pm.selectedIndex == 0)
+		this.am_pm.selectedIndex = am_pm;
 	
 	if (this.swat_date && set_date)
 		this.swat_date.setNow(false);
@@ -134,8 +138,8 @@ SwatTimeEntry.prototype.setDefault = function(set_date)
 	if (this.second && this.second.selectedIndex == 0)
 		this.second.selectedIndex = 1;
 
-	if (this.ampm && this.ampm.selectedIndex == 0)
-		this.ampm.selectedIndex = 1;
+	if (this.am_pm && this.am_pm.selectedIndex == 0)
+		this.am_pm.selectedIndex = 1;
 
 	if (this.swat_date && set_date)
 		this.swat_date.setDefault(false);
@@ -147,30 +151,36 @@ SwatTimeEntry.prototype.update = function(field)
 	if (!this.hour)
 		return;
 
-	var index = null;
+	var index;
+
 	switch (field) {
-		case 'hour':
-			index = this.hour.selectedIndex;
-			break;
-		case 'minute':
-			index = this.minute.selectedIndex;
-			break;
-		case 'second':
-			index = this.second.selectedIndex;
-			break;
-		case 'ampm':
-			index = this.ampm.selectedIndex;
-			break;
+	case 'hour':
+		index = this.hour.selectedIndex;
+		break;
+	case 'minute':
+		index = this.minute.selectedIndex;
+		break;
+	case 'second':
+		index = this.second.selectedIndex;
+		break;
+	case 'am_pm':
+		index = this.am_pm.selectedIndex;
+		break;
 	}
 
 	// don't do anything if we select the blank option
-	if (index != 0) {
+	if (index > 0) {
 		var now = new Date();	
 		var this_hour = now.getHours();
-		
-		if (this_hour > 11)
-			this_hour = this_hour - 12;
-		
+
+		if (this.twelve_hour) {
+			if (this_hour > 12)
+				this_hour -= 12;
+
+			if (this_hour == 0)
+				this_hour = 12;
+		}
+
 		if (this.reverseLookup('hour', this.hour.selectedIndex) == this_hour)
 			this.setNow(true);
 		else
