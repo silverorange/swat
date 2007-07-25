a0e854177acc8ffd54140ed547ad20cdf8087327
Nick Burka <nick@silverorange.com>
2007-07-25 22:46:38 +0000

    First commit of a SwatImagePreviewDisplay that uses YUI to display a lightbox style preview.
    Features added so far:
    - clicking anywhere after the preview is initiated will close it
    - image always fits in the viewable area of the page with padding
    
    Features we may want to add:
    - we could display the title of the image and a close link
    - it may make sense to add a border around the image
    - we can get rid of the semi-transparent background if people want
    - we could add a loading graphic - though the image usually loads quite fast
      (it may in fact be getting pre-loaded, I'm not sure)
    
    
    
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@18752 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Swat/SwatImagePreviewDisplay.php
@@ -0,0 +1,121 @@
+<?php
+
+/* vim: set noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+
+require_once 'Swat/SwatImageDisplay.php';
+
+/**
+ * Image display control
+ *
+ * This control simply displays a static image.
+ *
+ * @package   Swat
+ * @copyright 2005-2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SwatImagePreviewDisplay extends SwatImageDisplay
+{
+	// {{{ public properties
+
+	/**
+	 * Preview Image
+	 *
+	 * The src attribute in the XHTML img tag.
+	 *
+	 * @var string
+	 */
+	public $preview_image;
+
+	/**
+	 * Optional array of values to substitute into the preview image property
+	 *
+	 * Uses vsprintf() syntax, for example:
+	 *
+	 * <code>
+	 * $my_image->preview_image = 'mydir/%s.%s';
+	 * $my_image->preview_image_values = array('myfilename', 'ext');
+	 * </code>
+	 *
+	 * @var array
+	 */
+	public $preview_image_values = array();
+
+	/**
+	 * Preview Image height
+	 *
+	 * The height attribute in the XHTML img tag.
+	 *
+	 * @var integer
+	 */
+	public $preview_height = null;
+
+	/**
+	 * Preview Image width
+	 *
+	 * The width attribute in the XHTML img tag.
+	 *
+	 * @var integer
+	 */
+	public $preview_width = null;
+
+	// }}}
+	// {{{ public function __construct()
+
+	/**
+	 * Creates a new image preview display
+	 *
+	 * @param string $id a non-visible unique id for this widget.
+	 *
+	 * @see SwatWidget::__construct()
+	 */
+	public function __construct($id = null)
+	{
+		parent::__construct($id);
+
+		$yui = new SwatYUI(array('dom', 'event'));
+		$this->html_head_entry_set->addEntrySet($yui->getHtmlHeadEntrySet());
+
+		$this->addJavaScript('packages/swat/javascript/swat-image-preview-display.js',
+			Swat::PACKAGE_ID);
+
+		$this->addStyleSheet('packages/swat/styles/swat-image-preview-display.css',
+			Swat::PACKAGE_ID);
+	}
+
+	// }}}
+	// {{{ public function display()
+
+	/**
+	 * Displays this image
+	 */
+	public function display()
+	{
+		if (!$this->visible)
+			return;
+
+		parent::display();
+
+		Swat::displayInlineJavaScript($this->getInlineJavaScript());
+	}
+
+	// }}}
+	// {{{ protected function getInlineJavaScript()
+
+	/**
+	 * Gets inline JavaScript required by this image preview.
+	 *
+	 * @return string inline JavaScript needed by this widget.
+	 */
+	protected function getInlineJavaScript()
+	{
+		$javascript = sprintf("var %s = new SwatImagePreviewDisplay('%s', '%s', %d, %d);",
+			$this->id, $this->id, $this->preview_image,
+			$this->preview_width, $this->preview_height);
+
+		return $javascript;
+	}
+
+	// }}}
+}
+
+?>
