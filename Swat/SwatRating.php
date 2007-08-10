51a2aa76e155d23241afaed9c06da9fbe980e2f9
Charles Waddell <charles@silverorange.com>
2007-08-10 18:44:00 +0000

    Ticket #1127, the new rating widget for swat.
    svn commit r19324

---
+++ Swat/SwatRating.php
@@ -0,0 +1,115 @@
+<?php
+
+/* vim: set noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+
+require_once 'Swat/SwatFlydown.php';
+
+/**
+ * A javascript widget for recording a four star rating
+ *
+ * @package   Swat
+ * @copyright 2007 silverorange
+ * @lisence   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SwatRating extends SwatFlydown
+{
+	// {{{ public function __construct()
+
+	/**
+	 * Creates a new rating widget
+	 *
+	 * @param string $id a non-visible unique id for this rating widget.
+	 */
+	public function __construct($id = null)
+	{
+		parent::__construct($id);
+
+		$this->requires_id = true;
+
+		$this->addJavaScript('packages/swat/javascript/swat-rating.js',
+			Swat::PACKAGE_ID);
+
+		$this->addStyleSheet('packages/swat/styles/swat-rating.css',
+			Swat::PACKAGE_ID);
+	}
+
+	// }}}
+	// {{{ public function init()
+
+	/**
+	 * Initializes this rating widget
+	 *
+	 * Initializes this rating widget by creating and adding the options
+	 * to the flydown.
+	 */
+	public function init()
+	{
+		parent::init();
+
+		$ratings = array(
+			1 => 'One Star',
+			2 => 'Two Stars',
+			3 => 'Three Stars',
+			4 => 'Four Stars');
+
+		$this->addOptionsByArray($ratings);
+		$this->serialize_values = false;
+		$this->unique_values = true;
+	}
+
+	// }}}
+	// {{{ public function process()
+
+	/**
+	 * Processes this rating widget
+	 *
+	 * Process this rating widget and converts the value if it is a string
+	 */
+	public function process()
+	{
+		parent::process();
+
+		if (is_string($this->value)) 
+			$this->value = intval($this->value);
+	}
+
+	// }}}
+	//  {{{ public function display()
+	
+	/**
+	 * Displays this rating widget
+	 *
+	 * Displays this rating widget as a XHTML select
+	 */
+	public function display()
+	{
+
+		echo '<div id="ratingdiv">';
+
+		parent::display();
+
+		echo '</div>';		
+		Swat::displayInlineJavaScript($this->getInlineJavaScript());
+	}
+
+	// }}}
+	// {{{ protceted function getInlineJavaScript()
+
+	/**
+	 * Gets the inline JavaScript for this rating to function
+	 * 
+	 * The inline JavaScript creates an instance of the 
+	 * SwatRating widget with the name $this->id_'obj'.
+	 *
+	 * @return string the inline JavaScript required for this control to 
+	 *					function
+	 */
+	protected function getInlineJavaScript()
+	{
+		return "var {$this->id}_obj = new SwatRating('{$this->id}');";
+	}
+
+	// }}}
+}
+
+?>
