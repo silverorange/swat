2c6aceb39c106388bee9fcaf45c50ca745a404ef
Michael Gauthier <mike@silverorange.com>
2007-08-26 19:52:33 +0000

    Add recaptcha package to swat demo index.php and add a multiline comment explaining the use of package config in the demo.
    svn commit r19694

--- demo/www/index.php
+++ demo/www/index.php
@@ -2,12 +2,18 @@
 
 /* vim: set noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
 
+/*
+ * PackageConfig is a silverorange package used for configuring our internal
+ * development environment. It can be installed using the public silverorange
+ * PEAR channel but is not required for either Swat or the Swat demo.
+ */
 @include_once 'PackageConfig.php';
 if (class_exists('PackageConfig')) {
 	PackageConfig::setWorkDirPosition(3);
 	PackageConfig::addPackage('site');
 	PackageConfig::addPackage('swat');
 	PackageConfig::addPackage('jquery');
+	PackageConfig::addPackage('recaptcha');
 }
 
 require_once 'Swat/SwatAutoloader.php';
