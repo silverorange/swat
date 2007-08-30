425eb28ad37f20ebddd55c49e3e17d087ae42b84
Michael Gauthier <mike@silverorange.com>
2007-08-30 18:42:23 +0000

    Add select list to Swat demo.
    svn commit r19796

---
+++ demo/include/demos/SelectListDemo.php
@@ -0,0 +1,35 @@
+<?php
+
+/* vim: set noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+
+require_once 'Demo.php';
+
+/**
+ * A demo using a select list 
+ *
+ * @package   SwatDemo
+ * @copyright 2005-2007 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SelectListDemo extends Demo
+{
+	// {{{ public function buildDemoUI()
+
+	public function buildDemoUI(SwatUI $ui)
+	{
+		$select_list_options = array(
+			0 => 'Apple',
+			1 => 'Orange',
+			2 => 'Banana',
+			3 => 'Pear',
+			4 => 'Pineapple',
+			5 => 'Kiwi');
+
+		$select_list = $ui->getWidget('select_list');
+		$select_list->addOptionsByArray($select_list_options);
+	}
+
+	// }}}
+}
+
+?>
