79b1f70aa7da2c15dff4353b3c88764ef7e9a616
Michael Gauthier <mike@silverorange.com>
2007-08-30 18:22:53 +0000

    Select list widget.
    svn commit r19795

---
+++ Swat/SwatSelectList.php
@@ -0,0 +1,66 @@
+<?php
+
+require_once 'Swat/SwatCheckboxList.php';
+require_once 'Swat/SwatHtmlTag.php';
+
+/**
+ * List of selectable options
+ *
+ * @package   Swat
+ * @copyright 2007 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SwatSelectList extends SwatCheckboxList
+{
+	// {{{ public properties
+
+	/**
+	 * Optional number of rows in the select list
+	 *
+	 * @var integer
+	 */
+	public $size;
+
+	// }}}
+	// {{{ public function display()
+
+	/**
+	 * Displays this select list
+	 */
+	public function display()
+	{
+		$options = $this->getOptions();
+
+		if (!$this->visible || count($options) == 0)
+			return;
+
+		$this->getForm()->addHiddenField($this->id.'_submitted', 1);
+
+		$select_tag = new SwatHtmlTag('select');
+		$select_tag->id = $this->id;
+		$select_tag->name = $this->id.'[]';
+		$select_tag->class = 'swat-select-list';
+		$select_tag->multiple = 'multiple';
+		$select_tag->size = $this->size;
+		$select_tag->open();
+
+		foreach ($options as $key => $option) {
+
+			$option_tag = new SwatHtmlTag('option');
+			$option_tag->value = (string)$option->value;
+			$option_tag->id = $this->id.'_'.$key.'_'.$option_tag->value;
+			$option_tag->selected = null;
+			if (in_array($option->value, $this->values))
+				$option_tag->selected = 'selected';
+
+			$option_tag->setContent($option->title, $option->content_type);
+			$option_tag->display();
+		}
+
+		$select_tag->close();
+	}
+
+	// }}}
+}
+
+?>
