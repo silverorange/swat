silverorange/swat

commit 98302e0944db6a497ffd56dceab157efbe819d9c

Split retrieving raw value into its own protected method in SwatEntry.
git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@20318 94490950-a86c-4493-9ccb-530f72933e47

Swat/SwatEntry.php
@@ -132,27 +132,7 @@ class SwatEntry extends SwatInputControl implements SwatState
 	{
 		parent::process();
 
-		$data = &$this->getForm()->getFormData();
-
-		if ($this->autocomplete) {
-			$id = $this->id;
-		} else {
-			if (isset($data[$this->id.'_nonce'])) {
-				$id = $data[$this->id.'_nonce'];
-			} else {
-				$this->value = null;
-				return;
-			}
-		}
-
-		if (!isset($data[$id])) {
-			$this->value = null;
-			return;
-		} elseif (strlen($data[$id]) == 0) {
-			$this->value = null;
-		} else {
-			$this->value = $data[$id];
-		}
+		$this->value = $this->getRawValue();
 
 		$len = ($this->value === null) ? 0 : strlen($this->value);
 
@@ -341,6 +321,37 @@ class SwatEntry extends SwatInputControl implements SwatState
 			$this->nonce = 'n'.md5(rand());
 
 		return $this->nonce;
+	}
+
+	// }}}
+	// {{{ protected function getRawValue()
+
+	/**
+	 * Gets the raw value entered by the user before processing
+	 *
+	 * @return string the rav value entred by the user before processing.
+	 */
+	protected function getRawValue()
+	{
+		$value = null;
+
+		$data = &$this->getForm()->getFormData();
+
+		if ($this->autocomplete) {
+			$id = $this->id;
+			if (isset($data[$id]) && strlen($data[$id]) > 0) {
+				$value = $data[$id];
+			}
+		} else {
+			if (isset($data[$this->id.'_nonce'])) {
+				$id = $data[$this->id.'_nonce'];
+				if (isset($data[$id]) && strlen($data[$id]) > 0) {
+					$value = $data[$id];
+				}
+			}
+		}
+
+		return $value;
 	}
 
 	// }}}

commit 4ecb47542faa02cb78951030ac5d157c1eef8610

Fix a bug caused by r20318 that didn't shortcut return when no value was submitted by the user. This caused validation errors on widgets that were not visible on the form.
git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@20454 94490950-a86c-4493-9ccb-530f72933e47

Swat/SwatEntry.php
@@ -132,6 +132,12 @@ class SwatEntry extends SwatInputControl implements SwatState
 	{
 		parent::process();
 
+		// if nothing was submitted by the user, shortcut return 
+		if (!$this->hasRawValue()) {
+			$this->value = null;
+			return;
+		}
+
 		$this->value = $this->getRawValue();
 
 		$len = ($this->value === null) ? 0 : strlen($this->value);
@@ -331,6 +337,8 @@ class SwatEntry extends SwatInputControl implements SwatState
 	 *
 	 * @return string the rav value entred by the user before processing or
 	 *                 null if no value was entered by the user.
+	 *
+	 * @see SwatEntry::hasRawValue()
 	 */
 	protected function getRawValue()
 	{
@@ -353,6 +361,43 @@ class SwatEntry extends SwatInputControl implements SwatState
 		}
 
 		return $value;
+	}
+
+	// }}}
+	// {{{ protected function hasRawValue()
+
+	/**
+	 * Gets whether or not a value was submitted by the user for this entry
+	 *
+	 * Note: Users can submit a value of nothing and this method will return
+	 * true. This method only returns false if no data was submitted at all.
+	 *
+	 * @return boolean true if a value was submitted by the user for this entry
+	 *                  and false if no value was submitted by the user.
+	 *
+	 * @see SwatEntry::getRawValue()
+	 */
+	protected function hasRawValue()
+	{
+		$has_value = false;
+
+		$data = &$this->getForm()->getFormData();
+
+		if ($this->autocomplete) {
+			$id = $this->id;
+			if (isset($data[$id])) {
+				$has_value = true;
+			}
+		} else {
+			if (isset($data[$this->id.'_nonce'])) {
+				$id = $data[$this->id.'_nonce'];
+				if (isset($data[$id])) {
+					$has_value = true;
+				}
+			}
+		}
+
+		return $has_value;
 	}
 
 	// }}}