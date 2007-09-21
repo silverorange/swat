a8c1fe1d9c9b36e8347f9b068f5990826ed7b15b
Michael Gauthier <mike@silverorange.com>
2007-09-21 04:50:30 +0000

    Use SwatI18NLocale to format monetary values for SwatMoneyCellRenderer. This will allow custom overriding of specific format parts in the future. This also makes SwatMoneyCellRenderer work in Windows.
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@20466 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/SwatMoneyCellRenderer.php
+++ Swat/SwatMoneyCellRenderer.php
@@ -3,13 +3,13 @@
 /* vim: set noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
 
 require_once 'Swat/SwatCellRenderer.php';
-require_once 'Swat/SwatString.php';
+require_once 'SwatI18N/SwatI18NLocale.php';
 
 /**
  * A currency cell renderer
  *
  * @package   Swat
- * @copyright 2005-2006 silverorange
+ * @copyright 2005-2007 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class SwatMoneyCellRenderer extends SwatCellRenderer
@@ -31,21 +31,42 @@ class SwatMoneyCellRenderer extends SwatCellRenderer
 	public $value;
 
 	/**
-	 * Whether to display currency unit
+	 * If {@link SwatMoneyCellRenderer::$international} is false, whether to
+	 * render the international currency symbol
 	 *
-	 * If true, displays the international currency unit
+	 * If true, displays the international currency symbol. Use of this property
+	 * is discouraged in favour of using the
+	 * {@link SwatMoneyCellRendere::$international} property. Using this
+	 * property can render strings that are incorrect for the given locale.
+	 * Using the international property always renders strings correctly for the
+	 * specified locale.
+	 *
+	 * If {@link SwatMoneyCellRenderer::$international} is true, this property
+	 * has no effect.
 	 *
 	 * @var boolean
 	 */
 	public $display_currency = false;
 
 	/**
+	 * Whether or not to render the currency value using the international
+	 * format for the specified locale
+	 *
+	 * This uses the international currency symbol of the specified locale
+	 * instead of the national symbol. For example, the locale en_CA would
+	 * render $10 as CAD 10.00.
+	 *
+	 * @var boolean
+	 */
+	public $international = false;
+
+	/**
 	 * Number of decimal places to display
 	 *
-	 * If set to null, the default number of decimal places is used.
+	 * If set to null, the default number of decimal places for the specified
+	 * locale is used.
 	 *
 	 * @var integer
-	 * @see SwatString::moneyFormat()
 	 */
 	public $decimal_places = null;
 
@@ -79,10 +100,16 @@ class SwatMoneyCellRenderer extends SwatCellRenderer
 
 		parent::render();
 
+		$locale = SwatI18NLocale::get($this->locale);
+
 		echo SwatString::minimizeEntities(
-			SwatString::moneyFormat(
-				$this->value, $this->locale, $this->display_currency,
-				$this->decimal_places));
+			$locale->formatCurrency($this->value, $this->international,
+				array('fractional_digits' => $this->decimal_places)));
+
+		if (!$this->international && $this->display_currency) {
+			echo '&nbsp;', SwatString::minimizeEntities(
+			$locale->getInternationalCurrencySymbol());
+		}
 	}
 
 	// }}}
