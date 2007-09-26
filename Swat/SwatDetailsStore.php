efdc0c7b70dfd3f8b53dd0fef7b892be9edfabef
Michael Gauthier <mike@silverorange.com>
2007-09-26 18:31:21 +0000

    Add some docs.
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@20619 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/SwatDetailsStore.php
+++ Swat/SwatDetailsStore.php
@@ -18,12 +18,35 @@ class SwatDetailsStore extends SwatObject
 {
 	// {{{ private properties
 
-	private $base_object = null;
+	/**
+	 * The base object for this details store
+	 *
+	 * Properties of this details store are taken from this base object unless
+	 * they are manually specified.
+	 *
+	 * @var stdClass
+	 */
+	private $base_object;
+
+	/**
+	 * Manually set data of this details store
+	 *
+	 * @var array
+	 */
 	private $data = array();
 
 	// }}}
 	// {{{ public function __construct()
 
+	/**
+	 * Creates a new details store
+	 *
+	 * @param stdClass $base_object optional. The object to initialize this
+	 *                               details store with. Properties in this
+	 *                               details store will be taken from the base
+	 *                               object unless they are manually set on
+	 *                               this details store.
+	 */
 	public function __construct($base_object = null)
 	{
 		if ($base_object !== null && is_object($base_object)
