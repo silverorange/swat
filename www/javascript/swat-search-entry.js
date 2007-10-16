c7dc60083a3c21d8cd8d79bb3654816a59c91185
Michael Gauthier <mike@silverorange.com>
2007-10-16 18:52:47 +0000

    Fix cross-browser bugs in search-entry and fix absolute positioning of span.
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@21448 94490950-a86c-4493-9ccb-530f72933e47

--- www/javascript/swat-search-entry.js
+++ www/javascript/swat-search-entry.js
@@ -25,37 +25,105 @@ function SwatSearchEntry(id)
 		YAHOO.util.Event.addListener(this.input, 'blur', this.handleBlur,
 			this, true);
 
-		var text = document.createTextNode(this.label_text);
-		this.span = document.createElement('span');
-		YAHOO.util.Dom.addClass(this.span, 'swat-search-entry-empty');
-		this.span.appendChild(text);
-		this.span.style.position = 'absolute';
+		this.faux_input = document.createElement('input');
+		this.faux_input.setAttribute('type', 'text');
+		this.faux_input.value = this.label_text;
+		this.faux_input.size = this.input.size;
 
-		if (this.input.value == '')
-			this.span.style.display = 'block';
-		else
-			this.span.style.display = 'none';
-
-		YAHOO.util.Event.addListener(this.span, 'mousedown',
+		YAHOO.util.Dom.addClass(this.faux_input, 'swat-entry');
+		YAHOO.util.Dom.addClass(this.faux_input, 'swat-search-entry-empty');
+		YAHOO.util.Event.addListener(this.faux_input, 'mousedown',
 			this.handleMouseDown, this, true);
 
-		this.input.parentNode.insertBefore(this.span, this.input);
+		YAHOO.util.Event.addListener(this.faux_input, 'focus',
+			this.handleMouseDown, this, true);
+
+		if (this.input.value == '')
+			this.showFauxInput();
+		else
+			this.hideFauxInput();
 	}
 }
 
 SwatSearchEntry.prototype.handleMouseDown = function(e)
 {
+	YAHOO.util.Event.preventDefault(e);
+	this.hideFauxInput();
 	this.input.focus();
 }
 
 SwatSearchEntry.prototype.handleFocus = function(e)
 {
-	this.span.style.display = 'none';
+	this.hideFauxInput();
 }
 
 SwatSearchEntry.prototype.handleBlur = function(e)
 {
-	if (this.input.value == '') {
-		this.span.style.display = 'block';
+	if (this.input.value == '')
+		this.showFauxInput();
+}
+
+SwatSearchEntry.prototype.showFauxInput = function()
+{
+	// update computed styles to match real input
+	var styles = [
+		'paddingTop',        'paddingRight',
+		'paddingBottom',     'paddingLeft',
+
+		'marginTop',         'marginRight',
+		'marginBottom',      'marginLeft',
+
+		'borderTopWidth',    'borderRightWidth',
+		'borderBottomWidth', 'borderLeftWidth',
+
+		'borderTopStyle',    'borderRightStyle',
+		'borderBottomStyle', 'borderLeftStyle'
+	];
+
+	for (var i = 0; i < styles.length; i++) {
+		var current_style = YAHOO.util.Dom.getStyle(this.input, styles[i]);
+		this.faux_input.style[styles[i]] = current_style;
 	}
+
+	// hack for IE and WebKit default border styles
+	var border_styles = [
+		'borderTopStyle',    'borderRightStyle',
+		'borderBottomStyle', 'borderTopStyle'
+	];
+
+	var inset_border = false;
+	for (var i = 0; i < border_styles.length; i++) {
+		var current_style = YAHOO.util.Dom.getStyle(this.input,
+			border_styles[i]);
+
+		if (current_style == 'inset') {
+			inset_border = true;
+			break;
+		}
+	}
+
+	// only set border color if border is not inset (hack for IE and WebKit)
+	if (!inset_border) {
+		var border_colors = [
+			'borderTopColor',    'borderRightColor',
+			'borderBottomColor', 'borderLeftColor'
+		];
+
+
+		for (var i = 0; i < border_colors.length; i++) {
+			var current_style = YAHOO.util.Dom.getStyle(this.input,
+				border_colors[i]);
+
+			this.faux_input.style[border_colors[i]] = current_style;
+		}
+	}
+
+	if (this.input.parentNode)
+		this.input.parentNode.replaceChild(this.faux_input, this.input);
+}
+
+SwatSearchEntry.prototype.hideFauxInput = function()
+{
+	if (this.faux_input.parentNode)
+		this.faux_input.parentNode.replaceChild(this.input, this.faux_input);
 }
