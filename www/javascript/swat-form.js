ff38d9a0b2cca2724bf54b26f48ee4e192207899
Michael Gauthier <mike@silverorange.com>
2007-10-17 16:52:31 +0000

    Don't autofocus insensitive elements.
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@21518 94490950-a86c-4493-9ccb-530f72933e47

--- www/javascript/swat-form.js
+++ www/javascript/swat-form.js
@@ -8,9 +8,12 @@ SwatForm.prototype.setDefaultFocus = function(element_id)
 {
 	// TODO: check if another element in this form is already focused
 
-	var element = document.getElementById(element_id);
+	function is_function(obj)
+	{
+		return (typeof obj == 'function' || typeof obj == 'object');
+	}
 
-	if (element && (typeof element.focus == 'function' ||
-		typeof element.focus == 'object'))
-			element.focus();
+	var element = document.getElementById(element_id);
+	if (element && element.disabled == false && is_function(element.focus))
+		element.focus();
 }
