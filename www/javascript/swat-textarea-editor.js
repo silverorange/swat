6480bbd80b8cedc5773c9f0123f5fa7e5b24f7ab
Michael Gauthier <mike@silverorange.com>
2008-02-04 20:31:39 +0000

    Updates to the RTE to show a source-code editor, set up translations (not complete yet) and use the simple editor instead of the full editor.
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@27061 94490950-a86c-4493-9ccb-530f72933e47

--- www/javascript/swat-textarea-editor.js
+++ www/javascript/swat-textarea-editor.js
@@ -3,6 +3,7 @@ function SwatTextareaEditor(id, width, height)
 	this.id = id;
 	this.height = height;
 	this.width = width;
+	this.show_source_editor = false;
 
 	YAHOO.util.Event.onDOMReady(this.init, this, true);
 }
@@ -17,7 +18,7 @@ SwatTextareaEditor.prototype.init = function()
 		titlebar: 'Text'
 	};
 
-	this.editor = new YAHOO.widget.Editor(this.id, config);
+	this.editor = new YAHOO.widget.SimpleEditor(this.id, config);
 
 	this.editor._defaultToolbar.buttonType = 'basic';
 	this.editor._defaultToolbar.titlebar = '';
@@ -79,5 +80,85 @@ SwatTextareaEditor.prototype.init = function()
 		}
 	];
 
+	this.editor.on('toolbarLoaded', this.addSourceEditButton, this, true);
+	this.editor.on('cleanHTML', function(ev)
+	{
+		this.get('element').value = ev.html;
+	}, this.editor, true);
+
+	this.editor.on('afterRender', function()
+	{
+		var wrapper = this.get('editor_wrapper');
+		wrapper.appendChild(this.get('element'));
+		this.setStyle('width', '100%');
+		this.setStyle('height', '100%');
+		this.setStyle('visibility', '');
+		this.setStyle('top', '');
+		this.setStyle('left', '');
+		this.setStyle('position', '');
+
+		this.addClass('swat-textarea-editor-hidden');
+	}, this.editor, true);
+
 	this.editor.render();
+}
+
+SwatTextareaEditor.prototype.addSourceEditButton = function()
+{
+	var source_edit_button_config = {
+		type: 'push', label: 'Edit HTML Code', value: 'editcode'
+	};
+
+	this.editor.toolbar.addButtonToGroup(source_edit_button_config,
+		'insertitem');
+
+	this.editor.toolbar.on('editcodeClick', this.toggleSourceEditor,
+		this, true);
+}
+
+SwatTextareaEditor.prototype.toggleSourceEditor = function()
+{
+	if (this.show_source_editor)
+		this.hideSourceEditor();
+	else
+		this.showSourceEditor();
+}
+
+SwatTextareaEditor.prototype.showSourceEditor = function()
+{
+	if (!this.show_source_editor) {
+		var ta = this.editor.get('element');
+		var iframe = this.editor.get('iframe').get('element');
+
+		this.editor.cleanHTML();
+		YAHOO.util.Dom.addClass(iframe, 'swat-textarea-editor-hidden');
+		YAHOO.util.Dom.removeClass(ta, 'swat-textarea-editor-hidden');
+		this.editor.toolbar.set('disabled', true);
+		this.editor.toolbar.getButtonByValue('editcode').set('disabled', false);
+		this.editor.toolbar.selectButton('editcode');
+		this.editor.dompath.innerHTML = 'Editing HTML Code';
+		this.editor.hide();
+
+		this.show_source_editor = true;
+	}
+}
+
+SwatTextareaEditor.prototype.hideSourceEditor = function()
+{
+	if (this.show_source_editor) {
+		var ta = this.editor.get('element');
+		var iframe = this.editor.get('iframe').get('element');
+
+		this.editor.toolbar.set('disabled', false);
+		this.editor.setEditorHTML(ta.value)
+		if (!this.editor.browser.ie) {
+			this.editor._setDesignMode('on');
+		}
+		YAHOO.util.Dom.removeClass(iframe, 'swat-textarea-editor-hidden');
+		YAHOO.util.Dom.addClass(ta, 'swat-textarea-editor-hidden');
+		this.editor.show();
+		this.editor._focusWindow();
+
+		this.show_source_editor = false;
+	}
 }
