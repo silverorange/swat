3c4a587041b223b2f09444f9d794adca2ddaa6d0
Michael Gauthier <mike@silverorange.com>
2008-02-26 20:58:42 +0000

    New HTML head entry type for links
    git-svn-id: file:///Users/gauthierm/svnrepo/swat/trunk@28141 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Swat/SwatLinkHtmlHeadEntry.php
@@ -0,0 +1,99 @@
+<?php
+
+/* vim: set noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+
+require_once 'Swat/SwatHtmlHeadEntry.php';
+require_once 'Swat/SwatHtmlTag.php';
+
+/**
+ * Stores and outputs an HTML head entry for an XHTML link element
+ *
+ * @package   Swat
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SwatLinkHtmlHeadEntry extends SwatHtmlHeadEntry
+{
+	// {{{ protected properties
+
+	/**
+	 * The URI linked to by this link
+	 *
+	 * @var string
+	 */
+	protected $uri;
+
+	/**
+	 * The title of this link
+	 *
+	 * @var string
+	 */
+	protected $title;
+
+	/**
+	 * How this link relates to the containing document
+	 *
+	 * @var string
+	 */
+	protected $relationship;
+
+	/**
+	 * The media type of the content linked to by this link
+	 *
+	 * @var string
+	 */
+	protected $type;
+
+	// }}}
+	// {{{ public function __construct()
+
+	/**
+	 * Creates a new link HTML head entry
+	 *
+	 * @param string $uri the URI linked to by this link.
+	 * @param string $relationship optional. How this link relates to the
+	 *                              containing document.
+	 * @param string $type optional. The media type of the content linked to by
+	 *                      this link.
+	 * @param string $title optional. The title of this link.
+	 *
+	 * @param integer $package_id the package id of the package this HTML head
+	 *                             entry belongs to.
+	 */
+	public function __construct($uri, $relationship = null, $type = null,
+		$title = null, $package_id = null)
+	{
+		$hash = md5($uri.$relationship.$type.$title);
+		parent::__construct($hash, $package_id);
+
+		$this->uri = $uri;
+		$this->relationship = $relationship;
+		$this->type = $type;
+		$this->title = $title;
+	}
+
+	// }}}
+	// {{{ public function display()
+
+	public function display($uri_prefix = '')
+	{
+		$link = new SwatHtmlTag('link');
+		$link->title = $this->title;
+		$link->rel = $this->relationship;
+		$link->type = $this->type;
+		$link->href = $this->uri;
+		$link->display();
+	}
+
+	// }}}
+	// {{{ public function displayInline()
+
+	public function displayInline($path)
+	{
+		$this->display();
+	}
+
+	// }}}
+}
+
+?>
