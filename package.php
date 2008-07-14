f7268fe00b2434e3a8e3b2bcbe88614ed1e5104c
Keith Burgoyne <keith@silverorange.com>
2008-07-14 16:14:31 +0000

    prepare for release of 1.3.35
    git-svn-id: file:///Users/gauthierm/svnrepo/public/swat/trunk@34848 94490950-a86c-4493-9ccb-530f72933e47

--- package.php
+++ package.php
@@ -4,7 +4,7 @@
 
 require_once 'PEAR/PackageFileManager2.php';
 
-$version = '1.3.34';
+$version = '1.3.35';
 $notes = <<<EOT
 * made it possible to set the 'next' and 'previous' text for SwatPagination
 * added CSS classes to all SwatEntry widgets
