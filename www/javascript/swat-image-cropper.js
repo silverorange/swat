68198fc011f1b42a62de3587d09c88aaf19a70d3
Michael Gauthier <mike@silverorange.com>
2008-10-24 18:39:16 +0000

    Update SwatImageCropper to use YUI 2.6.0 imagecropper. Also: * fix crop_ratio to by width:height instead of height:width * rename crop_box_* properties as crop_* * fix autosizing when a crop_ratio is specified.
    svn commit r38109

--- www/javascript/swat-image-cropper.js
+++ www/javascript/swat-image-cropper.js
@@ -1,8 +1,9 @@
 function SwatImageCropper(id, config)
 {
 	this.id = id;
+
 	this.cropper = new YAHOO.widget.ImageCropper(this.id + '_image', config);
-	this.cropper.onChangeEvent.subscribe(this.handleChange, this, true);
+	this.cropper.on('moveEvent', this.handleChange, this, true);
 
 	this.crop_box_width = document.getElementById(this.id + '_width');
 	this.crop_box_height = document.getElementById(this.id + '_height');
@@ -12,10 +13,9 @@ function SwatImageCropper(id, config)
 
 SwatImageCropper.prototype.handleChange = function()
 {
-	var region = this.cropper.getCropRegion();
-
-	this.crop_box_width.value = region.w;
-	this.crop_box_height.value = region.h;
-	this.crop_box_x.value = region.x;
-	this.crop_box_y.value = region.y;
+	var coords = this.cropper.getCropCoords();
+	this.crop_box_width.value  = coords.width;
+	this.crop_box_height.value = coords.height;
+	this.crop_box_x.value      = coords.left;
+	this.crop_box_y.value      = coords.top;
 }
