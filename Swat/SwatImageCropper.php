2b767d6f63b91257e995c49f64d702c28163504d
Michael Gauthier <mike@silverorange.com>
2008-10-24 18:39:16 +0000

    Update SwatImageCropper to use YUI 2.6.0 imagecropper. Also: * fix crop_ratio to by width:height instead of height:width * rename crop_box_* properties as crop_* * fix autosizing when a crop_ratio is specified.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/swat/trunk@38109 94490950-a86c-4493-9ccb-530f72933e47

--- Swat/SwatImageCropper.php
+++ Swat/SwatImageCropper.php
@@ -12,15 +12,8 @@ require_once 'Swat/SwatYUI.php';
  * This widget uses JavaScript to present an adjustable boundry to define how
  * an image should be cropped.
  *
- * This uses a YUI-based image cropping widget licensed under a BSD license
- * written by Julien Lecomte. Since this widget is not distributed with YUI,
- * the code is included in Swat. There are small local modifications present in
- * Swat to fix some CSS issues in IE6. See
- * {@link http://www.julienlecomte.net/blog/2007/07/24/yui-based-image-cropper-widget/}
- * for details about the YUI-based widget.
- *
  * @package   Swat
- * @copyright 2007 silverorange
+ * @copyright 2008 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class SwatImageCropper extends SwatInputControl
@@ -53,33 +46,78 @@ class SwatImageCropper extends SwatInputControl
 	 *
 	 * @var float
 	 */
-	public $crop_box_ratio;
+	public $crop_ratio;
 
 	/**
 	 * Width of the crop bounding box
 	 *
 	 * @var integer
 	 */
-	public $crop_box_width;
+	public $crop_width;
 
 	/**
 	 * Height of the crop bounding box
 	 *
 	 * @var integer
 	 */
-	public $crop_box_height;
+	public $crop_height;
 
 	/**
 	 * Position of the left side of the crop bounding box
 	 *
 	 * @var integer
 	 */
-	public $crop_box_left;
+	public $crop_left;
 
 	/**
 	 * Position of the top side of the crop bounding box
 	 *
 	 * @var integer
+	 */
+	public $crop_top;
+
+	/**
+	 * Alias for {@link SwatImageCropper::$crop_ratio}
+	 *
+	 * @var float
+	 *
+	 * @deprecated Use {@link SwatImageCropper::$crop_ratio} instead.
+	 */
+	public $crop_box_ratio;
+
+	/**
+	 * Alias for {@link SwatImageCropper::$crop_width}
+	 *
+	 * @var integer
+	 *
+	 * @deprecated Use {@link SwatImageCropper::$crop_width} instead.
+	 */
+	public $crop_box_width;
+
+	/**
+	 * Alias for {@link SwatImageCropper::$crop_height}
+	 *
+	 * @var integer
+	 *
+	 * @deprecated Use {@link SwatImageCropper::$crop_height} instead.
+	 */
+	public $crop_box_height;
+
+	/**
+	 * Alias for {@link SwatImageCropper::$crop_left}
+	 *
+	 * @var integer
+	 *
+	 * @deprecated Use {@link SwatImageCropper::$crop_left} instead.
+	 */
+	public $crop_box_left;
+
+	/**
+	 * Alias for {@link SwatImageCropper::$crop_top}
+	 *
+	 * @var integer
+	 *
+	 * @deprecated Use {@link SwatImageCropper::$crop_top} instead.
 	 */
 	public $crop_box_top;
 
@@ -99,16 +137,8 @@ class SwatImageCropper extends SwatInputControl
 
 		$this->requires_id = true;
 
-		$yui = new SwatYUI(array('dom', 'event', 'dragdrop'));
+		$yui = new SwatYUI(array('imagecropper'));
 		$this->html_head_entry_set->addEntrySet($yui->getHtmlHeadEntrySet());
-
-		$this->addJavaScript(
-			'packages/swat/javascript/swat-yui-image-cropper.js',
-			SwatYUI::PACKAGE_ID);
-
-		$this->addStyleSheet(
-			'packages/swat/styles/swat-yui-image-cropper.css',
-			SwatYUI::PACKAGE_ID);
 
 		$this->addJavaScript('packages/swat/javascript/swat-image-cropper.js',
 			Swat::PACKAGE_ID);
@@ -123,10 +153,16 @@ class SwatImageCropper extends SwatInputControl
 
 		$data = $this->getForm()->getFormData();
 
-		$this->crop_box_width  = $data[$this->id.'_width'];
-		$this->crop_box_height = $data[$this->id.'_height'];
-		$this->crop_box_left   = $data[$this->id.'_x'];
-		$this->crop_box_top    = $data[$this->id.'_y'];
+		$this->crop_width  = $data[$this->id.'_width'];
+		$this->crop_height = $data[$this->id.'_height'];
+		$this->crop_left   = $data[$this->id.'_x'];
+		$this->crop_top    = $data[$this->id.'_y'];
+
+		// deprecated aliases
+		$this->crop_box_width  = $this->crop_width;
+		$this->crop_box_height = $this->crop_height;
+		$this->crop_box_left   = $this->crop_left;
+		$this->crop_box_top    = $this->crop_top;
 	}
 
 	// }}}
@@ -146,7 +182,7 @@ class SwatImageCropper extends SwatInputControl
 
 		$div_tag = new SwatHtmlTag('div');
 		$div_tag->id = $this->id;
-		$div_tag->class = 'swat-image-cropper';
+		$div_tag->class = 'swat-image-cropper yui-skin-sam';
 		$div_tag->open();
 
 		$image_tag = new SwatHtmlTag('img');
@@ -162,22 +198,22 @@ class SwatImageCropper extends SwatInputControl
 
 		$input_tag->id = $this->id.'_width';
 		$input_tag->name = $this->id.'_width';
-		$input_tag->value = $this->crop_box_width;
+		$input_tag->value = $this->crop_width;
 		$input_tag->display();
 
 		$input_tag->id = $this->id.'_height';
 		$input_tag->name = $this->id.'_height';
-		$input_tag->value = $this->crop_box_height;
+		$input_tag->value = $this->crop_height;
 		$input_tag->display();
 
 		$input_tag->id = $this->id.'_x';
 		$input_tag->name = $this->id.'_x';
-		$input_tag->value = $this->crop_box_left;
+		$input_tag->value = $this->crop_left;
 		$input_tag->display();
 
 		$input_tag->id = $this->id.'_y';
 		$input_tag->name = $this->id.'_y';
-		$input_tag->value = $this->crop_box_top;
+		$input_tag->value = $this->crop_top;
 		$input_tag->display();
 
 		$div_tag->close();
@@ -197,30 +233,33 @@ class SwatImageCropper extends SwatInputControl
 	{
 		$options = array();
 
-		if ($this->crop_box_width !== null)
-			$options['w'] = $this->crop_box_width;
+		if ($this->crop_width !== null)
+			$options['initWidth'] = $this->crop_width;
 
-		if ($this->crop_box_height !== null)
-			$options['h'] = $this->crop_box_height;
+		if ($this->crop_height !== null)
+			$options['initHeight'] = $this->crop_height;
 
-		if ($this->crop_box_left !== null)
-			$options['x'] = $this->crop_box_left;
+		if ($this->crop_left !== null && $this->crop_top !== null) {
+			$options['initialXY'] =
+				'['.$this->crop_left.', '.$this->crop_top.']';
+		}
 
-		if ($this->crop_box_top !== null)
-			$options['y'] = $this->crop_box_top;
+		if ($this->crop_ratio !== null) {
+			$options['ratio'] = 'true';
+		}
 
-		if ($this->crop_box_ratio !== null)
-			$options['xyratio'] = $this->crop_box_ratio;
+		$options['status'] = 'false';
 
 		$options_string = '';
 		$first = true;
+
 		foreach ($options as $key => $value) {
 			if ($first)
 				$first = false;
 			else
 				$options_string.= ', ';
 
-			$options_string.= sprintf("%s: %F", $key, $value);
+			$options_string.= sprintf("%s: %s", $key, $value);
 		}
 
 		return sprintf("%1\$s_obj = new SwatImageCropper(".
@@ -234,68 +273,97 @@ class SwatImageCropper extends SwatInputControl
 	 * Automatically sets crop box dimensions if they are not specified and
 	 * constrains crop box dimensions to image size
 	 *
-	 * Crop dimensions are automatically set as large as possible and centred
-	 * on the image if they are not specified. If the specified crop dimensions
-	 * are outside the image dimensions, the x and y coordinates are first
-	 * placed inside the image and then the width and height are adjusted to
-	 * make the crop box fit inside the image dimensions.
+	 * Crop dimensions are automatically set as at 50% and centered on the
+	 * image if they are not specified. If the specified crop dimensions are
+	 * outside the image dimensions, the x and y coordinates are first placed
+	 * inside the image and then the width and height are adjusted to make the
+	 * the crop box fit inside the image dimensions.
 	 */
 	protected function autoCropBoxDimensions()
 	{
+		// support deprecated aliases
+		if ($this->crop_box_width !== null && $this->crop_width === null) {
+			$this->crop_width = $this->crop_box_width;
+		}
+
+		if ($this->crop_box_height !== null && $this->crop_height === null) {
+			$this->crop_height = $this->crop_box_height;
+		}
+
+		if ($this->crop_box_top !== null && $this->crop_top === null) {
+			$this->crop_top = $this->crop_box_top;
+		}
+
+		if ($this->crop_box_left !== null && $this->crop_left === null) {
+			$this->crop_left = $this->crop_box_left;
+		}
+
+		if ($this->crop_box_ratio !== null && $this->crop_ratio === null) {
+			$this->crop_ratio = $this->crop_box_ratio;
+		}
+
 		// fix bad ratio
 		if ($this->crop_box_ratio == 0)
 			$this->crop_box_ratio = null;
 
 		// autoset width
-		if ($this->crop_box_width === null) {
-			if ($this->crop_box_ratio === null || $this->crop_box_ratio <= 1)
-				$this->crop_box_width = round($this->image_width * 0.95);
-			else
-				$this->crop_box_width =
-					round($this->image_height / $this->crop_box_ratio);
+		if ($this->crop_width === null) {
+			if ($this->crop_ratio === null) {
+				$this->crop_width = round($this->image_width * 0.5);
+			} elseif ($this->crop_ratio > 1) {
+				$this->crop_width = $this->image_width;
+			} else {
+				$this->crop_width =
+					round($this->image_height * $this->crop_ratio);
+			}
 		}
 
 		// autoset height
-		if ($this->crop_box_height === null) {
-			if ($this->crop_box_ratio === null || $this->crop_box_ratio > 1)
-				$this->crop_box_height = round($this->image_height * 0.95);
-			else
-				$this->crop_box_height =
-					round($this->image_width * $this->crop_box_ratio);
+		if ($this->crop_height === null) {
+			if ($this->crop_ratio === null) {
+				$this->crop_height = round($this->image_height * 0.5);
+			} elseif ($this->crop_ratio <= 1) {
+				$this->crop_height = $this->image_height;
+			} else {
+				$this->crop_height =
+					round($this->image_width / $this->crop_ratio);
+			}
 		}
 
 		// autoset left
-		if ($this->crop_box_left === null) {
-			if ($this->crop_box_width < $this->image_width)
-				$this->crop_box_left =
-					round(($this->image_width - $this->crop_box_width) / 2);
-			else
-				$this->crop_box_left = 0;
+		if ($this->crop_left === null) {
+			if ($this->crop_width < $this->image_width) {
+				$this->crop_left =
+					round(($this->image_width - $this->crop_width) / 2);
+			} else {
+				$this->crop_left = 0;
+			}
 		}
 
 		// autoset top
-		if ($this->crop_box_top === null) {
-			if ($this->crop_box_height < $this->image_height)
-				$this->crop_box_top =
-					round(($this->image_height - $this->crop_box_height) / 2);
-			else
-				$this->crop_box_top = 0;
+		if ($this->crop_top === null) {
+			if ($this->crop_height < $this->image_height) {
+				$this->crop_top =
+					round(($this->image_height - $this->crop_height) / 2);
+			} else {
+				$this->crop_top = 0;
+			}
 		}
 
 		// constrain dimensions to image size
-		$this->crop_box_left = max($this->crop_box_left, 0);
-		$this->crop_box_left =
-			min($this->crop_box_left, $this->image_width - 2);
+		$this->crop_left = max($this->crop_left, 0);
+		$this->crop_left = min($this->crop_left, $this->image_width - 2);
 
-		$this->crop_box_top = max($this->crop_box_top, 0);
-		$this->crop_box_top =
-			min($this->crop_box_top, $this->image_height - 2);
+		$this->crop_top = max($this->crop_top, 0);
+		$this->crop_top = min($this->crop_top, $this->image_height - 2);
 
-		if ($this->crop_box_left + $this->crop_box_width > $this->image_width)
-			$this->crop_box_width = $this->image_width - $this->crop_box_left;
+		if ($this->crop_left + $this->crop_width > $this->image_width) {
+			$this->crop_width = $this->image_width - $this->crop_left;
+		}
 
-		if ($this->crop_box_top + $this->crop_box_height > $this->image_height)
-			$this->crop_box_height = $this->image_height - $this->crop_box_top;
+		if ($this->crop_top + $this->crop_height > $this->image_height) {
+			$this->crop_height = $this->image_height - $this->crop_top;
+		}
 	}
 
 	// }}}
