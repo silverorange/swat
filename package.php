b3e1366e6df57ddf91d840331a90a7d87970f799
Keith Burgoyne <keith@silverorange.com>
2009-02-05 15:15:29 +0000

    prepare for release of 1.3.63
    git-svn-id: file:///Users/gauthierm/svnrepo/public/swat/trunk@42157 94490950-a86c-4493-9ccb-530f72933e47

--- package.php
+++ package.php
@@ -4,7 +4,7 @@
 
 require_once 'PEAR/PackageFileManager2.php';
 
-$version = '1.3.62';
+$version = '1.3.63';
 $notes = <<<EOT
 * Fix bug in SwatDBRecordsetWrapper::loadAllSubRecordsets()
 * add optional extended-check-all to checkall widget
