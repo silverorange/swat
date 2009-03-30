faf78979b547ad67b3e29046c44763f0d1630cb3
Isaac Grant <isaac@silverorange.com>
2009-03-30 17:57:41 +0000

    new radio list that mirrors SwatYesNoFlydown
    git-svn-id: file:///Users/gauthierm/svnrepo/public/swat/trunk@44329 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Swat/SwatYesNoRadioList.php
@@ -0,0 +1,43 @@
+<?php
+
+/* vim: set noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+
+require_once 'Swat/SwatRadioList.php';
+
+/**
+ * A radio list selection widget for a Yes/No option.
+ *
+ * @package   Swat
+ * @copyright 2009 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SwatYesNoRadioList extends SwatRadioList
+{
+	// {{{ constants
+
+	const NO  = false;
+	const YES = true;
+
+	// }}}
+	// {{{ public function __construct()
+
+	/**
+	 * Creates a new yes/no radio list
+	 *
+	 * Sets the options of this radio list to be yes and no.
+	 *
+	 * @param string $id a non-visible unique id for this widget.
+	 *
+	 * @see SwatWidget::__construct()
+	 */
+	public function __construct($id = null)
+	{
+		parent::__construct($id);
+		$this->addOption(self::NO,  Swat::_('No'));
+		$this->addOption(self::YES, Swat::_('Yes'));
+	}
+
+	// }}}
+}
+
+?>
