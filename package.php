c50a8e8a5652741755bb18555ea7bd363d100905
Keith Burgoyne <keith@silverorange.com>
2009-04-29 20:10:34 +0000

    prepare for release of 1.3.71
    svn commit r45158

--- package.php
+++ package.php
@@ -4,7 +4,7 @@
 
 require_once 'PEAR/PackageFileManager2.php';
 
-$version = '1.3.70';
+$version = '1.3.71';
 $notes = <<<EOT
 No release notes for you!
 EOT;
@@ -32,6 +32,7 @@ $result = $package->setOptions(
 			'www'      => 'data',
 			'demo'     => 'data',
 			'system'   => 'data',
+			'/'        => 'data'
 		),
 	)
 );
