69a83e6f83738cb0d5720a4518132eb35402dab3
Michael Gauthier <mike@silverorange.com>
2009-05-26 20:42:11 +0000

    Remove unused lines and set editor to use compressed files.
    svn commit r45914

--- Swat/SwatTextareaEditor.php
+++ Swat/SwatTextareaEditor.php
@@ -84,7 +84,7 @@ class SwatTextareaEditor extends SwatTextarea
 		$this->rows = 30;
 
 		$this->addJavaScript(
-			'packages/swat/javascript/tiny_mce/tiny_mce_src.js',
+			'packages/swat/javascript/tiny_mce/tiny_mce.js',
 			Swat::PACKAGE_ID);
 	}
 
@@ -208,10 +208,6 @@ class SwatTextareaEditor extends SwatTextarea
 			'theme_advanced_toolbar_location'   => 'top',
 			'theme_advanced_toolbar_align'      => 'left',
 			'theme_advanced_blockformats'       => $formats,
-//			'theme_advanced_resizing'           => true,
-//			'theme_advanced_resize_horizontal'  => false,
-//			'theme_advanced_path'               => false,
-//			'theme_advanced_statusbar_location' => 'bottom',
 			'skin'                              => 'swat',
 			'plugins'                           => 'swat,media',
 		);
