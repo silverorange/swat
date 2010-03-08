c4955c9147687c08ec2a0fcd9effd3f98a894825
Steven Garrity <steven@silverorange.com>
2010-03-08 16:32:54 +0000

    Add white, black, and 50% grey to Tango palette to round out the number of colors
    svn commit r53439

--- Swat/SwatSimpleColorEntry.php
+++ Swat/SwatSimpleColorEntry.php
@@ -46,11 +46,11 @@ class SwatSimpleColorEntry extends SwatAbstractOverlay
 	 * @var array
 	 */
 	public $colors = array(
-		'eeeeec', 'd3d7cf', 'babdb6', '888a85', '555753', '2e3436',
-		'fce94f', 'edd400', 'c4a000', 'fcaf3e', 'f57900', 'ce5c00',
-		'e9b96e', 'c17d11', '8f5902', '8ae234', '73d216', '4e9a06',
-		'729fcf', '3465a4', '204a87', 'ad7fa8', '75507b', '5c3566',
-		'ef2929', 'cc0000', 'a40000',
+		'ffffff', 'eeeeec', 'd3d7cf', 'babdb6', '888a85', '666666',
+		'555753', '2e3436', '000000', 'fce94f', 'edd400', 'c4a000',
+		'fcaf3e', 'f57900', 'ce5c00', 'e9b96e', 'c17d11', '8f5902',
+		'8ae234', '73d216', '4e9a06', '729fcf', '3465a4', '204a87',
+		'ad7fa8', '75507b', '5c3566', 'ef2929', 'cc0000', 'a40000',
 		);
 
 	// }}}
