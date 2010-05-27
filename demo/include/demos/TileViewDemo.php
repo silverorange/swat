d4ae984cf440777941fe6cc917f43e882a2473a0
Michael Gauthier <mike@silverorange.com>
2010-05-27 13:05:35 +0000

    ws
    svn commit r55493

--- demo/include/demos/TileViewDemo.php
+++ demo/include/demos/TileViewDemo.php
@@ -18,12 +18,72 @@ class TileViewDemo extends Demo
 	public function buildDemoUI(SwatUI $ui)
 	{
 		$data = array(
-			array('images/apple.png', 28, 28, 'Apple', 'red', false, true,
-				new SwatDate('2005-09-01'), 0.5),
-			array('images/orange.png', 28, 28, 'Orange', 'orange', false, false,
-				new SwatDate('2005-04-20'), 0.75),
-			array('images/strawberry.png', 28, 28, 'Strawberry', 'red', true, false,
-				new SwatDate('2005-07-05'), 0.6)
+			array(
+				'images/apple.png',
+				28,
+				28,
+				'Apple',
+				'red',
+				false,
+				true,
+				new SwatDate('2005-09-20'),
+				0.5,
+			),
+			array(
+				'images/orange.png',
+				28,
+				28,
+				'Orange',
+				'orange',
+				false,
+				false,
+				new SwatDate('2005-09-20'),
+				0.75,
+			),
+			array(
+				'images/apple.png',
+				28,
+				28,
+				'Rotten Berry',
+				'rotten',
+				false,
+				true,
+				new SwatDate('2005-09-20'),
+				0.5,
+			),
+			array(
+				'images/strawberry.png',
+				28,
+				28,
+				'Strawberry',
+				'red',
+				true,
+				false,
+				new SwatDate('2005-07-05'),
+				0.6,
+			),
+			array(
+				'images/strawberry.png',
+				28,
+				28,
+				'Mushberry',
+				'mush',
+				true,
+				false,
+				new SwatDate('2005-07-05'),
+				0.6,
+			),
+			array(
+				'images/orange.png',
+				28,
+				28,
+				'Grapefruit',
+				'grapefruit',
+				false,
+				false,
+				new SwatDate('2005-04-20'),
+				0.75,
+			),
 		);
 
 		$tile_view = $ui->getWidget('tile_view');
@@ -31,15 +91,16 @@ class TileViewDemo extends Demo
 
 		foreach ($data as $datum) {
 			$fruit = new FruitObject();
-			$fruit->image = $datum[0];
-			$fruit->image_width = $datum[1];
+
+			$fruit->image        = $datum[0];
+			$fruit->image_width  = $datum[1];
 			$fruit->image_height = $datum[2];
-			$fruit->title = $datum[3];
-			$fruit->color = $datum[4];
-			$fruit->makes_jam = $datum[5];
-			$fruit->makes_pie = $datum[6];
+			$fruit->title        = $datum[3];
+			$fruit->color        = $datum[4];
+			$fruit->makes_jam    = $datum[5];
+			$fruit->makes_pie    = $datum[6];
 			$fruit->harvest_date = $datum[7];
-			$fruit->cost = $datum[8];
+			$fruit->cost         = $datum[8];
 
 			$table_store->addRow($fruit);
 		}
