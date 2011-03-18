cd8c83b038bd58bd50de06084397666d9c04639d
Michael Gauthier <mike@silverorange.com>
2011-03-18 20:00:26 +0000

    Switch to HTML5 doctype and add IE version to html tag in Swat demo.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/swat/trunk@60910 94490950-a86c-4493-9ccb-530f72933e47

--- demo/include/layout.php
+++ demo/include/layout.php
@@ -1,5 +1,8 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie8"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html> <!--<![endif]-->
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 	<?php echo $html_head_entries; ?>
