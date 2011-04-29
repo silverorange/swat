d60160b1ad53cd7aa631fc81a890e25ab4606b4e
Charles Waddell <charles@silverorange.com>
2011-04-29 18:23:37 +0000

    Revert r61911.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/swat/trunk@62041 94490950-a86c-4493-9ccb-530f72933e47

--- SwatDB/SwatDBRecordsetWrapper.php
+++ SwatDB/SwatDBRecordsetWrapper.php
@@ -736,16 +736,7 @@ abstract class SwatDBRecordsetWrapper extends SwatObject
 		$current_record_id = null;
 		$current_recordset = null;
 		foreach ($recordset as $record) {
-			if ($record->hasInternalValue($binding_field)) {
-				$record_id = $record->getInternalValue($binding_field);
-			} else if (isset($record->$binding_field)) {
-				$record_id = $record->$binding_field;
-			} else {
-				throw new SwatDBException(sprintf(
-					'Binding field “%s ”does not exist on the sub recordset.',
-					$binding_field));
-			}
-
+			$record_id = $record->getInternalValue($binding_field);
 			if ($record_id !== $current_record_id) {
 				$current_record_id = $record_id;
 				$current_recordset = $this[$record_id]->$name;
