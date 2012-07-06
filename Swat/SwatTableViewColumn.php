silverorange/swat

commit f83c69d9e4a381eec305665022be9e435ef9dccd

add content_type properties to title and abbreviated_title. Should this throw an error when you try to pass a xhtml snippt to a title when there is an abbreviated title (as it will always mimimize the entities)?
git-svn-id: file:///Users/gauthierm/svnrepo/public/swat/trunk@72767 94490950-a86c-4493-9ccb-530f72933e47

Swat/SwatTableViewColumn.php
@@ -42,6 +42,17 @@ class SwatTableViewColumn extends SwatCellRendererContainer
 	public $title = '';
 
 	/**
+	 * Optional content type for the title
+	 *
+	 * Default text/plain, use text/xml for XHTML fragments. Note that if an
+	 * $abbreviated_title is set that this is ignored and minimizesEntities() is
+	 * called on the title as the title is used as a html tag attribute.
+	 *
+	 * @var string
+	 */
+	public $title_content_type = 'text/plain';
+
+	/**
 	 * Optional abbreviated title of this column
 	 *
 	 * If set, an HTML abbr tag is used to display the $title property along
@@ -50,6 +61,15 @@ class SwatTableViewColumn extends SwatCellRendererContainer
 	 * @var string
 	 */
 	public $abbreviated_title = null;
+
+	/**
+	 * Optional content type for the abbreviated title.
+	 *
+	 * Default text/plain, use text/xml for XHTML fragments.
+	 *
+	 * @var string
+	 */
+	public $abbreviated_title_content_type = 'text/plain';
 
 	/**
 	 * The {@link SwatTableView} associated with this column
@@ -194,11 +214,19 @@ class SwatTableViewColumn extends SwatCellRendererContainer
 			$title = $this->title;
 
 		if ($this->abbreviated_title === null) {
-			echo SwatString::minimizeEntities($title);
+			if ($this->title_content_type === 'text/plain') {
+				echo SwatString::minimizeEntities($title);
+			} else {
+				echo $this->content;
+			}
 		} else {
 			$abbr_tag = new SwatHtmlTag('abbr');
-			$abbr_tag->title = $title;
-			$abbr_tag->setContent($this->abbreviated_title);
+			// Note: This always minimizes entities in titles regardless of
+			// $this->title_content_type, as its a html tag attribute.
+			$abbr_tag->title = SwatString::minimizeEntities($title);
+			$abbr_tag->setContent($this->abbreviated_title,
+				$this->abbreviated_title_content_type);
+
 			$abbr_tag->display();
 		}
 	}

Swat/SwatTableViewOrderableColumn.php
@@ -215,11 +215,15 @@ class SwatTableViewOrderableColumn extends SwatTableViewColumn
 			$title = '';
 		}
 
-		echo SwatString::minimizeEntities($title);
+		if ($this->title_content_type === 'text/plain') {
+			echo SwatString::minimizeEntities($title);
+		} else {
+			echo $title;
+		}
 
 		$span_tag = new SwatHtmlTag('span');
 		$span_tag->class = 'swat-table-view-orderable-column-title-last';
-		$span_tag->setContent($last_word);
+		$span_tag->setContent($last_word, $this->title_content_type);
 		$span_tag->display();
 
 		$anchor->close();

Swat/SwatTableViewSpanningColumn.php
@@ -9,7 +9,7 @@ require_once 'Swat/SwatHtmlTag.php';
  * This is a table view column that gets its own row.
  *
  * @package   Swat
- * @copyright 2005-2006 silverorange
+ * @copyright 2005-2012 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class SwatTableViewSpanningColumn extends SwatTableViewColumn
@@ -75,7 +75,7 @@ class SwatTableViewSpanningColumn extends SwatTableViewColumn
 
 			$th_tag = new SwatHtmlTag('th', $this->getThAttributes());
 			$th_tag->colspan = $offset;
-			$th_tag->setContent($this->title.':');
+			$th_tag->setContent($this->title.':', $this->title_content_type);
 			$th_tag->display();
 		} elseif ($offset > 0) {
 			$td_tag = new SwatHtmlTag('td');

commit 6c47268d7b765ac38ae92481cef69c249f1ab757

fix a bug introduced in r72767 when adding content_type to the title. Content isn't a varaible in the column object.
git-svn-id: file:///Users/gauthierm/svnrepo/public/swat/trunk@72923 94490950-a86c-4493-9ccb-530f72933e47

Swat/SwatTableViewColumn.php
@@ -217,7 +217,7 @@ class SwatTableViewColumn extends SwatCellRendererContainer
 			if ($this->title_content_type === 'text/plain') {
 				echo SwatString::minimizeEntities($title);
 			} else {
-				echo $this->content;
+				echo $this->title;
 			}
 		} else {
 			$abbr_tag = new SwatHtmlTag('abbr');