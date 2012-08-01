f8c5d2530be7e21c0671021e307763017de6cd0e
Nick Burka <nick@silverorange.com>
2012-08-01 14:09:24 +0000

    Stylesheet include for LESS css
    git-svn-id: file:///Users/gauthierm/svnrepo/public/swat/trunk@73481 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Swat/SwatLessStyleSheetHtmlHeadEntry.php
@@ -0,0 +1,37 @@
+<?php
+
+/* vim: set noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+
+require_once 'Swat/SwatStyleSheetHtmlHeadEntry.php';
+
+/**
+ * Stores and outputs an HTML head entry for a LESS stylesheet include
+ *
+ * @package   Swat
+ * @copyright 2012 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SiteLessStyleSheetHtmlHeadEntry extends SwatStyleSheetHtmlHeadEntry
+{
+	// {{{ public function display()
+
+	public function display($uri_prefix = '', $tag = null)
+	{
+		$uri = $this->uri;
+
+		// append tag if it is set
+		if ($tag !== null) {
+			$uri = (strpos($uri, '?') === false ) ?
+				$uri.'?'.$tag :
+				$uri.'&'.$tag;
+		}
+
+		printf('<link rel="stylesheet/less" type="text/css" href="%s%s" />',
+			$uri_prefix,
+			$uri);
+	}
+
+	// }}}
+}
+
+?>
