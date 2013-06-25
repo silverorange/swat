0a43b17b8daf0f75700dadd66ecb40c750040b73
Michael Gauthier <mike@silverorange.com>
2013-06-25 21:04:30 +0000

    Interface and exception for marshalling objects. See http://en.wikipedia.org/wiki/Marshalling_(computer_science)
    git-svn-id: file:///Users/gauthierm/svnrepo/public/swat/trunk@81893 94490950-a86c-4493-9ccb-530f72933e47

---
+++ SwatDB/SwatDBMarshallable.php
@@ -0,0 +1,69 @@
+<?php
+
+/* vim: set noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+
+require_once 'SwatDB/exceptions/SwatDBMarshallException.php';
+
+/**
+ * Interface for marshalling and unmarshalling data-objects
+ *
+ * Marshalling converts data-objects into primitive data types which can easily
+ * and efficiently be serialized. Unmarshalling restores an object using
+ * previously marshalled data.
+ *
+ * An advantage of marshalling over straight serialization is you can specify
+ * exactly what tree of objects will be included in the marshalled result.
+ *
+ * @package   SwatDB
+ * @copyright 2013 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+interface SwatDBmarshallable
+{
+	// {{{ public function marshall()
+	/**
+	 * Marshalls this object
+	 *
+	 * <code>
+	 * <?php
+	 * $data = $object->marshall(
+	 *     array(
+	 *         'daughters',
+	 *         'sons' => array(
+	 *             'grandkids'
+	 *         )
+	 *     )
+	 * );
+	 * ?>
+	 * </code>
+	 *
+	 * @param array $tree optional. An array representing the data-structure
+	 *                    sub-tree to include in the marshalled data.
+	 *
+	 * @return array the marshalled data.
+	 *
+	 * @throws SwatDBMarshallException if one of the sub-tree properties
+	 *         cannot be marshalled.
+	 */
+	public function marshall(array $tree = array());
+
+	// }}}
+	// {{{ public function unmarshall()
+
+	/**
+	 * Unmarshalls this object using the specified data
+	 *
+	 * <code>
+	 * <?php
+	 * $object->unmarshall($data);
+	 * ?>
+	 * </code>
+	 *
+	 * @param array $data optional. The marshalled object data.
+	 */
+	public function unmarshall(array $data = array());
+
+	// }}}
+}
+
+?>
---
+++ SwatDB/exceptions/SwatDBMarshallException.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once 'SwatDB/exceptions/SwatDBException.php';
+
+/* vim: set noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+
+/**
+ * Thrown when a property that can not be marshalled is asked to be
+ * marshalled
+ *
+ * @package   SwatDB
+ * @copyright 2013 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SwatDBMarshallException extends SwatDBException
+{
+	// {{{ protected properties
+
+	/**
+	 * @var string
+	 */
+	protected $property = '';
+
+	// }}}
+	// {{{ public function __construct()
+
+	public function __construct($message, $code = 0, $property = '')
+	{
+		parent::__construct($message, $code);
+		$this->property = $property;
+	}
+
+	// }}}
+	// {{{ public function getProperty()
+
+	public function getProperty()
+	{
+		return $this->property;
+	}
+
+	// }}}
+}
+
+?>
