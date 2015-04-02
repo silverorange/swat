291ab493985a05cb3b4ab8461e2e2fdeb257b284
Michael Gauthier <mike@silverorange.com>
2015-04-02 18:30:12 -0300

    Start converting view and table view JS to jquery.

--- www/javascript/swat-view.js
+++ www/javascript/swat-view.js
@@ -1,175 +1,208 @@
+(function($) {
+
 /**
  * Creates a new recordset view
  *
  * This is the base class used for recordset views. It is primarily
  * responsible for providing helper methods for dynamically highlighting
- * selecgted items in the view.
- *
- * @param String id the identifier of this view.
+ * selected items in the view.
  *
  * @see SwatTableView
  * @see SwatTileView
  */
-function SwatView(id)
-{
-	this.id = id;
-	this.item_selection_counts = [];
-	this.item_selectors = [];
-	this.selector_item_counts = [];
-	this.items = [];
-}
+$.widget('swat.swatview', {
+	version: '2.2.3',
+	_create: function() {
+		this._itemSelectionCounts = {};
+		this._itemSelectors = {};
+		this._selectorItemCounts = {};
+		this._items = [];
 
-/**
- * Gets an item node given an arbitrary descendant node
- *
- * @param DOMElement node the arbitrary descendant node.
- *
- * @return DOMElement the item node.
- */
-SwatView.prototype.getItemNode = function(node)
-{
-	return node;
-};
+		this._createItems();
+	},
 
-/**
- * Gets an identifier for an item in this view
- *
- * @param DOMElement item_node an item node in this view
- *
- * @return String an identifier for the given item node.
- *
- * @see SwatView::getItemNode()
- * @todo I'd like to improve this method to not use an O(n) lookup algorithm.
- */
-SwatView.prototype.getItemNodeKey = function(item_node)
-{
-	var key = null;
-	for (var i = 0; i < this.items.length; i++) {
-		if (item_node === this.items[i]) {
-			key = i;
-			break;
-		}
-	}
-	return key;
-};
+	/**
+	 * Populates the items array with the items in this view.
+	 */
+	_createItems: $.noop,
 
-/**
- * Gets the number of items selected in this view for the specified selector
- *
- * @param String the selector identifier to coult the selected items for.
- *
- * @return Number the number of selected items for the given selector.
- */
-SwatView.prototype.getSelectorItemCount = function(selector)
-{
-	if (this.selector_item_counts[selector])
-		return this.selector_item_counts[selector];
+	/**
+	 * Gets a view item given an arbitrary descendant
+	 *
+	 * @param jQuery the the arbitrary descendant.
+	 *
+	 * @return jQuery the view item to which the descendant belongs.
+	 */
+	_getItem: function(node) {
+		return node;
+	},
 
-	return 0;
-};
-
-/**
- * Selects an item node in this view
- *
- * An item may be selected multiple times by different selectors. This can
- * be checked using the SwatView::isSelected() method.
- *
- * @param DOMElement node an arbitrary descendant node of the item node to be
- *                         selected.
- * @param String selector an identifier of the object that selected the item
- *                         node.
- */
-SwatView.prototype.selectItem = function(node, selector)
-{
-	// get main selectable item node key
-	var key = this.getItemNodeKey(this.getItemNode(node));
-
-	if (!this.item_selectors[key])
-		this.item_selectors[key] = [];
-
-	// if this item node is already not selected by the selector, increment
-	// the selection count
-	if (!this.item_selectors[key][selector]) {
-		// increment selection count for the item
-		if (this.item_selection_counts[key]) {
-			this.item_selection_counts[key]++;
-		} else {
-			this.item_selection_counts[key] = 1;
-		}
-
-		// increment item count for the selector
-		if (this.selector_item_counts[selector]) {
-			this.selector_item_counts[selector]++;
-		} else {
-			this.selector_item_counts[selector] = 1;
-		}
-	}
-
-	// remember that this node is selected by the selector
-	this.item_selectors[key][selector] = true;
-};
-
-/**
- * Deselects an item node in this view
- *
- * An item may be selected multiple times by different selectors. This can
- * be checked using the SwatView::isSelected() method.
- *
- * @param DOMElement node an arbitrary descendant node of the item node to be
- *                         deselected.
- * @param String selector an identifier of the object that deselected the item
- *                         node.
- */
-SwatView.prototype.deselectItem = function(node, selector)
-{
-	// get main selectable item node
-	var key = this.getItemNodeKey(this.getItemNode(node));
-
-	// can only deselect if the item node is selected
-	if (this.item_selectors[key]) {
-		// check if the item node is selected by the selector
-		if (this.item_selectors[key][selector]) {
-			// remember that the item node is not selected by the selector
-			this.item_selectors[key][selector] = false;
-
-			// decrement the selection count for the item
-			if (this.item_selection_counts[key]) {
-				this.item_selection_counts[key] =
-					Math.max(this.item_selection_counts[key] - 1, 0);
-			} else {
-				this.item_selection_counts[key] = 0;
-			}
-
-			// decrement the item count for the selector
-			if (this.selector_item_counts[selector]) {
-				this.selector_item_counts[selector] =
-					Math.max(this.selector_item_counts[selector] - 1, 0);
-			} else {
-				this.selector_item_counts[selector] = 0;
+	/**
+	 * Gets an identifier for an item in this view
+	 *
+	 * @param jQuery item the item in this view.
+	 *
+	 * @return String an identifier for the given item. If the item is not an
+	 *                item in this view, null is returned.
+	 *
+	 * @see _getItem()
+	 *
+	 * @todo Improve this method to not use an O(n) lookup algorithm.
+	 */
+	_getItemId: function(item) {
+		var id = null;
+		for (var i = 0; i < this._items.length; i++) {
+			if (item.eq(this._items[i])) {
+				id = i;
+				break;
 			}
 		}
-	}
-};
+		return id;
+	},
 
-/**
- * Checks whether or not an item node is selected given an arbitrary
- * descendant node
- *
- * An item is considered selected if one or more selectors have selected
- * it.
- *
- * @param DOMElement node an arbitrary descendant node of the item node to
- *                         be checked for selection.
- *
- * @return Boolean true if the item node is selected and false if it is not.
- */
-SwatView.prototype.isSelected = function(node)
-{
-	var key = this.getItemNodeKey(this.getItemNode(node));
-	if (typeof(this.item_selection_counts[key]) == 'undefined')
+	/**
+	 * Gets the number of items selected in this view for the specified
+	 * selector
+	 *
+	 * @param String selector the selector identifier for which to count the
+	 *                        selected items.
+	 *
+	 * @return Number the number of selected items for the given selector.
+	 */
+	_getSelectorItemCount: function(selector) {
+		if (this._selectorItemCounts[selector]) {
+			return this._selectorItemCounts[selector];
+		}
+
+		return 0;
+	},
+
+	selectItem: function(item, selector) {
+		var that = this;
+		item.each(function() {
+			that._selectItem($(this), selector);
+		});
+	},
+
+	deselectItem: function(item, selector) {
+		var that = this;
+		item.each(function() {
+			that._deselectItem($(this), selector);
+		});
+	},
+
+	/**
+	 * Selects an item in this view
+	 *
+	 * An item may be selected multiple times by different selectors. This can
+	 * be checked using the isSelected() method.
+	 *
+	 * @param jQuery item     an arbitrary descendant of the item to be
+	 *                        selected.
+	 * @param String selector the selector identifier.
+	 */
+	_selectItem: function(item, selector) {
+		// get main selectable item node key
+		var itemId = this._getItemId(this._getItem(item));
+
+		// create item selection if it doesn't exist
+		if (!this._itemSelectors[itemId]) {
+			this._itemSelectors[itemId] = {};
+			this._itemSelectors[itemId][selector] = false;
+		}
+
+		// if this item node is already not selected by the selector, increment
+		// the selection count
+		if (!this._itemSelectors[itemId][selector]) {
+			// increment selection count for the item
+			if (this._itemSelectionCounts[itemId]) {
+				this._itemSelectionCounts[itemId]++;
+			} else {
+				this._itemSelectionCounts[itemId] = 1;
+			}
+
+			// increment item count for the selector
+			if (this._selectorItemCounts[selector]) {
+				this._selectorItemCounts[selector]++;
+			} else {
+				this._selectorItemCounts[selector] = 1;
+			}
+		}
+
+		// remember that the item is selected by the selector
+		this._itemSelectors[itemId][selector] = true;
+
+		return this;
+	},
+
+	/**
+	 * Deselects an item in this view
+	 *
+	 * An item may be selected multiple times by different selectors. This can
+	 * be checked using the isSelected() method.
+	 *
+	 * @param jQuery item     an arbitrary descendant of the item to be
+	 *                        deselected.
+	 * @param String selector the selector identifier.
+	 */
+	_deselectItem: function(item, selector) {
+		// get main selectable item node
+		var itemId = this._getItemId(this._getItem(item));
+
+		// can only deselect if the item node is selected
+		if (this._itemSelectors[itemId]) {
+			// check if the item node is selected by the selector
+			if (this._itemSelectors[itemId][selector]) {
+				// remember that the item node is not selected by the selector
+				this._itemSelectors[itemId][selector] = false;
+
+				// decrement the selection count for the item
+				if (this._itemSelectionCounts[itemId]) {
+					this._itemSelectionCounts[itemId] = Math.max(
+						this._itemSelectionCounts[itemId] - 1,
+						0
+					);
+				} else {
+					this._itemSelectionCounts[itemId] = 0;
+				}
+
+				// decrement the item count for the selector
+				if (this._selectorItemCounts[selector]) {
+					this._selectorItemCounts[selector] = Math.max(
+						this._selectorItemCounts[selector] - 1,
+						0
+					);
+				} else {
+					this._selectorItemCounts[selector] = 0;
+				}
+			}
+		}
+
+		return this;
+	},
+
+	/**
+	 * Checks whether or not an item is selected given an arbitrary descendant
+	 *
+	 * An item is considered selected if one or more selectors have selected
+	 * it.
+	 *
+	 * @param jQuery item an arbitrary descendant of the item to be checked for
+	 *                    selection.
+	 *
+	 * @return Boolean true if the item is selected and false if it is not.
+	 */
+	isSelected: function(item) {
+		var itemId = this._getItemId(this._getItem(item));
 		var selected = false;
-	else
-		var selected = (this.item_selection_counts[key] > 0);
 
-	return selected;
-};
+		if (typeof this._itemSelectionCounts[itemId] !== 'undefined') {
+			selected = (this._itemSelectionCounts[itemId] > 0);
+		}
+
+		return selected;
+	}
+
+});
+
+})(jQuery);
