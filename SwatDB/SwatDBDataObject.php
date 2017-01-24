7415529fe69945b175ab1a647c12ca5db83b42e2
Isaac Grant <isaac@silverorange.com>
2017-01-24 13:21:43 -0400

    Add getAttributes() method to SwatDBDataObject
    Now that we're using getter and setter patterns in our dataobjects we
    can no longer use get_object_vars() on the dataobject to get all the
    properties we care about, for example when editing with admin tools.
    
    getAttributes() is a replacement for places where we have relied on
    get_object_vars() in the past to remedy this.

--- SwatDB/SwatDBDataObject.php
+++ SwatDB/SwatDBDataObject.php
@@ -488,6 +488,32 @@ class SwatDBDataObject extends SwatObject
 	}
 
 	// }}}
+	// {{{ public function getAttributes()
+
+	/**
+	 * Returns an array of the public and protected properties of this object
+	 *
+	 * This array is useful for places where get_object_vars() is useful but we
+	 * also want to return the protected properties alongside the public onces.
+	 * For example when using getter and setter methods instead of public
+	 * properities on a dataobject.
+	 *
+	 * @return array an array of public and protected properties.
+	 *
+	 * @see SwatDBDataObject::getPublicProperties()
+	 * @see SwatDBDataObject::getProtectedProperties()
+	 */
+	public function getAttributes()
+	{
+		$property_array = array_merge(
+			$this->getPublicProperties(),
+			$this->getProtectedProperties()
+		);
+
+		return $property_array;
+	}
+
+	// }}}
 	// {{{ protected function setInternalValue()
 
 	protected function setInternalValue($name, $value)
