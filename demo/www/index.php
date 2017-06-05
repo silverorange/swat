8100e7cb8f183671446ea0e47f685dd0cb5d32bb
Michael Gauthier <mike@silverorange.com>
2017-06-04 22:52:54 -0300

    Fix cs

--- demo/www/index.php
+++ demo/www/index.php
@@ -2,8 +2,8 @@
 
 setlocale(LC_ALL, 'fr_FR.utf8');
 
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/../include/DemoApplication.php';
+require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/../include/DemoApplication.php';
 
 SwatException::setupHandler();
 
