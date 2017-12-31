9b0e34930b2c472302cfaa934db05385823224c5
Michael Gauthier <mike@silverorange.com>
2017-12-31 06:08:17 -0400

    Add styles to layout

--- demo/include/layout.php
+++ demo/include/layout.php
@@ -4,7 +4,8 @@
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 		<script src="packages/yui/yahoo-dom-event/yahoo-dom-event.js"></script>
 		<script src="bundle.js"></script>
-		<style type="text/css" media="all">@import "styles/demo.css";</style>
+		<link rel="stylesheet" type="text/css" href="bundle.css" />
+		<link rel="stylesheet" type="text/css" href="styles/demo.css" />
 		<title><?php echo $title; ?></title>
 		<link rel="icon" href="favicon.ico" type="image/x-icon" />
 	</head>
