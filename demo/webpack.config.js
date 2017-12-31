8d9116c75bb85518ee72f1e2822659e9df7d029f
Michael Gauthier <mike@silverorange.com>
2017-12-31 06:08:01 -0400

    Add fileloader and extract text plugins

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,3 +1,5 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
 	entry: './vendor/silverorange/swat/www/javascript/index.js',
 	output: {
@@ -5,6 +7,14 @@ module.exports = {
 	},
 	module: {
 		rules: [
+			{
+				test: /\.css$/,
+				exclude: /(node_modules)/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: [ 'css-loader' ]
+				})
+			},
 			{
 				test: /\.js$/,
 				exclude: /(node_modules)/,
@@ -14,10 +24,25 @@ module.exports = {
 						presets: ['env']
 					}
 				}
+			},
+			{
+				test: /\.(gif|png|jpe?g|svg)$/i,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'file-loader',
+					options: {
+						useRelativePath: true,
+						publicPath: 'images/',
+						outputPath: 'www/images/'
+					}
+				}
 			}
 		]
 	},
 	resolve: {
 		symlinks: false
-	}
+	},
+	plugins: [
+		new ExtractTextPlugin('www/bundle.css')
+	]
 };
