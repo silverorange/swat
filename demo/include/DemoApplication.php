6bad35be273efd3e83df65578da739b5cdb7070a
Michael Gauthier <mike@silverorange.com>
2018-03-23 22:47:12 -0300

    Remove unused ReCaptcha widget

--- demo/include/DemoApplication.php
+++ demo/include/DemoApplication.php
@@ -42,7 +42,6 @@ class DemoApplication
 		'ProgressBar'       => 'SwatProgressBar',
 		'RadioList'         => 'SwatRadioList',
 		'Rating'            => 'SwatRating',
-		'Recaptcha'         => 'SwatReCaptcha',
 		'Replicable'        => 'SwatReplicable',
 		'SelectList'        => 'SwatSelectList',
 		'String'            => 'SwatString',
