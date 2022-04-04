be9bfa5f56ed11f72cf53a4c4d1a3e6a70b4f292
Philip Parke <philip@silverorange.com>
2022-04-04 10:26:04 -0300

    add an html head entry class to handle external urls

---
+++ Swat/SwatExternalJavaScriptHtmlHeadEntry.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Stores and outputs an HTML head entry for a JavaScript include
+ *
+ * @package   Swat
+ * @copyright 2006-2016 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class SwatExternalJavaScriptHtmlHeadEntry extends SwatHtmlHeadEntry
+{
+    // {{{ protected function displayInternal()
+
+    protected function displayInternal()
+    {
+        $uri = $this->uri;
+
+        printf(
+            '<script type="text/javascript" src="%s"></script>',
+            $uri
+        );
+    }
+
+    // }}}
+    // {{{ protected function displayInlineInternal()
+
+    protected function displayInlineInternal($path)
+    {
+        echo '<script type="text/javascript">';
+        readfile($path . $this->getUri());
+        echo '</script>';
+    }
+
+    // }}}
+}
