e0cf49cd10c20f08b5f1cf3a0e21120f96475395
Philip Parke <philip@silverorange.com>
2022-04-20 14:28:58 -0300

    try without quoting js string

--- Swat/SwatTextareaEditor.php
+++ Swat/SwatTextareaEditor.php
@@ -345,7 +345,7 @@ class SwatTextareaEditor extends SwatTextarea
         $lines = array();
         foreach ($this->getConfig() as $name => $value) {
             if (is_string($value)) {
-                $value = SwatString::quoteJavaScriptString($value);
+                $value = "'{$value}'";
             } elseif (is_bool($value)) {
                 $value = $value ? 'true' : 'false';
             }
