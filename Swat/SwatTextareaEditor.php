9583a3fd29f9b772c88313f0bb6f7e286d16d8f1
Philip Parke <philip@silverorange.com>
2022-04-26 16:50:00 -0300

    backcolor

--- Swat/SwatTextareaEditor.php
+++ Swat/SwatTextareaEditor.php
@@ -408,7 +408,7 @@ class SwatTextareaEditor extends SwatTextarea
 				toRemove.forEach(r => data.node.removeChild(r));
 			},\n" .
             "\tmenubar: false,\n" .
-            "\ttextcolor_map: [\n" .
+            "\tbackcolor_map: [\n" .
             "\t\t'ffefee', 'Pitfall',\n" .
             "\t\t'e6f9f6', 'Pearl',\n" .
             "\t\t'fff0e2', 'Perspective',\n" .
