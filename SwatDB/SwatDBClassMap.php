300e674f71155f8605776adca96823346c043ebe
Colin Viebrock <colin@viebrock.ca>
2024-07-11 08:38:17 -0500

    Don't extend SwatObject

--- SwatDB/SwatDBClassMap.php
+++ SwatDB/SwatDBClassMap.php
@@ -9,7 +9,7 @@
  * @copyright 2006-2016 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-class SwatDBClassMap extends SwatObject
+class SwatDBClassMap
 {
     // {{{ private properties
 
