07eba010df3a985359c53f920025422ac2dd9122
Colin Viebrock <colin@viebrock.ca>
2024-09-16 16:25:28 -0500

    Use a closing word that isn't part of the heredoc/nowdoc content

--- Swat/SwatXHTMLTextarea.php
+++ Swat/SwatXHTMLTextarea.php
@@ -56,7 +56,7 @@ class SwatXHTMLTextarea extends SwatTextarea
 
             $xhtml_template =
                 $doctype .
-                <<<XHTML
+                <<<EOB
                 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
                 	<head>
                 		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
@@ -69,7 +69,7 @@ class SwatXHTMLTextarea extends SwatTextarea
                 	</body>
                 </html>
 
-                XHTML;
+                EOB;
         }
 
 
