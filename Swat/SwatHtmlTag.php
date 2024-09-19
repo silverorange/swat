7fcf453ca47d5ffe2cfc6cfd9486cbf1cdc435b9
Colin Viebrock <colin@viebrock.ca>
2024-09-19 16:43:09 -0500

    Add AllowDynamicProperties attribute (not strictly needed because of __get/__set methods)

--- Swat/SwatHtmlTag.php
+++ Swat/SwatHtmlTag.php
@@ -7,6 +7,8 @@
  * @copyright 2004-2016 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
+
+#[\AllowDynamicProperties]
 class SwatHtmlTag extends SwatObject
 {
 
