be5176ef4839ce647ae5fe435e410d000a3c1210
Colin Viebrock <colin@viebrock.ca>
2024-09-20 11:06:12 -0500

    Fix to string concat, and switch fall-through comments

--- Swat/SwatFileEntry.php
+++ Swat/SwatFileEntry.php
@@ -260,7 +260,7 @@ class SwatFileEntry extends SwatInputControl
         }
 
         throw new SwatException(
-            "Path '{$path}' is not a " . 'directory or does not exist.',
+            "Path '{$path}' is not a directory or does not exist.",
         );
     }
 
@@ -606,24 +606,23 @@ class SwatFileEntry extends SwatInputControl
             $value = (int) mb_substr($ini_value, 0, -1);
 
             // No breaks on purpose. We want the values to fall through.
-            // "no break" comments below are for PSR coding style
             switch ($size) {
                 case 'P':
                     $value *= 1024;
-
                     // no break
+
                 case 'T':
                     $value *= 1024;
-
                     // no break
+
                 case 'G':
                     $value *= 1024;
-
                     // no break
+
                 case 'M':
                     $value *= 1024;
-
                     // no break
+
                 case 'K':
                     $value *= 1024;
                     break;
